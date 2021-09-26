032582408616f152d5a3da1e7de8b0ee8d4411cf
yagami-light <kevalvaghela1993@gmail.com>
2021-09-26 09:47:39 +0530

    Time: 19 ms (6.24%), Space: 53.7 MB (15.06%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+    Set<Integer> set=new HashSet();        
+        for(int n:nums){
+            if(!set.contains(n))
+                set.add(n);
+            else return true;
+            
+        }
+        return false;
+        
+    }
+}
