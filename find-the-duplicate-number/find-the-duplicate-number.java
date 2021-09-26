13339eef4968f665b5af14989ff853c8c233e84c
yagami-light <kevalvaghela1993@gmail.com>
2021-09-26 16:23:22 +0530

    Time: 4 ms (88.50%), Space: 56.9 MB (51.67%) - LeetHub

---
+++ find-the-duplicate-number/find-the-duplicate-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow=nums[0];
+        int fast=nums[nums[0]];
+        
+        while(fast!=slow){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            
+        }
+        fast=0;
+        while(fast!=slow){
+            fast=nums[fast];
+            slow=nums[slow];
+            
+        }
+        
+        return slow;
+    }
+}
