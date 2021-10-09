69a7d9f78552ba751895af943b61ad8ac56a5969
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-10-09 19:26:51 +0530

    Time: 47 ms (14.53%), Space: 51.7 MB (5.55%) - LeetHub

--- permutations-ii/permutations-ii.java
+++ permutations-ii/permutations-ii.java
@@ -1,35 +1,38 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        
         List<List<Integer>> result=new ArrayList();
-        Arrays.sort(nums);
         boolean[] used=new boolean[nums.length];
+        Arrays.sort(nums);
         backTrack(nums,new ArrayList(),result,used);
+        
+        
         return result;
         
     }
     
     
-    private void backTrack(final int[] nums,final List<Integer> currentList,final List<List<Integer>> result,boolean[] used){
-        
+    private void backTrack(int[] nums,List<Integer> currentList,List<List<Integer>> result, boolean[] used){
+        System.out.println(currentList);
         if(currentList.size()==nums.length){
             result.add(new ArrayList(currentList));
             return;
-            
         }
         
+        
         for(int i=0;i<nums.length;i++){
-            
-            if(used[i] || (i>0 && nums[i]==nums[i-1] && !used[i-1])) continue;
+            if(used[i]) continue;
+           
+            if(i>0 && nums[i]==nums[i-1] && !used[i-1]) continue;
+             currentList.add(nums[i]);
             used[i]=true;
-            currentList.add(nums[i]);
             backTrack(nums,currentList,result,used);
-            used[i]=false;
             currentList.remove(currentList.size()-1);
+            used[i]=false;
             
             
         }
         
     }
+    
     
 }
