ec2d7ca41fc2040b145b3c6ff352b3e15fb61ee4
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-10-09 21:11:17 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (47.46%) - LeetHub

--- rotate-image/rotate-image.java
+++ rotate-image/rotate-image.java
@@ -1,39 +1,44 @@
 class Solution {
     public void rotate(int[][] matrix) {
         
-        int n=matrix.length;
         
-        for(int i=0;i<n;i++){
+        rotateUtil(matrix);
+        
+    }
+    
+    
+    private void rotateUtil(int[][] mat){
+        int m=mat.length;
+        int n=mat[0].length;
+        for(int i=0;i<m;i++){
             
             for(int j=i;j<n;j++){
-                
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=temp;
+                int temp=mat[i][j];
+                mat[i][j]=mat[j][i];
+                mat[j][i]=temp;
                 
                 
             }
+            
         }
         
         
-        
-        for(int i=0;i<n;i++){
+        for(int i=0;i<m;i++){
             
             for(int j=0;j<n/2;j++){
                 
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[i][n-1-j];
-                matrix[i][n-1-j]=temp;
-                
-                
+                int temp=mat[i][j];
+                mat[i][j]=mat[i][n-1-j];
+                mat[i][n-1-j]=temp;
                 
             }
             
             
-            
         }
+        
         
         
         
     }
+    
 }
