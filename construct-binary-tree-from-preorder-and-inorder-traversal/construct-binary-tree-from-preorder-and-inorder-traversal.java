fe63b6fbd0c739c2829feeb6823b8915e00a2e8a
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-10-16 18:28:07 +0530

    Time: 45 ms (5.10%), Space: 41.5 MB (31.60%) - LeetHub

--- construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,39 +15,60 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preOrder, int[] inOrder) {
-        
         return buildTreeUtil(preOrder,inOrder);
+        
     }
     
     
-    private TreeNode buildTreeUtil(final int[] preOrder, int[] inOrder){
-        
-        Map<Integer,Integer> indexCache=new HashMap();
+    private TreeNode buildTreeUtil(int[] preOrder,int[] inOrder){
+        int n=preOrder.length;
+        Map<Integer,Integer> map=new HashMap();
         for(int i=0;i<inOrder.length;i++)
-            indexCache.put(inOrder[i],i);
+            map.put(inOrder[i],i);
+        // int indexIn=map.get(preOrder[0]);
+       
+       return buildTreeUtil(preOrder,inOrder,0,n-1,0,map);
+     
         
-        return buildTreeUtil(preOrder,inOrder,0,inOrder.length-1,0,indexCache);
+        
         
         
     }
-    
-    
-    
-    private TreeNode buildTreeUtil(final int[] preOrder,int[] inOrder,int inStart,int inEnd, int preIndexStart,Map<Integer,Integer> indexCache){
-        if((preIndexStart>= preOrder.length) || (inStart>inEnd))
-        return null;
-        TreeNode root=new TreeNode(preOrder[preIndexStart]);
-        int inStartIndex=indexCache.get(root.val);
-
-        root.left=buildTreeUtil(preOrder,inOrder,inStart,inStartIndex-1,preIndexStart+1,indexCache);
-        root.right=buildTreeUtil(preOrder,inOrder,inStartIndex+1,inEnd,preIndexStart+1+inStartIndex-inStart,indexCache);
+      private TreeNode buildTreeUtil2(final int[] preOrder,final int[] inOrder,int inOrderIndexStart,int inOrderIndexEnd,int preOrderIndexStart,Map<Integer,Integer> cache){
+        if((preOrderIndexStart>=preOrder.length || inOrderIndexStart> inOrderIndexEnd))
+            return null;
+        TreeNode root=new TreeNode(preOrder[preOrderIndexStart]);
+        int inStartIndex=cache.get(root.val);
         
-        
+        root.left=buildTreeUtil2(preOrder,inOrder,inOrderIndexStart,inStartIndex-1,preOrderIndexStart+1,cache);
+        root.right=buildTreeUtil2(preOrder,inOrder,inStartIndex+1,inOrderIndexEnd,preOrderIndexStart+1+inStartIndex-inOrderIndexStart,cache);
         
         return root;
         
         
         
+        
     }
+    
+    private TreeNode buildTreeUtil(int[] preOrder,int[] inOrder,int inOrderIndexStart,int inOrderIndexEnd,int preOrderIndexStart,Map<Integer,Integer> map){
+        System.out.println("printing index preStartIndex "+preOrderIndexStart+"inOrderIndex start "+inOrderIndexStart +"inOrder end "+inOrderIndexEnd);
+        if(preOrderIndexStart >= preOrder.length ||  inOrderIndexStart>inOrderIndexEnd )
+            return null;
+        
+        
+        
+        TreeNode root=new TreeNode(preOrder[preOrderIndexStart]);
+        System.out.println("priting value "+root.val);
+        int inStartIndex=map.get(root.val);
+        
+        root.left=buildTreeUtil(preOrder,inOrder,inOrderIndexStart,inStartIndex-1,preOrderIndexStart+1,map);
+        root.right=buildTreeUtil(preOrder,inOrder,inStartIndex+1,inOrderIndexEnd,preOrderIndexStart+1+inStartIndex-inOrderIndexStart,map);
+        
+        
+        return root;
+        
+    }
+    
+    
     
 }
