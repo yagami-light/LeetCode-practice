19f9dd11d4fd5a46f107c0ca084e536dcbbde14b
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-10-16 22:09:12 +0530

    Time: 34 ms (6.79%), Space: 47.9 MB (8.93%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.java
+++ top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,34 +1,29 @@
 import java.util.function.*;
 class Solution {
-    class Node{
-        int value;
+    class classNode{
+        Integer value;
         Long freq;
-        Node(int k,Long v){
-            value=k;
-            freq=v;
-            
+        
+        classNode(Integer value,Long freq){
+            this.value=value;
+            this.freq=freq;
         }
         
+        
     }
-    
     public int[] topKFrequent(int[] nums, int k) {
+        PriorityQueue<classNode> pq=new PriorityQueue(Comparator.comparing((classNode node)->node.freq).reversed());
         int[] result=new int[k];
-        final PriorityQueue<Node> pq=new PriorityQueue(Comparator.comparing((Node n)-> n.freq).reversed());
-        Map<Integer,Long> freq= Arrays.stream(nums).boxed().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
-        freq.entrySet().stream().forEach((i)-> pq.add(new Node(i.getKey(),(i.getValue()))));
+        Map<Integer,Long> map=Arrays.stream(nums).boxed().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+        map.entrySet().stream().forEach(i->pq.add(new classNode(i.getKey(),i.getValue())));
         
         for(int i=0;i<k;i++){
             result[i]=pq.remove().value;
+            
             
         }
         
         return result;
         
-        
     }
-    
-    
-    
-    
-    
 }
