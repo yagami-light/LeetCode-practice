30c559101d1aa22300d0dbf33e2c4b02c78e8bcd
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-10-17 10:36:03 +0530

    Time: 1 ms (95.47%), Space: 40.6 MB (69.91%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.java
+++ set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,42 +1,54 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        
         setZeroesUtil(matrix);
     }
     
-    private void setZeroesUtil(int[][] mat){
-        int m=mat.length;
-        int n=mat[0].length;
-        
-        int col0=1;
+    private void setZeroesUtil(int[][] matrix){
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int isColZero=1;
         
         for(int i=0;i<m;i++){
-                if(mat[i][0]==0) col0=0;
+            
+            if(matrix[i][0]==0)
+                isColZero=0;
             
             for(int j=1;j<n;j++){
                 
-                if(mat[i][j]==0){
-                    mat[i][0]=mat[0][j]=0;
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
                 
+                
+                
             }
+            
         }
+        
         
         
         for(int i=m-1;i>=0;i--){
-            
+          
             
             for(int j=n-1;j>=1;j--){
                 
-                if(mat[i][0]==0 || mat[0][j]==0)
-                    mat[i][j]=0;
+                 
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                    matrix[i][j]=0;
+                
+                
             }
-            if(col0==0){
-                mat[i][0]=0;
-            }
+             if(isColZero==0)
+                     matrix[i][0]=0;
             
         }
         
         
+        
+        
+        
     }
+    
 }
