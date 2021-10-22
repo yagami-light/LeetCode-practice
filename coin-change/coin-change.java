04b44a5cc8ce3aafd43df7db1ce277c477ecd253
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-10-22 12:03:58 +0530

    Time: 43 ms (20.35%), Space: 42.2 MB (17.86%) - LeetHub

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -1,38 +1,33 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] cache=new int[amount+1];
-        return coinChangeUtil(coins,amount,0,cache);
+        
+       int  cache[]=new int[amount+1];
+        return coinChangeUtil(coins,amount,cache);
     }
     
     
     
-    private int coinChangeUtil(int[] coins,int amount,int index,int[] cache){
-      
+    private int coinChangeUtil(int[] coins,int amount,int[] cache){
         if(amount<0)
             return -1;
         if(amount==0)
             return 0;
-          if(cache[amount]!=0)
+        if(cache[amount]!=0)
             return cache[amount];
         
         int min=Integer.MAX_VALUE;
-        for(int i=0;i<coins.length;i++){
-            int taken=coinChangeUtil(coins,amount-coins[i],i,cache);
-            if(taken >=0 && taken<min){
-                min=1+taken;
-            }
+        for(int coin:coins){
             
-            
+            int res=coinChangeUtil(coins,amount-coin,cache);
+            if(res>=0 && res<min)
+                min=res+1;
             
         }
         
         
-        int computedValue= (min==Integer.MAX_VALUE) ? -1 : min;
+        int computedValue=(min==Integer.MAX_VALUE) ? -1 : min;
         cache[amount]=computedValue;
         return cache[amount];
         
     }
-    
-    
-    
 }
