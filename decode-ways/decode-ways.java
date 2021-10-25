0af76d940fd49a00a34352a1580d1dc4e9b68188
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-10-25 10:15:36 +0530

    Time: 20 ms (5.02%), Space: 39.1 MB (31.30%) - LeetHub

--- decode-ways/decode-ways.java
+++ decode-ways/decode-ways.java
@@ -1,5 +1,44 @@
 class Solution {
-    public int numDecodings(String s) {
+    
+    public int numDecodings(String s){
+        int[] cache=new int[s.length()+1];
+        
+        return numDecodingsUtil2(0,s,cache);
+    }
+    
+    
+    private int numDecodingsUtil2(int p,String s,int[] cache){
+        int n=s.length();
+        if(p==n) return 1;
+        
+        if(s.charAt(p)=='0')
+            return 0;
+        if(cache[p]!=0)
+            return cache[p];
+        
+        int res=numDecodingsUtil2(p+1,s,cache);
+        
+        if(p<n-1 && (s.charAt(p)=='1' || s.charAt(p)=='2' &&  s.charAt(p+1) < '7'))
+            res+=numDecodingsUtil2(p+2,s,cache);
+        
+        cache[p] = res;
+        return cache[p];
+        
+        
+    }
+    
+    
+    
+   /*   int n=s.length();
+        if(p==n) return 1;
+        if(s.charAt(p)=='0') return 0;
+        int res=numDecodings(p+1,s);
+        if(p<n-1&&(s.charAt(p)=='1'||s.charAt(p)=='2'&&s.charAt(p+1)<'7')) 
+			res+=numDecodings(p+2,s);
+        return res;
+    */
+    
+    private  int numDecodingsUtil1(String s) {
         
         if(s==null || s.length() ==0)
             return 0;
