346bde5dc79b91c100ec877aa6f045f4a3948e60
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-10-26 12:35:48 +0530

    Time: 8 ms (47.39%), Space: 39.9 MB (41.13%) - LeetHub

--- word-break/word-break.java
+++ word-break/word-break.java
@@ -1,24 +1,23 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        
+     
         return wordBreakUtil(s,wordDict);
     }
     
     
-    private boolean wordBreakUtil(String s, List<String> list ){
-        boolean[] check=new boolean[s.length()+1];
-        check[0]=true;
+    private boolean wordBreakUtil(String s, List<String> wordDict ){
+        boolean[] checked=new boolean[s.length()+1];
+        checked[0]=true;
+        
         for(int i=1;i<=s.length();i++){
-            
             
             for(int j=0;j<i;j++){
                 
+                
                 String subString=s.substring(j,i);
-                System.out.println("subString "+subString);
-                if(check[j] && list.contains(subString)){
-                    System.out.println("check found at index "+i+" for subString "+subString);
-                    check[i]=true;
-                    
+                if(checked[j] && wordDict.contains(subString))
+                {
+                    checked[i]=true;
                 }
                 
                 
@@ -27,10 +26,9 @@ class Solution {
         }
         
         
-        return check[s.length()];
+        return checked[s.length()];
+        
         
         
     }
-    
-    
 }
