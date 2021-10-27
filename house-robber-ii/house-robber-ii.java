a8c0f38465f003d075dbb320e066688f2b7c4f3c
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-10-27 11:25:22 +0530

    Time: 0 ms (100.00%), Space: 36.5 MB (65.71%) - LeetHub

--- house-robber-ii/house-robber-ii.java
+++ house-robber-ii/house-robber-ii.java
@@ -1,50 +1,33 @@
 class Solution {
     public int rob(int[] nums) {
         
-        
-        return robUtil(nums);
-        
-    }
-    
-    
-    private int robUtil(int[] nums){
         if(nums.length==1)
             return nums[0];
         Map<Integer,Integer> map=new HashMap();
-        int[] visted=new int[nums.length];
-        int first=nums[0];
-        nums[0]=0;
         
-        int excludeFirst= robWrap(nums, 0,map,visted,false);
-        nums[0]=first;
-        nums[nums.length-1]=0;
-         Map<Integer,Integer> map2=new HashMap();
-        int excludeLast=robWrap(nums,0,map2,visted,false);
-        
-        return Math.max(excludeFirst,excludeLast);
+        int firstIgnore=robUtil(nums,1,1,nums.length,map);
+        map.clear();
+        int lastIgnore=robUtil(nums,0,0,nums.length-1,map);
+        return Math.max(firstIgnore,lastIgnore);
         
         
     }
     
-    
-    private int robWrap(int[] nums,int  index,Map<Integer,Integer> map,int[] visted,boolean revisted){
+    private int robUtil(int[] nums,int index,int start,int end,Map<Integer,Integer> map){
+        if(index>=end)
+            return 0;
         if(map.get(index)!=null)
             return map.get(index);
-        if(index>=nums.length)
-           return 0;
-                  
         
-        int skip=robWrap(nums,(index+1),map,visted,revisted);
-        int taken=nums[index]+robWrap(nums,(index+2),map,visted,revisted);
         
-        int maxValue=Math.max(skip,taken);
-        map.put(index,maxValue);
-        visted[index]=1;
+        int taken=nums[index]+robUtil(nums,index+2,start,end,map);
+        int notTaken=robUtil(nums,index+1,start,end,map);
+        
+        int max=Math.max(taken,notTaken);
+        map.put(index,max);
+        
         return map.get(index);
         
-        
-        
     }
-    
     
 }
