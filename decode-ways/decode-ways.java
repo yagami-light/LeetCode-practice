7f4588e2b39330000d48104abf49c45f2aa974f5
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-10-27 20:27:52 +0530

    Time: 26 ms (5.04%), Space: 39.4 MB (14.87%) - LeetHub

--- decode-ways/decode-ways.java
+++ decode-ways/decode-ways.java
@@ -1,70 +1,47 @@
 class Solution {
-    
-    public int numDecodings(String s){
-        int[] cache=new int[s.length()+1];
+    public int numDecodings(String s) {
         
-        return numDecodingsUtil2(0,s,cache);
+        return numDecodeUtil(s);
     }
     
-    
-    private int numDecodingsUtil2(int p,String s,int[] cache){
-        int n=s.length();
-        if(p==n) return 1;
-        
-        if(s.charAt(p)=='0')
-            return 0;
-        if(cache[p]!=0)
-            return cache[p];
-        
-        int res=numDecodingsUtil2(p+1,s,cache);
-        
-        if(p<n-1 && (s.charAt(p)=='1' || s.charAt(p)=='2' &&  s.charAt(p+1) < '7'))
-            res+=numDecodingsUtil2(p+2,s,cache);
-        
-        cache[p] = res;
-        return cache[p];
-        
-        
-    }
-    
-    
-    
-   /*   int n=s.length();
-        if(p==n) return 1;
-        if(s.charAt(p)=='0') return 0;
-        int res=numDecodings(p+1,s);
-        if(p<n-1&&(s.charAt(p)=='1'||s.charAt(p)=='2'&&s.charAt(p+1)<'7')) 
-			res+=numDecodings(p+2,s);
-        return res;
-    */
-    
-    private  int numDecodingsUtil1(String s) {
-        
-        if(s==null || s.length() ==0)
-            return 0;
-        
+    private int numDecodeUtil(String s){
         int n=s.length();
         int[] dp=new int[n+1];
+        
+        int res=0;
         dp[0]=1;
         dp[1]=s.charAt(0)!='0' ? 1 : 0;
-        for(int i=2;i<=n;i++){
+        for(int i=2;i <= n;i++){
             
-            int first=Integer.valueOf(s.substring(i-1,i));
-            int second=Integer.valueOf(s.substring(i-2,i));
-            
-            if(first>=1 && first<=9){
+            String firstDigit=s.substring(i-1,i);
+            int firstDigitInt=Integer.parseInt(firstDigit);
+            System.out.println("firstDigitInt "+firstDigitInt);
+
+            if(firstDigitInt >=1 && firstDigitInt <=9){
+                System.out.println("firstDigitInt "+firstDigitInt);
+
                 dp[i]+=dp[i-1];
-                
             }
             
-            if(second>=10 && second<=26)
+            String secondDigit=s.substring(i-2,i);
+            int secondDigitInt=Integer.parseInt(secondDigit);
+            
+            System.out.println("secondDigitInt "+secondDigitInt);
+            if(secondDigitInt>=10 && secondDigitInt<=26)
                 dp[i]+=dp[i-2];
+            
+           
+            
+            
             
         }
         
         
-        return dp[n];
+        return dp[s.length()];
+        
+        
         
         
     }
+    
 }
