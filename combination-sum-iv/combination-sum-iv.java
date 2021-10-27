2ff7c1f0e9e15e8fb91652349251b6184adf1223
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-10-27 10:53:53 +0530

    Time: 1 ms (86.49%), Space: 37.1 MB (53.86%) - LeetHub

--- combination-sum-iv/combination-sum-iv.java
+++ combination-sum-iv/combination-sum-iv.java
@@ -1,39 +1,33 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
+        // int[] result=new int[1];
         Map<Integer,Integer> map=new HashMap();
-        
-        
-        return combSum4(nums,target,map);
+        return combinationSum4Util(nums,target,map);
         
     }
     
     
-    private int combSum4(int[] nums,int target,Map<Integer,Integer> map){
-        if(map.get(target)!=null)
-            return map.get(target);
-        if(target==0)
+    private int combinationSum4Util(int[] nums,int target,Map<Integer,Integer> map){
+        // System.out.println("curr SUm "+target);
+        if(target==0){
+           
             return 1;
+        }
         if(target<0)
             return 0;
-        
+        if(map.get(target)!=null) return map.get(target);
+      
         int res=0;
         for(int i=0;i<nums.length;i++){
-            
-            
-            res+=combSum4(nums,target-nums[i],map);
-            
+        
+            res+=combinationSum4Util(nums,target-nums[i],map);
             
         }
-        
         
         map.put(target,res);
         return map.get(target);
         
         
-        
-        
-        
     }
-    
     
 }
