d9b768b01de0b07d5f7f1fbf5ebb3fb94ecb34b4
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-12-07 09:57:33 +0530

    Time: 2 ms (70.00%), Space: 51.9 MB (87.14%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,39 +1,23 @@
 class Solution {
     public int maxProfit(int[] prices) {
-    
-        return maxProfit2(prices);
+        
+        return maxProfitUtil(prices);
     }
     
     
-    private int maxProfit1(int[] prices){
-            int maxProfit=0;
-        for(int i=0;i<prices.length;i++){
-            
-            for(int j=i+1;j<prices.length;j++){
-                
-                maxProfit=Math.max(maxProfit,prices[j]-prices[i]);
-            }
-            
-        }
-        
-        return maxProfit;
-        
-    }
     
-    
-    private int maxProfit2(int[] prices){
-        int n=prices.length;
-        int max=0;
-        int maxProfit=0;
+    private int maxProfitUtil(int[] p){
+        int maxValue=Integer.MIN_VALUE;
+        int maxProfit=Integer.MIN_VALUE;
+        int n=p.length;
+        
         for(int i=n-1;i>=0;i--){
-            
-            max=Math.max(max,prices[i]);
-            maxProfit=Math.max(maxProfit,max-prices[i]);
-            
-            
+            maxValue=Math.max(maxValue,p[i]);
+            maxProfit=Math.max(maxProfit,maxValue-p[i]);
         }
         
+        
         return maxProfit;
+        
     }
-    
 }
