077dbecf3132590439550f785c2f44cbf56ab727
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-12-08 18:59:14 +0530

    Time: 2 ms (55.69%), Space: 45 MB (27.34%) - LeetHub

--- counting-bits/counting-bits.java
+++ counting-bits/counting-bits.java
@@ -1,27 +1,17 @@
 class Solution {
     public int[] countBits(int n) {
         int[] result=new int[n+1];
+        result[0]=0;
+        //result[1]=1;
+        
+        
         for(int i=0;i<=n;i++){
-            result[i]=countBit(i);
+         
+            result[i]=result[i>>1] + (i&1);
         }
+        
         
         return result;
         
-        
     }
-    
-    
-    private int countBit(int n){
-        int result=0;
-        while(n!=0){
-        if((n & 1)==1)
-            result++;
-        n=n>>>1;
-        }
-        return result;
-        
-        
-    }
-    
-    
 }
