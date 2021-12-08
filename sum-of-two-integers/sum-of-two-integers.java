b528624395eaa3d0276a9766ac9ec51e24cb6cf4
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-12-08 17:18:18 +0530

    Time: 0 ms (100.00%), Space: 37.7 MB (17.71%) - LeetHub

--- sum-of-two-integers/sum-of-two-integers.java
+++ sum-of-two-integers/sum-of-two-integers.java
@@ -1,42 +1,22 @@
 class Solution {
     public int getSum(int a, int b) {
-        int and=b;
-        int result=-1;
-        while(and!=0){
-        int xor= a^b;
-         and=a&b;
-        and=and <<1;
         
-        result=and | xor;
-        a=xor;
-            b=and;
-        }
-        return a;
-            
-            
+        /*
+        
+        3 110
+        2 010
+        ------
+        5 101
+        
+        */
+        
+        if(b==0)
+            return a;
+        int carry=a&b;
+        int xor=a^b;
+        
+        return getSum(xor , (carry << 1));
+        
         
     }
 }
-
-
-
-/*
-
-0001
-0010
------
-0011 -XOR
-0000- AND
-0011- result
-
-
-
-0010 -2
-0011 -3
--------    
-0001--XOR
-0010-AND
-0100- left AND
-0101- result
-    
-    */
