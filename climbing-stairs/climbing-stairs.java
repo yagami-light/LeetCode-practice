4f3ddf92208309b276f785d7cb0a28349a9c038f
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-12-08 19:52:05 +0530

    Time: 0 ms (100.00%), Space: 37.7 MB (12.60%) - LeetHub

--- climbing-stairs/climbing-stairs.java
+++ climbing-stairs/climbing-stairs.java
@@ -1,17 +1,22 @@
 class Solution {
-    public int climbStairs(int n) {
+    
+       public  int climbStairs(int n) {
         int[] cache=new int[46];
-        return climbSUtil(n,cache);
+        return climbStairsUtil(n,cache);
+        
+        
     }
     
-    public int climbSUtil(int n,int[] cache){
-        if(cache[n]!=0) return cache[n];
-        if(n==0|| n==1 || n==2)
+    
+    private int climbStairsUtil(int n,int[] cache) {
+        if(cache[n]!=0)
+            return cache[n];
+        
+        if(n==0 || n==1 || n==2)
             return n;
         
-        int val= climbSUtil(n-1,cache)+climbSUtil(n-2,cache);
-        cache[n]=val;
+        cache[n]= climbStairsUtil(n-1,cache)+climbStairsUtil(n-2,cache);
         return cache[n];
+        
     }
-    
 }
