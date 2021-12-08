bfe1649ce7eea27e855bbc16b3a77aeadb158e4b
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-12-08 19:46:51 +0530

    Time: 2 ms (26.10%), Space: 39.8 MB (12.86%) - LeetHub

--- reverse-bits/reverse-bits.java
+++ reverse-bits/reverse-bits.java
@@ -2,7 +2,20 @@ public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
         
-        return Integer.reverse(n);
+        int result=0;
+        int number=n;
         
+        
+        
+        for(int i=0;i<32;i++){
+            result= result << 1;
+            
+            if((number & 1 ) == 1)
+                result  = result | 1;
+            
+            number = number >>> 1;
+        }
+        
+        return result;
     }
 }
