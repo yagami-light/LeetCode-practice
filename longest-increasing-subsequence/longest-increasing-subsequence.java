8d52c9e88fa15d63d7444ef1f639ee9e38f86089
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-12-08 20:11:39 +0530

    Time: 64 ms (31.56%), Space: 38.7 MB (70.53%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.java
+++ longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,27 +1,20 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int dp[]=new int[nums.length];
+        
+        int[] dp=new int[nums.length];
         Arrays.fill(dp,1);
         
         for(int i=1;i<nums.length;i++){
+            
             for(int j=0;j<i;j++){
                 
-                if(nums[j]<nums[i] && (dp[i] <= (dp[j]+1))){
-                    dp[i]=dp[j]+1;
-                }
-                
-                
+                if((nums[j]< nums[i]) &&  (dp[i] < dp[j]+1))
+                    dp[i]= dp[j] +1 ;
             }
         }
         
         
-        
         return Arrays.stream(dp).max().getAsInt();
-        
-        
         
     }
 }
-
-
-//1,2,1,1,1,1
