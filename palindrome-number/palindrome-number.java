3779822a19b7d46fde302ab1e80828dbd2d1f754
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-12-26 18:09:10 +0530

    Time: 51 ms (5.07%), Space: 38.9 MB (29.75%) - LeetHub

--- palindrome-number/palindrome-number.java
+++ palindrome-number/palindrome-number.java
@@ -1,8 +1,10 @@
 class Solution {
     public boolean isPalindrome(int x) {
         
-        String strInt=new String(x+"");
-        return strInt.equals((new StringBuilder(strInt)).reverse().toString());
+        System.out.println((new StringBuilder(x+"").reverse().toString()));
+        System.out.println(x+"");
+        return new String(new StringBuilder(x+"").reverse()).equals(x+"");
+        
         
     }
 }
