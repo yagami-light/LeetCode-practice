aa5e09878f318883a156059fdd36d84c6f55d76f
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-12-27 15:26:14 +0530

    Time: 0 ms (100.00%), Space: 37.1 MB (74.14%) - LeetHub

--- longest-common-prefix/longest-common-prefix.java
+++ longest-common-prefix/longest-common-prefix.java
@@ -1,23 +1,23 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String prefix="";
-        if(strs==null || strs.length == 0)
-            return prefix;
-        prefix=strs[0];
+        
+        if(strs == null || strs.length == 0)
+            return "";
+        
+        
+        String prefix=strs[0];
         int index=1;
+        
         while(index<strs.length){
             
             while(strs[index].indexOf(prefix)!=0)
-            prefix=prefix.substring(0,prefix.length()-1);
-            index++;
-            }
-        
-        
-          return prefix;
+                prefix=prefix.substring(0,prefix.length()-1);
+            
+              index++;
         }
         
       
-        
-        
+        return prefix;
         
     }
+}
