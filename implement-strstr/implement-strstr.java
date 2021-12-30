b84d41f23bf215092a217a84143336e9b308a126
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2021-12-30 09:38:52 +0530

    Time: 1325 ms (32.32%), Space: 39.4 MB (34.73%) - LeetHub

--- implement-strstr/implement-strstr.java
+++ implement-strstr/implement-strstr.java
@@ -1,8 +1,26 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if(needle =="")
-            return 0;
         
-        return haystack.indexOf(needle);
+        int m=haystack.length();
+        int n=needle.length();
+        
+        for(int i=0;;i++){
+            
+            for(int j=0;;j++){
+                
+                if(j==n)
+                    return i;
+                if(i+j == m)
+                    return -1;
+                
+                if(needle.charAt(j) != haystack.charAt(i+j))
+                    break;
+                
+                
+            }
+            
+        }
+        
+        // return -1;
     }
 }
