12fd462af180e368341a305dde6554c19655864e
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-01-16 18:02:47 +0530

    Time: 53 ms (12.10%), Space: 41.1 MB (31.71%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.java
@@ -6,24 +6,23 @@ class Solution {
         int[] cnt=new int[n];
         int maxLen=1;
         int result=0;
-        // // Arrays.fill(len,1);
+        // Arrays.fill(len,1);
         // Arrays.fill(cnt,1);
-//         
+        len[0]=len[0]=1;
         for(int i=0;i<n;i++){
-            len[i] = cnt[i] = 1; 
+            len[i]=cnt[i]=1;
             
             for(int j=0;j<i;j++){
                 
                 
                 if(nums[i]> nums[j]){
-                     if(len[i]==len[j]+1){
+                    if(len[i]==len[j]+1){
                         cnt[i]+=cnt[j];
                     }
                     if(len[i]<len[j]+1){
                         len[i]=len[j]+1;
                         cnt[i]=cnt[j];
                     }
-                   
                     
                 }
                 // System.out.println("len is "+len[i]);
@@ -31,11 +30,12 @@ class Solution {
                 
             }
             
+            
             if(maxLen==len[i])
                 result+=cnt[i];
-            if(maxLen<len[i]){
-                result=cnt[i];
-                maxLen=len[i];
+            if(maxLen < len[i]){
+                maxLen = len[i];
+                result = cnt[i];
             }
             
             
