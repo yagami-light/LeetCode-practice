e86ac3e0e3391c5ab2fc1bcf42d6addfb1eff9a2
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-01-17 11:55:19 +0530

    Time: 63 ms (31.03%), Space: 39.7 MB (53.85%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,27 +1,48 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int sum=0;
+        int n=nums.length;
         for(int c: nums)
             sum+=c;
         
-        if((sum & 1)==1)
+        if(sum % 2 != 0)
             return false;
         
-        
         sum/=2;
-        int n=nums.length;
+        
         boolean[][] dp=new boolean[n+1][sum+1];
         
-        for(int i=0;i<=sum;i++){
-            
+         
+         for(int i=0;i<=sum;i++){
             dp[0][i]=false;
-            
         }
-        for(int i=0;i<=n;i++)
+        for(int i=0;i<=n;i++){
             dp[i][0]=true;
+        }
+       
+        
+         // dp[0][0]=true;
         
         
-        for(int i=1;i<=n;i++){
+//         for(int i=1;i<=n;i++){
+            
+//             for(int j=1;j<=sum;j++){
+                
+//                   dp[i][j]=dp[i-1][j];   
+//                 if(j>=nums[i-1])
+//                 // dp[i][j]= (dp[i][j])|| (dp[i-1][j-nums[i-1]])  ;
+//                 dp[i][j]= (dp[i][j]) || (dp[i-1][j-nums[i-1]]);
+
+                    
+               
+                
+                
+//             }
+            
+//         }
+        
+        
+          for(int i=1;i<=n;i++){
             
             for(int j=1;j<=sum;j++){
                 
@@ -38,8 +59,8 @@ class Solution {
         }
         
         
-        return dp[n][sum];
         
+        return dp[n][sum];
         
     }
 }
