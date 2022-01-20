25fe1b1d0e79b1e8d88de2a53e2fa3b3ca2c8596
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-01-20 19:26:09 +0530

    Time: 20 ms (61.64%), Space: 38.7 MB (63.89%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.java
@@ -1,54 +1,62 @@
 class Solution {
     public int findNumberOfLIS(int[] nums) {
-        
         int n=nums.length;
+        int[] cnt=new int[n]; 
         int[] len=new int[n];
-        int[] cnt=new int[n];
+        int res=0;
         int maxLen=1;
-        int result=0;
-        // Arrays.fill(len,1);
-        // Arrays.fill(cnt,1);
-        len[0]=len[0]=1;
+        
+        
         for(int i=0;i<n;i++){
-            len[i]=cnt[i]=1;
+            
+            cnt[i]=len[i]=1;
             
             for(int j=0;j<i;j++){
                 
-                
-                if(nums[i]> nums[j]){
-                    if(len[i]==len[j]+1){
-                        cnt[i]+=cnt[j];
-                    }
-                    if(len[i]<len[j]+1){
-                        len[i]=len[j]+1;
-                        cnt[i]=cnt[j];
-                    }
+                if(nums[i]>nums[j]){
+                if(len[i]==len[j]+1){
+                    cnt[i]+=cnt[j];
                     
                 }
-                // System.out.println("len is "+len[i]);
-                // maxLen=Math.max(maxLen,len[i]);
+                
+                
+                if(len[i]<len[j]+1){
+                    
+                    len[i]=len[j]+1;
+                    cnt[i]=cnt[j];
+                    
+                }
+                    
+                }
+            }
+            
+            
+            // maxLen=Math.max(maxLen,len[i]);
+            if(len[i]==maxLen){
+                
+                res+=cnt[i];
                 
             }
-            
-            
-            if(maxLen==len[i])
-                result+=cnt[i];
-            if(maxLen < len[i]){
-                maxLen = len[i];
-                result = cnt[i];
+            if(len[i]>maxLen){
+                
+                res=cnt[i];
+                maxLen=len[i];
             }
+            
+            
+            
+            
             
             
         }
         
         
-//         for(int i=0;i<n;i++){
-//             if(len[i]==maxLen)
-//                 result+=cnt[i];
-//         }
+        return res;
         
         
         
-        return result;
+        
+        
+        
     }
 }
