09b5aa78ae0c2320750062f274b4bb187d0dd78b
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-01-27 20:57:45 +0530

    Time: 1 ms (88.85%), Space: 43.1 MB (5.18%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,48 +1,21 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-       int  n =cost.length; 
-       int[] dp=new int[n+1];
-       dp[n]=0;
-       dp[n-1]=cost[n-1];
+        int n=cost.length;
+        int[] dp=new int[n+1];
         
-       for(int i=n-2;i>=0;i--){
-           
-           dp[i]=cost[i]+Math.min(dp[i+1],dp[i+2]);
-           
-           
-           
-       } 
+        for(int i=0;i<n;i++){
+            
+            if(i<2)
+                dp[i]=cost[i];
+            
+            else
+                dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
+            
+            
+        }
         
-        return Math.min(dp[0],dp[1]);
-        
-        
-        
+        return Math.min(dp[n-2],dp[n-1]);
         
         
     }
-    
-    
-    private int minCostClimbingStairsRec(int[] cost){
-         int dp[]=new int[cost.length];
-        
-      
-        
-        return Math.min(minCostClimbingStairsUtil(cost,0,0),minCostClimbingStairsUtil(cost,1,0));
-        
-    }
-    
-    
-    
-    private int minCostClimbingStairsUtil(int[] cost,int index,int currCost){
-      if(index>cost.length-1)
-          return 0;
-        
-        int skip=minCostClimbingStairsUtil(cost,index+2,currCost);
-        int notSkip=minCostClimbingStairsUtil(cost,index+1,currCost+cost[index]);
-        
-        return cost[index]+Math.min(skip,notSkip);
-        
-        
-    }
-    
 }
