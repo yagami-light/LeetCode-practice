9f5687583459626fd350cb9ea6f86ae95914e445
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-01-27 19:58:07 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (5.05%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,13 +1,30 @@
 class Solution {
     public boolean divisorGame(int n) {
         
-        if(n==1)
-            return false;
+//         if(n==1)
+//             return false;
         
-        if(n==2)
-            return true;
+//         if(n==2)
+//             return true;
         
-        return divisorGame(n-2);
+//         return divisorGame(n-2);
+        
+        boolean[] dp=new boolean[n+1];
+        for(int i=0;i<=n;i++){
+            
+            if(i==1 || i==0)
+                dp[i]=false;
+            else if(i==2)
+                dp[i]=true;
+            else
+            dp[i]=dp[i-2];
+            
+            
+        }
+        
+        return dp[n];
+        
+        
         
         
     }
