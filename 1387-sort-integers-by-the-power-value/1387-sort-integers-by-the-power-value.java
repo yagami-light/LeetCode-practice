e46e8a3e14d1dc0c392637ba83fb379734e0af7a
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-01-29 11:10:41 +0530

    Time: 1218 ms (5.06%), Space: 42.1 MB (46.77%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.java
@@ -0,0 +1,74 @@
+class Solution {
+    class Node{
+        int val;
+        int powerVal;
+        
+        Node(int val,int powerVal){
+            this.val=val;
+            this.powerVal=powerVal;
+        }
+        
+        public String toString(){
+            return ("val "+val +" powerVal "+powerVal);
+        }
+        
+    }
+    public int getKth(int lo, int hi, int k) {
+        Map<Integer,Integer> cache=new HashMap();
+        PriorityQueue<Node> pq=new PriorityQueue<>(((Node a,Node b)->(Integer.compare(a.powerVal,b.powerVal)==0 ? Integer.compare(a.val,b.val): Integer.compare(a.powerVal,b.powerVal))));
+        
+        for(int i=lo;i<=hi;i++){
+            
+            pq.add(new Node(i,getPowerValue(i,cache)));
+            
+        }
+        // System.out.println(pq);
+        int count=0;
+        while(count<k){
+            Node node=pq.remove();
+            if(k-1==count)
+                return node.val;
+            count++;
+        }
+        
+        return -1;
+        
+    }
+    
+    
+    private int getPowerValue(final int n,Map<Integer,Integer> cache){
+        int count=0;
+        int num=n;
+        // // System.out.println("intput n "+n);
+        // System.out.println("cache "+cache);
+        if(cache.get(num)!=null)
+            return cache.get(num);
+        
+        while(num!=1){
+            if(num % 2 != 0){
+                num=(3 * num)+1;
+                 count++;
+                int temp=getPowerValue(num,cache);
+                cache.put(num,temp);
+               
+                
+            }else{
+                
+                num/=2;
+                count++;
+                int temp=getPowerValue(num,cache);
+                cache.put(num,temp);
+                //count+=1+temp;
+            }
+            
+            
+            
+        }
+        cache.put(n,count);
+        return count;
+        
+        
+        
+    }
+    
+}
