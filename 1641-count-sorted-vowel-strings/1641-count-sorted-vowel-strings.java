8fdadb338e0e06ff46b3b7359f6dae1a7fa39d72
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-01-29 10:28:11 +0530

    Time: 1 ms (35.95%), Space: 40.7 MB (5.32%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,14 +1,14 @@
 class Solution {
     public int countVowelStrings(int n) {
-        int[] dp=new int[6];
+        int[] dp=new int[5];
         Arrays.fill(dp,1);
-        dp[5]=0;
+        // dp[5]=0;
         
         for(int i=0;i<n;i++){
             // System.out.println(Arrays.toString(dp));
 
             int sum=0;
-            for(int j=dp.length-2;j>=0;j--){
+            for(int j=dp.length-1;j>=0;j--){
                 
                 sum=sum+dp[j];
                 dp[j]=sum;
