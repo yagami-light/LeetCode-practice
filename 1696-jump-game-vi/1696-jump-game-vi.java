b8344e1825f601f1bf173a7ecc86fcb5c30614db
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-01 16:58:03 +0530

    Time: 65 ms (31.14%), Space: 114 MB (36.64%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        
+        Deque<Integer> dq=new LinkedList();
+        int n=nums.length;
+        dq.add(0);
+        
+        for(int i=1;i<n;i++){
+            
+            nums[i]=nums[i]+nums[dq.peek()];
+          
+            
+            while(!dq.isEmpty() && nums[dq.peekLast()] <=nums[i]){
+                
+                dq.removeLast();
+                
+                
+            }
+            
+            dq.add(i);
+            
+            if(i-dq.peekFirst() >= k)
+                dq.remove();
+            
+            
+            
+            
+            
+        }
+        return nums[n-1];
+        
+        
+        
+    }
+}
