ca2b56e58d3b8ef1d4612f8111ce0876549d233e
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-03 16:33:20 +0530

    Time: 1 ms (8.43%), Space: 41.7 MB (5.69%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,22 +1,21 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<=2)
-            return n;
+        
+//         if(n==0 || n==1|| n==2)
+//             return n;
+        
+//         return climbStairs(n-1)+climbStairs(n-2);
+        
         
         int[] dp=new int[n+1];
-        
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
-        
-        for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-            
+        for(int i=0;i<=n;i++){
+            if(i<=2)
+                dp[i]=i;
+            else
+                dp[i]=dp[i-2]+dp[i-1];
         }
-        
-        
+        System.out.println(Arrays.toString(dp));
         return dp[n];
-        
         
         
     }
