ba558e9f381bda961f80ddc3340307600c5da8da
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-03 18:12:56 +0530

    Time: 23 ms (39.33%), Space: 45.8 MB (15.39%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,31 +1,49 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
+        int m=text1.length();
+        int n=text2.length();
+        int[][] dp=new int[m+1][n+1];
         
-        int l1=text1.length();
-        int l2=text2.length();
         
-        int[][] dp=new int[l1+1][l2+1];
-        
-        for(int i=1;i<=l1;i++){
+        for(int i=0;i<=m;i++){
             
-            for(int j=1;j<=l2;j++){
+            
+            for(int j=0;j<=n;j++){
                 
-                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                if(i==0 || j==0)
+                    dp[i][j]=0;
+                else if(text1.charAt(i-1) == text2.charAt(j-1))
                     dp[i][j]=1+dp[i-1][j-1];
-
-                }else{
-                    
-                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
-                    
-                }
+                else
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                
+                
+                
                 
             }
+            
+            
         }
         
+        return dp[m][n];
         
-        return dp[l1][l2];
         
         
         
     }
 }
+
+/*
+
+Input: text1 = 
+"abcde", text2 = 
+"ace" 
+ a  c   e
+a
+b
+c
+d
+e
+
+
+*/
