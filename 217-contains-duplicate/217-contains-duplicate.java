07b2dd041193fc704927e7534a0beb9ab60b857c
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-03 12:55:24 +0530

    Time: 97 ms (5.53%), Space: 69.4 MB (38.86%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set=new HashSet();
+       Set<Integer> set=Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        System.out.println(set);
+        System.out.println(nums.length);
+        System.out.println(set.size());
+        return !(nums.length == set.size());
         
-        for(int n:nums){
-            if(set.contains(n)) return true;
-            set.add(n);
-        }
-        
-        return false;
     }
 }
