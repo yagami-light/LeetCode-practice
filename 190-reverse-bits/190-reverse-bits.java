b5654f1851b0fcf11a83887d7293418ac002b546
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-03 16:21:38 +0530

    Time: 1 ms (96.73%), Space: 42.8 MB (5.32%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -3,16 +3,17 @@ public class Solution {
     public int reverseBits(int n) {
         
         int result=0;
+        
         for(int i=0;i<32;i++){
-          
-            result=result <<1;
-            if((n & 1 )==1)
-            result|=1;
+            result=result<<1;
+            
+            if((n & 1)==1){
+                result|=1;
+            }
+            
             n=n>>>1;
-           
             
         }
-        
         return result;
         
     }
