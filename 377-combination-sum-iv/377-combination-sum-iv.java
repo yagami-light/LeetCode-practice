e1e1d665c7a10ef087c1d9cdc8bef1e1363eb310
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-03 21:56:48 +0530

    Time: 1 ms (88.93%), Space: 41.3 MB (7.28%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,31 +1,43 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
         
-        Map<Integer,Integer> cache=new HashMap();
-        return combinationSum4Util(nums,target,cache);
+        
+        int[] cache=new int[1001];
+        Arrays.fill(cache,-1);
+        
+        return combSum4Util(nums,target,0,cache,0);
+        // return count[0];
         
     }
     
     
-    private int combinationSum4Util(int[] nums, int target, Map<Integer,Integer> cache){
-        if(target==0)
-            return 1;
-        if(target<0)
+    private int combSum4Util(int[] nums,int target,int currSum,int cache[],int index){
+        if(index>= nums.length)
             return 0;
-        if(cache.get(target)!=null)
-            return cache.get(target);
-        int res=0;
-        for(int i:nums){
-            
-            res+=combinationSum4Util(nums,target-i,cache);
-            
-            
+        // System.out.println(currSum);
+        int count=0;
+        if(target == 0){
+           return 1;
         }
         
+       
         
-        cache.put(target,res);
-        return cache.get(target);
+        if(target < 0)
+            return 0;
+        
+         if(cache[target]!=-1)
+            return cache[target];
+        
+       for(int i=0;i<nums.length;i++){
+            
+            count+=combSum4Util(nums,target-nums[i],currSum,cache,i);
+        }
+        
+        cache[target]= count;
+        return cache[target];
+        
         
     }
+    
     
 }
