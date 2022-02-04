e2aed6e7f24d805fbd450727fced842c3a49eaab
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-04 11:01:17 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (5.67%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,23 +1,24 @@
 class Solution {
     public int rob(int[] nums) {
-        Map<Integer,Integer> cache=new HashMap();
+        int[] cache=new int[nums.length];
+        Arrays.fill(cache,-1);
         return robUtil(nums,0,cache);
     }
     
     
-    private int robUtil(int[] nums,int index,Map<Integer,Integer> cache){
-        if(index>= nums.length)
+    private int robUtil(int[] nums, int index,int[] cache){
+        if(index>=nums.length)
             return 0;
         
+        if(cache[index]!=-1)
+            return cache[index];
         
-        if(cache.get(index)!=null)
-            return cache.get(index);
         int skipCurrent=robUtil(nums,index+1,cache);
-        
         int takeCurrent=nums[index]+robUtil(nums,index+2,cache);
         
-        cache.put(index, Math.max(skipCurrent,takeCurrent));
-        return cache.get(index);
+        int maxAmount=Math.max(skipCurrent,takeCurrent);
+        cache[index]=maxAmount;
+        return maxAmount;
         
         
     }
