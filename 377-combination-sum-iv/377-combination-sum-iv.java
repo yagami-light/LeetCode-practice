1264f6535a9bb5fe54b45f6d268368054abe3201
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-04 10:54:24 +0530

    Time: 2 ms (52.07%), Space: 41 MB (9.55%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -2,13 +2,40 @@ class Solution {
     public int combinationSum4(int[] nums, int target) {
         
         
-        int[] cache=new int[1001];
-        Arrays.fill(cache,-1);
+//         int[] cache=new int[1001];
+//         Arrays.fill(cache,-1);
         
-        return combSum4Util(nums,target,0,cache,0);
-        // return count[0];
+//         return combSum4Util(nums,target,0,cache,0);
+//         // return count[0];
+        
+        
+        return combSum4(nums,target);
         
     }
+    
+    private int combSum4(int[] nums,int target){
+        int[] dp=new int[target+1];
+        dp[0]=1;
+        for(int i=1;i<dp.length;i++){
+            
+            for(int j=0;j<nums.length;j++){
+                // System.out.println("woow");
+
+                if(i-nums[j]>=0)
+                    
+                    dp[i]+=dp[i-nums[j]];
+                
+            }
+            
+            
+            
+        }
+        
+        return dp[target];
+        
+    }
+    
+    
     
     
     private int combSum4Util(int[] nums,int target,int currSum,int cache[],int index){
