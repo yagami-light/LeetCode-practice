5b592328a452a93fba896920d97246ba89449921
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-04 12:42:35 +0530

    Time: 1 ms (41.01%), Space: 41.8 MB (5.78%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,7 +1,9 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        Map<Pair<Integer,Integer>,Integer> cache=new HashMap();
-        return unqieuePath(m,n,0,0,cache);
+        // Map<Pair<Integer,Integer>,Integer> cache=new HashMap();
+        // return unqieuePath(m,n,0,0,cache);
+        
+        return unqieuePathDp(m,n); 
     }
     
     
@@ -20,6 +22,38 @@ class Solution {
         
         cache.put(new Pair(i,j),count);
         return count;
+        
+        
+        
+    }
+    
+    
+    private int unqieuePathDp(int m ,int n){
+        
+        int[][] dp=new int[m][n];
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            
+            
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0)
+                    dp[i][j]=1;
+                else if(j==0)
+                    dp[i][j]=dp[i-1][j];
+                else if(i==0)
+                    dp[i][j]=dp[i][j-1];
+                else
+                    
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                
+                
+            }
+            
+        }
+        
+        return dp[m-1][n-1];
+        
+        
         
         
         
