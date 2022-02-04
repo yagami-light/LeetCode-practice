8ae267d582f41a094d88a264cdfbed93cede0608
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-04 12:26:24 +0530

    Time: 2 ms (57.94%), Space: 42.4 MB (7.20%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,43 +1,34 @@
 class Solution {
     public int numDecodings(String s) {
-        
-        return numDecodingsUtil(s);
-    }
-    
-    
-    private int numDecodingsUtil(String s){
         int n=s.length();
-        int dp[]=new int[n+1];
-        
-        int res=0;
+        int[] dp=new int[n+1];
+     
         dp[0]=1;
         dp[1]=s.charAt(0)!='0' ? 1 : 0;
+        
+        
         for(int i=2;i<=n;i++){
             
-            String firstDigit=s.substring(i-1,i);
-            int firstDigitInt=Integer.parseInt(firstDigit);
-            
-            if(firstDigitInt >=1 && firstDigitInt <=9)
+            String singleDigit=s.substring(i-1,i);
+            // System.out.println(singleDigit);
+            int sinleDigitInt=Integer.parseInt(singleDigit);
+            if(sinleDigitInt >=1 && sinleDigitInt<=9)
                 dp[i]+=dp[i-1];
             
-            String secondDigit=s.substring(i-2,i);
-            int secondDigitInt=Integer.parseInt(secondDigit);
-            
-            if(secondDigitInt>=10 && secondDigitInt <=26)
+            String doubleDigit=s.substring(i-2,i);
+            // System.out.println(doubleDigit);
+            int doubleDigitInt=Integer.parseInt(doubleDigit);
+            if(doubleDigitInt>=10 && doubleDigitInt<=26)
                 dp[i]+=dp[i-2];
             
             
             
         }
         
-        
-        
         return dp[n];
         
         
-        
-        
-        
+        // return 1;
         
     }
 }
