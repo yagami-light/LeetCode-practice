718cece7f1cccd25be5f33470d235c88792ef55d
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-05 11:03:20 +0530

    Time: 446 ms (5.02%), Space: 164.8 MB (5.33%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -11,31 +11,63 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         
-        PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->Integer.compare(a.val,b.val));
         
-        for(ListNode node: lists)
-            if(node!=null)
-            pq.add(node);
-        
-        ListNode head=new ListNode();
-        ListNode dummy=head;
-        while(!pq.isEmpty()){
+        ListNode result=new ListNode(Integer.MIN_VALUE);
+      
+        for(ListNode node:lists){
             
-            ListNode node=pq.remove();
-            
-            dummy.next=node;
-            dummy=dummy.next;
-            
-            if(node.next!=null)
-                pq.add(node.next);
+            result=merge2List(result,node);
             
             
         }
         
-        return head.next;
+        
+        return result.next;
         
         
         
         
     }
+    
+    private ListNode merge2List(ListNode n1,ListNode n2){
+           ListNode newHead=new ListNode();
+            ListNode dummy=newHead;
+        if(n1==null && n2==null)
+            return  null;
+        
+        else if(n1==null && n2!=null)
+            return n2;
+        else if(n2==null && n1!=null)
+            return n1;
+        else{
+            
+         
+            
+            if(n1.val <n2.val){
+                
+                dummy.next=n1;
+                dummy=n1;
+                dummy.next=merge2List(n1.next,n2);
+                
+                
+            }else{
+                
+                dummy.next=n2;
+                dummy=n2;
+                dummy.next=merge2List(n1,n2.next);
+                
+                
+            }
+            
+            
+            
+            
+        }
+        
+        return newHead.next;
+        
+        
+    }
+    
+    
 }
