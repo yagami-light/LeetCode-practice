4b69fe8098b602d8e271d0a00aab64154c209afc
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-07 11:16:02 +0530

    Time: 3 ms (73.06%), Space: 44.3 MB (14.25%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,37 +14,32 @@
  * }
  */
 class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
+    public TreeNode buildTree(int[] pre, int[] in) {
         
         Map<Integer,Integer> map=new HashMap();
-        
-        for(int i=0;i<inorder.length;i++){
-            map.put(inorder[i],i);
+        for(int i=0;i<in.length;i++){
+            map.put(in[i],i);
         }
         
         
-        return buildTree(preorder,inorder,0,inorder.length-1,0,map);
+        return buildTreeUtil(pre,in,0,in.length-1,0,map);
         
     }
     
     
-    private TreeNode buildTree(int[] preorder,int[] inorder,int inIndexStart,int inIndexEnd,int preIndex,Map<Integer,Integer> map){
+    private TreeNode buildTreeUtil(int[] pre,int[] in,int inStart,int inEnd,int preStart,Map<Integer,Integer> map){
         
-        if(preIndex>=preorder.length || inIndexStart > inIndexEnd)
+        if(inStart> inEnd && preStart>inEnd)
             return null;
         
+        int inCalc=map.get(pre[preStart]);
         
-        TreeNode node=new TreeNode(preorder[preIndex]);
-        int inIndexEndCalc=map.get(node.val);
+        TreeNode node=new TreeNode(pre[preStart]);
         
-        node.left=buildTree(preorder,inorder,inIndexStart,inIndexEndCalc-1,preIndex+1,map);
-        node.right=buildTree(preorder,inorder,inIndexEndCalc+1,inIndexEnd,preIndex+1+(inIndexEndCalc-inIndexStart),map);
+        node.left=buildTreeUtil(pre,in,inStart,inCalc-1,preStart+1,map);
+        node.right=buildTreeUtil(pre,in,inCalc+1,inEnd,preStart+inCalc-inStart+1 ,map);
         
         return node;
         
-        
-        
-        
     }
-    
 }
