88817155eac9ad9a7f11d21176847f9ef8ad469f
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-09 11:22:46 +0530

    Time: 4 ms (30.48%), Space: 42.5 MB (30.23%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        
+        Map<Integer,Integer> map=new HashMap();
+        
+        for(int n:nums)
+            map.merge(n,1,Integer::sum);
+        
+        int[] res=new int[2];
+        int count=0;
+        for(int n:map.keySet()){
+            
+            if(map.get(n)==1)
+                res[count++]=n;
+            
+        }
+        
+        return res;
+        
+        
+    }
+}
