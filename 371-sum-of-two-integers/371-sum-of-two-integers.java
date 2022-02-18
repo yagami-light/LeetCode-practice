6635938e336a263ae365fc7a0e081565116a780f
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-18 18:36:18 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (33.01%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.java
+++ 371-sum-of-two-integers/371-sum-of-two-integers.java
@@ -4,11 +4,34 @@ class Solution {
         if(b==0)
             return a;
         
-        int and= a& b;
-        int xor= a^b;
+        
+        int and=a & b;
+        int xor=a ^ b;
         
         return getSum(xor,and<<1);
         
         
+        
+        
     }
 }
+
+/*
+
+101 3
+010 2
+
+----
+(a&b) | (a1&b1)
+
+
+
+101
+
+
+000 AND
+111 
+
+
+
+*/
