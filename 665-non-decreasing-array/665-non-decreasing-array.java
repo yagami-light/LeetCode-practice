fd5926bdfaacfbd8f6f3f2b499cea51502473ea1
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-21 10:55:14 +0530

    Time: 1 ms (82.15%), Space: 54.4 MB (8.97%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -2,55 +2,29 @@ class Solution {
     public boolean checkPossibility(int[] nums) {
         
         
-//         int cnt=0;
+        int cnt=0;
         
         
-//         for(int i=1;i<nums.length&&cnt<=1;i++){
+        for(int i=1;i<nums.length&&cnt<=1;i++){
             
-//             if(nums[i-1]>nums[i-1]){
-//                 cnt++;
-                
-                
-//                 if(i-2<0 || nums[i-2] <= nums[i])
-//                         nums[i-1]=nums[i];
-//                 else
-//                     nums[i]=nums[i-1];
-                
-                
-//             }
-            
-            
-            
-            
-//         }
-        
-//         return  cnt<=1;
-        
-        
-        
-        
-            int cnt = 0;                                                                    //the number of changes
-        for(int i = 1; i < nums.length && cnt<=1 ; i++){
-            if(nums[i-1] > nums[i]){
+            if(nums[i-1]>nums[i]){
                 cnt++;
-                if(i-2<0 || nums[i-2] <= nums[i])nums[i-1] = nums[i];                    //modify nums[i-1] of a priority
-                else nums[i] = nums[i-1];                                                //have to modify nums[i]
+                
+                
+                if(i<2 || nums[i-2] <= nums[i])
+                        nums[i-1]=nums[i];
+                else
+                    nums[i]=nums[i-1];
+                
+                
             }
+            
+            
+            
+            
         }
-        return cnt<=1; 
+        
+        return  cnt<=1;
+        
     }
-    
-    
-     public boolean checkPossibility1(int[] nums) {
-        int cnt = 0;                                                                    //the number of changes
-        for(int i = 1; i < nums.length && cnt<=1 ; i++){
-            if(nums[i-1] > nums[i]){
-                cnt++;
-                if(i-2<0 || nums[i-2] <= nums[i])nums[i-1] = nums[i];                    //modify nums[i-1] of a priority
-                else nums[i] = nums[i-1];                                                //have to modify nums[i]
-            }
-        }
-        return cnt<=1; 
-    }
-    
 }
