8795397d171469bad8bcc0b3985bf965f7601092
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-24 17:00:37 +0530

    Time: 23 ms (5.22%), Space: 43 MB (8.93%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,12 +1,65 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int index=0;
-        for(int n:nums){
-            if(n==target)
-                return index;
-            index++;
+        
+        int pivot=searchPivot(nums,0,nums.length-1,target);
+        
+        System.out.println("pivot is "+pivot);
+        if(nums[pivot]==target)
+            return pivot;
+        int leftHalf=searchBinUtil(nums,0,pivot,target);
+        if(leftHalf!=-1)
+            return leftHalf;
+            else
+                return searchBinUtil(nums,pivot,nums.length-1,target);
+        
+        
+        // return -1;
+    }
+    
+    
+    private int searchPivot(int[] nums,int start,int end, int target){
+        /*
+        
+       4 1,,6
+        
+        */
+        System.out.println("start "+start+" end "+end);
+        int mid=(start+end)/2;
+        System.out.println("mid "+mid);
+        if(nums[mid]>nums[end])
+            return searchPivot(nums,mid+1,end,target);
+        else if(nums[mid]<nums[start])
+            return searchPivot(nums,start,mid,target);
+        
+        
+        
+        
+        return start;
+    }
+    
+    private int searchBinUtil(int[] nums, int start, int end,int target){
+        if(start>end)
+            return -1;
+        
+        
+        int mid=(start+end)/2;
+        if(nums[mid]==target)
+            return mid;
+        if(target<nums[mid]){
+            return searchBinUtil(nums,start,mid-1,target);
+        }else{
+            return searchBinUtil(nums,mid+1,end,target);
         }
-        return -1;
         
     }
+    
+    
+    
 }
+
+/*
+
+
+
+
+*/
