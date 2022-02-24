ae4288c3e52c62090f24ea4b7eeeb8b9ed5f1843
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-24 17:21:02 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (36.23%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,65 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-        int pivot=searchPivot(nums,0,nums.length-1,target);
+        return searchUtil(nums,0,nums.length-1,target);
         
-        System.out.println("pivot is "+pivot);
-        if(nums[pivot]==target)
-            return pivot;
-        int leftHalf=searchBinUtil(nums,0,nums.length-1,target,pivot);
-       
-            return leftHalf;
-         
         
-        // return -1;
     }
     
-    
-    private int searchPivot(int[] nums,int start,int end, int target){
-        /*
-        
-       4 1,,6
-        
-        */
-        System.out.println("start "+start+" end "+end);
-        int mid=(start+end)/2;
-        System.out.println("mid "+mid);
-        if(nums[mid]>nums[end])
-            return searchPivot(nums,mid+1,end,target);
-        else if(nums[mid]<nums[start])
-            return searchPivot(nums,start,mid,target);
-        
-        
-        
-        
-        return start;
-    }
-    
-    private int searchBinUtil(int[] nums, int start, int end,int target,int pivot){
+    private int searchUtil(int[] nums, int start, int end,int target){
         if(start>end)
             return -1;
         
         
         int mid=(start+end)/2;
         
-        int roateMid=(mid+pivot)%nums.length;
-        if(nums[roateMid]==target)
-            return roateMid;
-        if(target<nums[roateMid]){
-            return searchBinUtil(nums,start,mid-1,target,pivot);
-        }else{
-            return searchBinUtil(nums,mid+1,end,target,pivot);
-        }
+        int num=(nums[mid] < nums[0] == target <nums[0]) ?
+            
+                nums[mid] :
+                target < nums[0] ?  Integer.MIN_VALUE : Integer.MAX_VALUE;
+        if(num<target){
+            return searchUtil(nums,mid+1,end,target);
+        }else if(num>target)
+            return searchUtil(nums,start,mid-1,target);
+        
+        return mid;
         
     }
     
     
-    
 }
-
-/*
-
-
-
-
-*/
