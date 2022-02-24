d76959f32c393494f8fca0732444d93904c44465
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-24 18:01:53 +0530

    Time: 6 ms (5.03%), Space: 43 MB (10.26%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,61 +1,24 @@
 class Solution {
     public boolean searchMatrix(int[][] mat, int target) {
-     
-        /*
-        00 01 02 03
-        10 11 12 13
-        20 21 22 23
-        30 31 33 33
+        int i=0,j=mat[0].length-1;
+        
+        
+        while( i< mat.length && j>=0){
             
-        0 1 2 3
-        4 5 6 7
-        8 9 10 11
-       12 13 14 15
+            System.out.println("test");
+               if(mat[i][j]==target)
+                return true;
+            if(mat[i][j]<target)
+                i++;
+            else
+                j--;
+         
             
             
-        index = i*rowLength+j    
             
-        i=index/rowLength, j=index%rowlength
-        
-        
-        */
-        
-        
-        int rowLen=mat[0].length;
-        int colLen=mat.length;
-        
-        
-        
-        return searchMatUtil(mat,0,(rowLen*colLen)-1,target,rowLen,colLen);
-        
+        }
+        return false;
         
         
     }
-    
-    private boolean searchMatUtil(int[][] mat,int start, int end, int target,int rowLen,int colLen){
-        
-        
-        if(start>end)
-            return false;
-        
-        int mid=(start+end)/2;
-        
-        
-        
-        if(mat[mid/rowLen][mid%rowLen]==target)
-            return true;
-        
-       if(mat[mid/rowLen][mid%rowLen] <target)
-          return searchMatUtil(mat,mid+1,end,target,rowLen,colLen);
-           else 
-               
-           return    searchMatUtil(mat,start,mid-1,target,rowLen,colLen);
-               
-            
-        
-        
-        
-    }
-    
-    
 }
