05191dacffb0b17052830d124c2df239d8b9dc23
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-25 13:52:24 +0530

    Time: 12 ms (10.14%), Space: 43.7 MB (6.52%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,15 +1,35 @@
 class Solution {
     public int findPeakElement(int[] nums) {
+        if(nums.length==1)
+            return 0;
+        return findPeakBin(nums,0,nums.length-1);
+    }
+    
+    
+    private int findPeakBin(int[] nums,int start,int end){
+       
+        System.out.println("start "+start+" end "+end);
         
-        int n=nums.length;
-        int res=0;
-        for(int i=0;i<nums.length;i++){
-            
-            res=nums[res]==Math.max(nums[res],nums[i]) ? res: i;
-            
-        }
+        if(start>end)
+            return -1;
         
-        return res;
+        int mid=(start+end)/2;
+        
+        if(0<mid && mid<nums.length-1 && nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]|| (mid==0 && mid<nums.length-1 && nums[mid]>nums[mid+1] ) || (mid>0 && mid==nums.length-1 && nums[mid]>nums[mid-1] ))
+            return mid;
+        
+        else if(mid==0 || nums[mid-1]<nums[mid]){
+            
+                return findPeakBin(nums,mid+1,end);
+            
+        }else 
+            
+            return findPeakBin(nums,start,mid-1);
+            
+            
+        
+        
         
     }
+    
 }
