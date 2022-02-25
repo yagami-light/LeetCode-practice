716c9bdc3a7c25c118cdaee962ca8e3dab6ed94f
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-25 14:21:11 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (24.40%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,26 +1,14 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         
-        return findPeakUtil(nums,0,nums.length-1);
-    }
-    
-    private int findPeakUtil(int[] nums, int start, int end){
+        int n=nums.length;
         
-        if(start==end)
-            return start;
-
-            // return -1;
+        for(int i=1;i<n;i++){
+            if(nums[i-1]>nums[i])
+                return i-1;
+        }
         
-        
-        int mid1=(start+end)/2;
-        int mid2=mid1+1;
-        
-        if(nums[mid1]>nums[mid2]){
-            return findPeakUtil(nums,start,mid1);
-        }else
-            return findPeakUtil(nums,mid2,end);
-        
+        return  n-1;
         
     }
-    
 }
