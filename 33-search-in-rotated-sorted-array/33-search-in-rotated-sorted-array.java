8ed564aac1ea33478a51a51e0a2849ae3818f496
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-25 11:17:10 +0530

    Time: 1 ms (63.09%), Space: 41.9 MB (41.40%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,30 +1,42 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-        return searchUtil(nums,0,nums.length-1,target);
-        
-        
+        return searchUtil(nums,target,0,nums.length-1);
     }
     
-    private int searchUtil(int[] nums, int start, int end,int target){
+    private int searchUtil(int[] nums,int target,int start,int end){
+        
         if(start>end)
             return -1;
         
-        
         int mid=(start+end)/2;
         
-        int num=(nums[mid] < nums[0] == target <nums[0]) ?
-            
-                nums[mid] :
-                target < nums[0] ?  Integer.MIN_VALUE : Integer.MAX_VALUE;
-        if(num<target){
-            return searchUtil(nums,mid+1,end,target);
-        }else if(num>target)
-            return searchUtil(nums,start,mid-1,target);
+        if(nums[mid] == target)
+            return mid;
         
-        return mid;
+        else if(nums[start]<= nums[mid]){
+            
+            if(nums[start] <=target && target<nums[mid])
+                return searchUtil(nums,target,start,mid-1);
+            else
+                return searchUtil(nums,target,mid+1,end);
+            
+            
+            
+        }else{
+            
+            if(nums[mid] < target && target <= nums[end])
+                return searchUtil(nums, target,mid+1,end);
+            else
+                return searchUtil(nums,target,start,mid-1);
+            
+            
+        }
+        
+        
+        
+        
         
     }
-    
     
 }
