be3ab85d407a4c709af37292ab4e3782a31eb17f
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-25 11:10:10 +0530

    Time: 1 ms (76.03%), Space: 44.4 MB (6.52%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,51 +1,45 @@
 class Solution {
     public boolean search(int[] nums, int target) {
         
-        
         return searchUtil(nums,target,0,nums.length-1);
-        
     }
     
     
-    private boolean searchUtil(int[] nums, int target,int start, int end ){
+    private boolean searchUtil(int[] nums,int target, int start, int end){
+        
         if(start>end)
             return false;
         
         int mid=(start+end)/2;
-        
         if(nums[mid]==target)
             return true;
         
-        if(nums[start]==nums[mid] && nums[end]==nums[mid])
-            return searchUtil(nums,target,start+1,end-1);
         
-        else if(nums[start]<=nums[mid] ){
+        if(nums[mid]==nums[start] && nums[mid]==nums[end])
+            return searchUtil(nums,target,start+1,end-1);
+        else if(nums[start]<=nums[mid]){
             
-            if(nums[start]<=target && target<nums[mid]){
-                
+            if(nums[start]<=target && target<nums[mid])
                 return searchUtil(nums,target,start,mid-1);
-                
-            }else
-                
+            else
                 return searchUtil(nums,target,mid+1,end);
             
             
-        }else{
+        }
+        else{
             
-            if(target<=nums[end] && nums[mid]<target){
-                
+            if(target<=nums[end] && nums[mid]<target )
                 return searchUtil(nums,target,mid+1,end);
-                
-            }else
-                
+            else
                 return searchUtil(nums,target,start,mid-1);
+            
             
         }
         
         
         
         
+        
     }
-    
     
 }
