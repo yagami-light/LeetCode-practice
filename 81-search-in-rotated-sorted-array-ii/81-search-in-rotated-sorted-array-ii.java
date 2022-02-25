7a740fcc96421bd78a91bcb0c22b926c2bb5e4f8
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-02-25 11:03:06 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (39.24%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,5 +1,51 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        return Arrays.stream(nums).anyMatch(e->e==target);
+        
+        
+        return searchUtil(nums,target,0,nums.length-1);
+        
     }
+    
+    
+    private boolean searchUtil(int[] nums, int target,int start, int end ){
+        if(start>end)
+            return false;
+        
+        int mid=(start+end)/2;
+        
+        if(nums[mid]==target)
+            return true;
+        
+        if(nums[start]==nums[mid] && nums[end]==nums[mid])
+            return searchUtil(nums,target,start+1,end-1);
+        
+        else if(nums[start]<=nums[mid] ){
+            
+            if(nums[start]<=target && target<nums[mid]){
+                
+                return searchUtil(nums,target,start,mid-1);
+                
+            }else
+                
+                return searchUtil(nums,target,mid+1,end);
+            
+            
+        }else{
+            
+            if(target<=nums[end] && nums[mid]<target){
+                
+                return searchUtil(nums,target,mid+1,end);
+                
+            }else
+                
+                return searchUtil(nums,target,start,mid-1);
+            
+        }
+        
+        
+        
+        
+    }
+    
+    
 }
