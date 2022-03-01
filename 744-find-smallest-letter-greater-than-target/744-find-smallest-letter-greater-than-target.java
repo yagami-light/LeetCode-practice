1b7ce02d57256e6f73f4be6a518dcf9f4949c1bb
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-03-01 13:23:18 +0530

    Time: 1 ms (46.13%), Space: 48.6 MB (5.43%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
@@ -1,31 +1,27 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
+                if(letters[letters.length-1]<=target) return letters[0];
+
+        return nextGreatChar(letters,target,0,letters.length-1);
         
-        return nextGreatLet(letters,target,0,letters.length-1);
     }
     
     
-    private char nextGreatLet(char[] letters, char target,int start,int end){
-        // System.out.println("start "+start+" end "+end+"target "+target);
-        if(start==end && target < letters[start])
-            return letters[start];
-        else if(start==end && target >= letters[start])
-            return letters[(start+1)%letters.length];
-         
+    private char nextGreatChar(char[] let,char target,int start,int end){
+        if(start>end)
+            return let[start];
+        
         int mid=(start+end)/2;
-        char midChar=letters[mid];
+        char midChar=let[mid];
         
         if(midChar<=target){
-            
-            return nextGreatLet(letters,target,mid+1,end);
-        }else if(midChar>target){
-            
-            return nextGreatLet(letters,target,start,mid);
-        }
-        return letters[end];
+            return nextGreatChar(let,target,mid+1,end);
+        }else
+            return nextGreatChar(let,target,start,mid-1);
         
         
         
     }
+    
     
 }
