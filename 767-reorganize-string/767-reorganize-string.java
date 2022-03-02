cc92b8c8aa62a3db0e9c356377ea22dac88affb4
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-03-02 11:47:24 +0530

    Time: 14 ms (9.34%), Space: 42.5 MB (20.81%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.java
+++ 767-reorganize-string/767-reorganize-string.java
@@ -1,48 +1,47 @@
 class Solution {
     public String reorganizeString(String s) {
-        
         Map<Character,Integer> map=new HashMap();
-        for(char c:s.toCharArray()){
+        
+        for(char c: s.toCharArray()){
             
             int count=map.getOrDefault(c,0)+1;
+            
             if(count> (s.length()+1)/2)
-                
                 return "";
             
             map.put(c,count);
+            
         }
+        
         
         PriorityQueue<Character> pq=new PriorityQueue<>((a,b)->Integer.compare(map.get(b),map.get(a)));
         pq.addAll(map.keySet());
-        
+        System.out.println("pq "+pq);
         StringBuilder str=new StringBuilder();
         while(!pq.isEmpty()){
-            System.out.println(str);
-            Character first=pq.remove();
-            if(str.length()==0 || str.charAt(str.length()-1)!=first){
-             
-           
-                str.append(first);
+            
+            char first=pq.remove();
+            if(str.isEmpty() || first != str.charAt(str.length()-1)){
+              str.append(first);
                 map.put(first,map.get(first)-1);
-            
-            
                 if(map.get(first)>0)
-                 pq.add(first);
-            
-            
-            }else if(!pq.isEmpty()){
-                Character second=pq.remove();
-                str.append(second);
-                map.put(second,map.get(second)-1);
-            
-                  if(map.get(second)>0)
-                    pq.add(second);
+                    pq.add(first);
+            }else{
                 
+            char second=pq.remove();
+            str.append(second);
+            map.put(second,map.get(second)-1);
+            if(map.get(second)>0)
+                pq.add(second);
                 pq.add(first);
                 
                 
             }
+            
+            
+            
         }
+        
         return str.toString();
         
         
