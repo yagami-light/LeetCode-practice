d52e8099565b57e2ad8490ec68fc29ca4bc55087
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-03-02 17:10:14 +0530

    Time: 32 ms (75.65%), Space: 118.1 MB (20.83%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,58 +1,22 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        // PriorityQueue<int[]> pq = new PriorityQueue<>(k, (a,b) -> Integer.compare(a[1],b[1]));
-
-        PriorityQueue<int[]> pq=new PriorityQueue<>(k,(a,b)->Integer.compare(((b[0]*b[0])+(b[1]*b[1])),((a[0]*a[0])+(a[1]*a[1]))));
-        int sizeOfPoints=points.length;
+        
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->Integer.compare((b[0]*b[0])+(b[1]*b[1]),(a[0]*a[0])+(a[1]*a[1])));
         
         
-        for(int i=0;i<sizeOfPoints;i++){
+        
+        for(int[] point: points){
             
-            
-            pq.add(points[i]);
+            pq.add(point);
             if(pq.size()>k)
                 pq.remove();
             
             
         }
         
-        int[][] result=new int[k][];
-        for(int i=0;i<k;i++){
-            
-            result[i]=pq.remove();
-            
-            
-        }
         
-        
-        return result;
-        
-        
-        
+        return pq.stream().toArray(int[][]::new);
         
         
     }
 }
-
-
-
-
-/*
-1,4,5,6,3,7
-    
-    
-K=3
-
-result: 1,3,4
-
-
-
-
-4
-3
-1    
-    
-    
-    
-    
-*/    
