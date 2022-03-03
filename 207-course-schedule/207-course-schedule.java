e1068ae2c104dd70cb4ea15f46b19fc6fea2e789
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-03-03 13:58:42 +0530

    Time: 3 ms (96.45%), Space: 47.6 MB (44.00%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -4,61 +4,49 @@ class Solution {
         ArrayList<Integer>[] graph=new ArrayList[n];
         int[] visited=new int[n];
         
-        for(int i=0;i<n;i++){
+ for(int i=0;i<n;i++){
             graph[i]=new ArrayList();
         }
-        
-        for(int[] node:pre){
+                
+             for(int[] node:pre){
             
             graph[node[0]].add(node[1]);
             
         }
-        
         for(int i=0;i<n;i++){
             
-          if(visited[i]==0){
-            if(isCycle(graph,i,visited))
+            if(isCycle(graph,visited,i))
                 return false;
-          }
             
             
         }
-        
         return true;
         
         
     }
-    /*
     
-    not pro=0
-    processing=1
-    processed=2
-    
-    
-    */
-    
-    
-    private boolean isCycle(ArrayList<Integer>[] graph,int n, int[] visited){
-        
-        if(visited[n]==1)
+    private boolean isCycle(ArrayList<Integer>[] graph,int[] visited,int index){
+        if(visited[index]==1)
             return true;
         
-        visited[n]=1;
+        visited[index]=1;
         
-        for(int i:graph[n]){
-            // System.out.println("node "+n+" values"+i);
-                if(visited[i]!=2)
-                if(isCycle(graph,i,visited))
-                    return true;
+        for(int i: graph[index]){
             
+            if(visited[i] != 2){
+                
+                if(isCycle(graph,visited,i))
+                    return true;
+                
+            }
             
         }
-        
-        visited[n]=2;
+        visited[index]=2;
         return false;
         
         
         
     }
+    
     
 }
