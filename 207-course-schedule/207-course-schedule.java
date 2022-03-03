36f376a7d87818a08bd8f7e285e6be582f5b3d22
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-03-03 16:55:28 +0530

    Time: 4 ms (90.26%), Space: 47.9 MB (37.76%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,44 +1,40 @@
 class Solution {
     public boolean canFinish(int n, int[][] pre) {
         
-        ArrayList<Integer>[] graph=new ArrayList[n];
         int[] indegree=new int[n];
-        for(int i=0;i<n;i++){
-            graph[i]=new ArrayList();
-        }
         
+        ArrayList<Integer>[] graph=new ArrayList[n];
+        for(int i=0;i<n;i++) graph[i]=new ArrayList();
         for(int i=0;i<pre.length;i++){
-            graph[pre[i][0]].add(pre[i][1]);
-            indegree[pre[i][1]]++;
+            
+            int curr=pre[i][0];
+            int prec=pre[i][1];
+            
+            indegree[prec]++;
+            graph[curr].add(prec);
+            
         }
-        
+        int count=0;
+        Queue<Integer> queue=new LinkedList();
         for(int i=0;i<n;i++){
-            int j=0;
-            for(;j<n;j++){
-                
-                if(indegree[j]==0)
-                    break;
-            }
-                if(j==n)
-                    return false;
-                
-                indegree[j]=-1;
-                
-                for(int num:graph[j]){
-                    indegree[num]--;
-                }
-                
-                
-            
-            
+            if(indegree[i]==0)
+                queue.add(i);
         }
         
+        while(!queue.isEmpty()){
+            int poppedValue=queue.remove();
+            count++;
+            
+            for(int i: graph[poppedValue]){
+                
+                indegree[i]--;
+                if(indegree[i]==0)
+                    queue.add(i);
+                
+            }
+        }
         
-        return true;
-        
-        
-        
-       
+        return count==n;
         
         
     }
