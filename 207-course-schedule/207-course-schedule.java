cd6c13a02a1944c5c76833121289e16fc6afeaa9
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-03-03 16:31:54 +0530

    Time: 3 ms (96.45%), Space: 42.2 MB (88.66%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,46 +1,44 @@
 class Solution {
     public boolean canFinish(int n, int[][] pre) {
         
-        int[] indegree=new int[n];
-        
         ArrayList<Integer>[] graph=new ArrayList[n];
-        
+        int[] indegree=new int[n];
         for(int i=0;i<n;i++){
             graph[i]=new ArrayList();
         }
         
         for(int i=0;i<pre.length;i++){
-            
-            int prec=pre[i][1];
-            int curr=pre[i][0];
-            
-            indegree[prec]++;
-            graph[curr].add(prec);
-            
-            
-            
+            graph[pre[i][0]].add(pre[i][1]);
+            indegree[pre[i][1]]++;
         }
-        
-        
         
         for(int i=0;i<n;i++){
             int j=0;
             for(;j<n;j++){
+                
                 if(indegree[j]==0)
                     break;
             }
-            if(j==n)
-                return false;
-            
-            indegree[j]=-1;
-            for(int num:graph[j])
-                indegree[num]--;
+                if(j==n)
+                    return false;
+                
+                indegree[j]=-1;
+                
+                for(int num:graph[j]){
+                    indegree[num]--;
+                }
+                
+                
             
             
         }
+        
+        
         return true;
         
         
+        
+       
         
         
     }
