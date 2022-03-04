64752b88fbde38dc8cdae7d122da38149b612f32
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-03-04 10:38:04 +0530

    Time: 4 ms (90.32%), Space: 48.2 MB (34.20%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,38 +1,37 @@
 class Solution {
     public boolean canFinish(int n, int[][] pre) {
-        
         int[] indegree=new int[n];
-        
         ArrayList<Integer>[] graph=new ArrayList[n];
+        
         for(int i=0;i<n;i++) graph[i]=new ArrayList();
-        for(int i=0;i<pre.length;i++){
-            
-            int curr=pre[i][0];
-            int prec=pre[i][1];
-            
-            indegree[prec]++;
-            graph[curr].add(prec);
-            
+        for(int i=0;i<pre.length;i++) { 
+            graph[pre[i][0]].add(pre[i][1]);
+            indegree[pre[i][1]]++;
         }
-        int count=0;
+        
         Queue<Integer> queue=new LinkedList();
         for(int i=0;i<n;i++){
             if(indegree[i]==0)
                 queue.add(i);
         }
         
+        int count=0;
         while(!queue.isEmpty()){
-            int poppedValue=queue.remove();
+            
+            int val=queue.remove();
             count++;
             
-            for(int i: graph[poppedValue]){
-                
+            for(int i: graph[val]){
                 indegree[i]--;
                 if(indegree[i]==0)
                     queue.add(i);
-                
             }
+            
+            
+            
+            
         }
+        
         
         return count==n;
         
