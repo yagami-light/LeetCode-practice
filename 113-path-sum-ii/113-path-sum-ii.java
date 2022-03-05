be7c2eaebdec7895eb7ede7344dae33bedce4d4c
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-03-05 20:29:50 +0530

    Time: 2 ms (76.72%), Space: 44.6 MB (36.55%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.java
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -15,35 +15,26 @@
  */
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> res=new ArrayList();
-        pathSum(root,targetSum,0,res,new ArrayList());
         
+         List<List<Integer>> res=new ArrayList();
+        pathSum(root,targetSum,res,new ArrayList());
         return res;
         
     }
     
-    private void pathSum(TreeNode root,int targetSum,int currSum,List<List<Integer>> res,List<Integer> currList){
-           if(root==null)
-            return; 
-        if(root.left ==null && root.right==null && currSum+root.val ==targetSum)
-        {
-            currList.add(root.val);
-            res.add(new ArrayList(currList));
-            currList.remove(currList.size()-1);
+    private void pathSum(TreeNode root, int targetSum,List<List<Integer>> res,List<Integer> currList){
+        if(root==null)
             return;
+        currList.add(root.val);
+        if(root.left == null && root.right == null && root.val == targetSum){
+            res.add(new ArrayList(currList));
+            // return;
         }
-        
-      
-     
-        
-        currList.add(root.val);    
-        pathSum(root.left,targetSum,currSum+root.val,res,currList);
-        pathSum(root.right,targetSum,currSum+root.val,res,currList);
-        currList.remove(currList.size()-1);    
-        
-        
-        
-        
+            
+        // currList.add(root.val);
+        pathSum(root.left,targetSum-root.val,res,currList);
+        pathSum(root.right,targetSum-root.val,res,currList);
+        currList.remove(currList.size()-1);
     }
     
     
