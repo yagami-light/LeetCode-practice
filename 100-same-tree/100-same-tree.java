39e3a82a2479bf3b2274843610beeda5e38806e8
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-03-05 11:29:02 +0530

    Time: 1 ms (9.04%), Space: 41.4 MB (37.09%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -16,14 +16,46 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         
-        if(p==null && q==null)
-            return true;
+        Stack<TreeNode> stack1=new Stack();
+        Stack<TreeNode> stack2=new Stack();
+        if(p != null)
+            stack1.add(p);
+        if(q!=null)
+            stack2.add(q);
         
-        if(p==null || q==null)
-            return false;
+        while(!stack1.isEmpty() && !stack2.isEmpty()){
+            
+            TreeNode pn=stack1.pop();
+            TreeNode qn=stack2.pop();
+            if(pn.val != qn.val )
+                return false;
+            
+            if(pn.left !=null)
+                stack1.add(pn.left);
+            
+            if(qn.left != null)
+                stack2.add(qn.left);
+            
+            if(stack1.size() != stack2.size())
+                return false;
+            
+              if(pn.right !=null)
+                stack1.add(pn.right);
+            
+            if(qn.right != null)
+                stack2.add(qn.right);
+            
+            if(stack1.size() != stack2.size())
+                return false;
+            
+            
+            
+            
+        }
         
-        return p.val==q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
         
+        
+        return stack1.size() == stack2.size();
         
     }
 }
