4d003a03ddf0343c02c72bc7e3a449bd532952cd
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-03-05 19:47:38 +0530

    Time: 1 ms (95.55%), Space: 90.3 MB (58.14%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
@@ -15,27 +15,38 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-        
-        
-        return minDepthUtil(root);
-        
-    }
-    
-    private int minDepthUtil(TreeNode root){
         if(root==null)
             return 0;
+        Queue<TreeNode> queue=new LinkedList();
+        queue.add(root);
+        int depth=1;
+        while(!queue.isEmpty()){
+            
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+            TreeNode node=queue.remove();
+            
+            if(node.left ==null && node.right == null){
+                
+                return depth;
+                
+            }
+            
+            if(node.left != null)
+                queue.add(node.left);
+            
+            if(node.right != null)
+                queue.add(node.right );
+            
+            
+            
+        }
+        
+        depth++;
         
         
-        int left=minDepthUtil(root.left);
-        int right=minDepthUtil(root.right);
-        
-        
-        
-        return (left==0 || right==0 )? (left+right+1) :  1+Math.min(left,right);
-        
-        
-        
+        }
+    
+    return  depth;
     }
-    
-    
 }
