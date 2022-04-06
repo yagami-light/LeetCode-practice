1cae942b943dccc02a55da2289b43694de7b8b91
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-04-06 10:14:10 +0530

    Time: 17 ms (8.99%), Space: 56.6 MB (69.09%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,67 +1,53 @@
 class Solution {
-    final static int[][] DIRECTIONS=new int[][]{{1,0},{0,-1},{-1,0},{0,1}};
+    private static int[][] DIRECTIONS=new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+    
+    
     public int numIslands(char[][] grid) {
-        
         int m=grid.length;
         int n=grid[0].length;
         int count=0;
-        
         for(int i=0;i<m;i++){
             
             
             for(int j=0;j<n;j++){
                 
-                
                 if(grid[i][j]=='1'){
-                    
-                    
                     count++;
+                    grid[i][j]='0';
+                    
                     Queue<int[]> queue=new LinkedList();
                     queue.add(new int[]{i,j});
-                    grid[i][j]='0';
                     while(!queue.isEmpty()){
                         
-                        int[] pop=queue.remove();
+                        int[] node=queue.remove();
                         
                         for(int[] dir:DIRECTIONS){
-                            int x=pop[0]+dir[0];
-                            int y=pop[1]+dir[1];
                             
-                            if(x<0 || y< 0 || x>=m || y >=n || grid[x][y]=='0')
+                            int x=node[0]+dir[0];
+                            int y=node[1]+dir[1];
+                            
+                            if(x<0 || y < 0 || x>=m || y>=n || grid[x][y]=='0')
                                 continue;
+                            
                             if(grid[x][y]=='1'){
-                            grid[x][y]='0';
-
-                            queue.add(new int[]{x,y});
+                                queue.add(new int[]{x,y});
+                                grid[x][y]='0';
                             }
                             
                             
                         }
                         
                         
-                        
                     }
-                    
-                    
-                    
-                    
-                    
                     
                     
                 }
                 
                 
-                
-                
-                
             }
             
-            
         }
-        
         return count;
-        
-        
         
         
     }
