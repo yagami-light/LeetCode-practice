6f08e2977b3bec263550377f69111669ac00a7fd
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-04-06 11:00:57 +0530

    Time: 12 ms (15.04%), Space: 57.7 MB (33.42%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,45 +1,97 @@
 class Solution {
     private static int[][] DIRECTIONS=new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
     
+    class UnionFind{
+        int[] parent;
+        int[] rank;
+        int[] size;
+        int count;
+        
+        UnionFind(char[][] grid){
+            int m=grid.length;
+            int n=grid[0].length;
+            
+            parent=new int[m*n];
+            rank=new int[m*n];
+            size=new int[m*n];
+            
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    
+                    parent[i*n+j]=i*n+j;
+                    if(grid[i][j]=='1')
+                        count++;
+                    size[i*n+j]=1;
+                }
+            }
+            // System.out.println()
+            
+        }
+        
+        private int find(int x){
+                                                // System.out.println("master");
+
+            while(x != parent[x]){
+x=parent[x];                
+            }
+            
+            
+            return x;
+        }
+        
+        
+        private void union(int x,int y){
+                                                // System.out.println("x "+x+" y"+y);
+
+            int px=find(x);
+            int py=find(y);
+            if(px!=py){
+                count--;
+                if(size[px]>=size[py]){
+                    parent[py]=px;
+                    size[px]+=size[py];
+                }
+                else{
+                    parent[px]=py;
+                    size[py]+=size[px];
+                }
+                
+            }
+            
+            
+        }
+        
+        
+        
+        
+    }
     
     public int numIslands(char[][] grid) {
+        
         int m=grid.length;
         int n=grid[0].length;
-        int count=0;
+        UnionFind uf=new UnionFind(grid);
         for(int i=0;i<m;i++){
-            
             
             for(int j=0;j<n;j++){
                 
+                
                 if(grid[i][j]=='1'){
-                    count++;
-                    grid[i][j]='0';
                     
-                    Queue<int[]> queue=new LinkedList();
-                    queue.add(new int[]{i,j});
-                    while(!queue.isEmpty()){
+                    for(int[] dir: DIRECTIONS){
                         
-                        int[] node=queue.remove();
+                        int x=i+dir[0];
+                        int y=j+dir[1];
                         
-                        for(int[] dir:DIRECTIONS){
-                            
-                            int x=node[0]+dir[0];
-                            int y=node[1]+dir[1];
-                            
-                            if(x<0 || y < 0 || x>=m || y>=n || grid[x][y]=='0')
-                                continue;
-                            
-                            if(grid[x][y]=='1'){
-                                queue.add(new int[]{x,y});
-                                grid[x][y]='0';
-                            }
-                            
-                            
-                        }
+                        if(x<0 || y< 0|| x>=m || y >=n || grid[x][y]=='0')
+                            continue;
+                        if(grid[x][y]=='1')
+                        uf.union(i*n+j,x*n+y);
+                        
+                        
                         
                         
                     }
-                    
                     
                 }
                 
@@ -47,7 +99,10 @@ class Solution {
             }
             
         }
-        return count;
+        
+        return uf.count;
+        
+        
         
         
     }
