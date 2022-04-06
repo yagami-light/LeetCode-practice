329c074d27fd3036a6ba428a77c18b991e83c26c
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-04-06 10:08:39 +0530

    Time: 9 ms (22.76%), Space: 56.1 MB (74.33%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,47 +1,68 @@
 class Solution {
-    private static int[][] DIRECTIONS=new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-    
+    final static int[][] DIRECTIONS=new int[][]{{1,0},{0,-1},{-1,0},{0,1}};
     public int numIslands(char[][] grid) {
+        
         int m=grid.length;
         int n=grid[0].length;
         int count=0;
+        
         for(int i=0;i<m;i++){
+            
             
             for(int j=0;j<n;j++){
                 
+                
                 if(grid[i][j]=='1'){
-                    count+=dfs(grid,i,j);
+                    
+                    
+                    count++;
+                    Queue<int[]> queue=new LinkedList();
+                    queue.add(new int[]{i,j});
+                    grid[i][j]='0';
+                    while(!queue.isEmpty()){
+                        
+                        int[] pop=queue.remove();
+                        
+                        for(int[] dir:DIRECTIONS){
+                            int x=pop[0]+dir[0];
+                            int y=pop[1]+dir[1];
+                            
+                            if(x<0 || y< 0 || x>=m || y >=n || grid[x][y]=='0')
+                                continue;
+                            if(grid[x][y]=='1'){
+                            grid[x][y]='0';
 
+                            queue.add(new int[]{x,y});
+                            }
+                            
+                            
+                        }
+                        
+                        
+                        
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                    
                 }
                 
                 
+                
+                
+                
             }
+            
+            
         }
         
         return count;
         
         
+        
+        
     }
-    
-    
-    private int dfs(char[][] grid,int i, int j ){
-        int m=grid.length;
-        int n=grid[0].length;
-        
-        if(i<0 || j<0 || i>=m || j >=n || grid[i][j]=='0'){
-            return 0;
-        }
-        
-        grid[i][j]='0';
-       
-        for(int[] dir: DIRECTIONS){
-            
-            dfs(grid,i+dir[0],j+dir[1]);
-        }
-        
-        
-        return 1;
-    }
-    
-    
 }
