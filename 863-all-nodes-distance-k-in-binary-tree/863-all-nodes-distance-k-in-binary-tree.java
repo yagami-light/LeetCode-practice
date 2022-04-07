b19d68a33a4d4452510dd22e678cf4afd9b03a7d
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-04-07 15:03:27 +0530

    Time: 17 ms (43.40%), Space: 43 MB (60.86%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -12,58 +12,57 @@ class Solution {
         Map<TreeNode,Integer> map=new HashMap();
         
         find(root,target,map);
+        
         List<Integer> res=new ArrayList();
-        search(root,target,k,map,res,0);
+        search(root,target,k,res,map,0);
+        
         return res;
+        
+        
     }
     
-    private int find(TreeNode root, TreeNode target,Map<TreeNode,Integer> map)
-    {
+    private int find(TreeNode root,TreeNode target,Map<TreeNode,Integer> map){
         if(root==null)
-            return -1;
+            return  -1;
         
-        
-        if(root==target){
+        if(target==root){
             map.put(root,0);
             return 0;
+            
         }
         
-        
-       int left=find(root.left,target,map);
+        int left=find(root.left,target,map);
         if(left>=0){
-            map.put(root,left+1);   
+            map.put(root,left+1);
             return left+1;
         }
-        
         int right=find(root.right,target,map);
         if(right>=0){
-            
             map.put(root,right+1);
             return right+1;
         }
         
         return -1;
         
-    }  
+        
+        
+    }
     
-    
-    private void search(TreeNode root,TreeNode target,int k, Map<TreeNode,Integer> map,List<Integer> res,int length){
+    private void search(TreeNode root,TreeNode target,int k, List<Integer> res,Map<TreeNode,Integer> map,int length){
         if(root==null)
             return;
-        
         if(map.containsKey(root))
             length=map.get(root);
-        
         
         if(length==k)
             res.add(root.val);
         
+        search(root.left,target,k,res,map,length+1);
+        search(root.right,target,k,res,map,length+1);
         
-        
-        search(root.left,target,k,map,res,length+1);
-        search(root.right,target,k,map,res,length+1);
         
     }
+    
     
     
 }
