1cfcb8ecbda39040c484438a88972d14396b9e0f
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-04-07 14:56:11 +0530

    Time: 19 ms (28.95%), Space: 42.5 MB (79.62%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -10,59 +10,60 @@
 class Solution {
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         Map<TreeNode,Integer> map=new HashMap();
-        find(root,target,map);
-        System.out.println(map);
-        System.out.println(target);
-        List<Integer> res=new ArrayList();
-        search(root,target,map,res,0,k);
-        return res;
         
+        find(root,target,map);
+        List<Integer> res=new ArrayList();
+        search(root,target,k,map,res,0);
+        return res;
     }
     
-    private void   search(TreeNode root,TreeNode target,Map<TreeNode,Integer> map, List<Integer> res,int length,int k)
+    private int find(TreeNode root, TreeNode target,Map<TreeNode,Integer> map)
     {
         if(root==null)
-            return;
-        if(map.containsKey(root))
-            length=map.get(root);
-        
-        if(k==length)
-            res.add(root.val);
-        search(root.left,target,map,res,length+1,k);
-        search(root.right,target,map,res,length+1,k);
-        
-    }
-    
-    
-    private int find(TreeNode root,TreeNode target,Map<TreeNode,Integer> map){
-        
-        if(root==null)
             return -1;
+        
         
         if(root==target){
             map.put(root,0);
             return 0;
         }
         
-        int left=find(root.left,target,map);
-         if(left>=0){
-            map.put(root,left+1);
-             return left+1;
+        
+       int left=find(root.left,target,map);
+        if(left>=0){
+            map.put(root,left+1);   
+            return left+1;
         }
+        
         int right=find(root.right,target,map);
         if(right>=0){
+            
             map.put(root,right+1);
             return right+1;
         }
         
-        
         return -1;
         
+    }  
+    
+    
+    private void search(TreeNode root,TreeNode target,int k, Map<TreeNode,Integer> map,List<Integer> res,int length){
+        if(root==null)
+            return;
+        
+        if(map.containsKey(root))
+            length=map.get(root);
+        
+        
+        if(length==k)
+            res.add(root.val);
+        
+        
+        
+        search(root.left,target,k,map,res,length+1);
+        search(root.right,target,k,map,res,length+1);
         
     }
-    
-    
-    
     
     
 }
