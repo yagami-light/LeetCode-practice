6574c2dd3344d6ade75f56f730f03ab54168ce80
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-02 15:39:52 +0530

    Time: 1 ms (31.73%), Space: 47.3 MB (45.39%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,105 +1,77 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int lo=0;
+        int low=0;
         int hi=nums.length-1;
         int[] ans=new int[]{-1,-1};
         
-        if(nums.length==0)
-            return ans;
-        
-        while(lo<=hi){
+        while(low<=hi){
             
-            int mid=lo+(hi-lo)/2;
+            int mid=low+(hi-low)/2;
             
-            if(target == nums[mid]){
-                hi=mid-1;
-            }else if(target < nums[mid]){
+            if(nums[mid]==target){
+                
+                int n=mid>0 ? nums[mid-1]: Integer.MIN_VALUE;
+                if(target>n){
+                    ans[0]=mid;
+                }
+                
+                
                 hi=mid-1;
                 
-            }else {
-                lo=mid+1;
+            }else if(target<nums[mid]){
+                
+                hi=mid-1;
+                
+                
+            }else{
+                low=mid+1;
+                
             }
             
             
+            
+            
+            
         }
         
+        low=0;
+        hi=nums.length-1;
         
-        if(lo==nums.length || nums[lo]!=target)
-            return ans;
-        
-        
-        ans[0]=lo;
-          lo=0;
-         hi=nums.length-1;
-        while(lo<=hi){
+        while(low<=hi){
             
-            int  mid=lo+(hi-lo)/2;
+            int mid=low+(hi-low)/2;
             
-            if(target == nums[mid]){
-                lo=mid+1;
-            }else if(target < nums[mid]){
+            if(nums[mid]==target){
+                
+                int n = mid < nums.length - 1 ? nums[mid + 1] : Integer.MAX_VALUE;
+            if (target < n) {
+                ans[1] = mid;
+                break;
+            }
+                
+                low=mid+1;
+                
+            }else if(target<nums[mid]){
+                
                 hi=mid-1;
                 
-            }else {
-                lo=mid+1;
+                
+            }else{
+                low=mid+1;
+                
             }
             
             
+            
+            
+            
         }
-        
-         ans[1] = hi;
-        
-       
         
         
         return ans;
         
         
+        
+        
     }
-    
-    /*
-    
-    public int[] searchRange(int[] nums, int target) {
-    int start = 0;
-    int end = nums.length - 1;
-    int[] ans = { -1, -1 };
-    if (nums.length == 0) {
-        return ans;
-    }
-    while (start <= end) {
-        int mid = (start + end) / 2;
-        if (target == nums[mid]) {
-            end = mid - 1;
-        } else if (target < nums[mid]) {
-            end = mid - 1;
-        } else {
-            start = mid + 1;
-        }
-    }
-    //考虑 tartget 是否存在，判断我们要找的值是否等于 target 并且是否越界
-    if (start == nums.length || nums[ start ] != target) {
-        return ans;
-    } else {
-        ans[0] = start;
-    }
-    ans[0] = start;
-    start = 0;
-    end = nums.length - 1;
-    while (start <= end) {
-        int mid = (start + end) / 2;
-        if (target == nums[mid]) {
-            start = mid + 1;
-        } else if (target < nums[mid]) {
-            end = mid - 1;
-        } else {
-            start = mid + 1;
-        }
-    }
-    ans[1] = end;
-    return ans;
-}
-
-    */
-    
-    
 }
