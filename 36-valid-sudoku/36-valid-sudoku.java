5ae5be826b3976a35760c020ebb52a7a6b04065a
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-02 19:01:44 +0530

    Time: 8 ms (44.94%), Space: 45.8 MB (65.90%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -4,87 +4,50 @@ class Solution {
         int m=board.length;
         int n=board[0].length;
         
+        
         for(int i=0;i<m;i++){
+            
             
             for(int j=0;j<n;j++){
                 
-                if(board[i][j]=='.')
-                    continue;
                 
-                if(!isValid(board,i,j,m,n))
+                if(!isValid(board, i, j,m,n))
                     return false;
-                
-                
-                }
-            
-        }
-                
-                
-        return true;
-        
-        
-        
-    }
-    
-    private boolean isValid(char[][] board,int i,int j,int m,int n){
-        char c=board[i][j];
-        
-        for(int k=0;k<m;k++){
-            if(board[k][j]=='.')
-                continue;
-            if(k!=i && c==board[k][j])
-                return false;
-        }
-        
-        
-         
-        for(int k=0;k<n;k++){
-            if(board[i][k]=='.')
-                continue;
-            if(k!=j && c==board[i][k])
-                return false;
-        }
-        
-        /*
-        00 01 02 .03 04 05 
-        10 11 12 .13 14 15
-        20 21 22 .23 24 25
-        
-        n=9;
-        
-        */
-        
-        
-        
-        for(int k=0;k<n;k++){
-            
-            if(board[3*(i/3) +k/3][3*(j/3) +k%3]=='.')
-                continue;
-            
-            if((3*(i/3) +k/3) !=i  && (3*(j/3) +k%3) !=j &&  board[3*(i/3) +k/3][3*(j/3) +k%3]==c){
-                
-//                 System.out.println("test"+i+" "+j+" "+k);
-//                 System.out.println((3*i/3 +k/3));
-//                 System.out.println((3*i/3 +k/3));
-                
-                return false;
-                
-                
             }
             
         }
-        
-        
-        
-        
-        
         return true;
+        
+        
+    }
+
+    
+    private boolean isValid(char[][] board,int row,int col,int m,int n){
+        char c=board[row][col];
+        for(int i=0;i<m;i++){
+            
+            if(i!=row && board[i][col]!='.' && board[i][col]==c)
+                return false;
+            
+             if(i!=col && board[row][i]!='.' && board[row][i]==c)
+                return false;
+            
+            
+            
+            
+             if((((3*(row/3)+i/3)))!=row && (((3*(col/3)+i%3))) != col && board[(((3*(row/3)+i/3)))][(((3*(col/3)+i%3)))]!='.' && board[(((3*(row/3)+i/3)))][(((3*(col/3)+i%3)))] ==c)
+                return false;
+            
+            
+            
+            
+            
+        }
+        return true;
+        
         
     }
     
     
-                
-                
-                
-                
+    
 }
