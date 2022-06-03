dcbedc544c431938998e91761f6ba73133052900
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-03 18:37:30 +0530

    Time: 24 ms (26.00%), Space: 42.8 MB (37.90%) - LeetHub

--- 38-count-and-say/38-count-and-say.java
+++ 38-count-and-say/38-count-and-say.java
@@ -4,36 +4,38 @@ class Solution {
             return "1";
         
         String last=countAndSay(n-1);
-        return getNextString(last);
+        
+        return getNextNumber(last);
         
     }
     
-    
-    
-    private String getNextString(String last){
+    private String getNextNumber(String last){
         if(last.length()==0)
             return "";
         
         int num=getRepeateNum(last);
         
-        return num+""+last.charAt(0)+getNextString(last.substring(num));
+        return num+""+last.charAt(0)+getNextNumber(last.substring(num));
+        
     }
     
     
-    private int getRepeateNum(String string) {
-    int count = 1;
-    char same = string.charAt(0);
-    for (int i = 1; i < string.length(); i++) {
-        if (same == string.charAt(i)) {
-            count++;
-        } else {
-            break;
+    private int getRepeateNum(String last){
+        if(last.length()==0)
+            return 0;
+        int count=1;
+        char initChar=last.charAt(0);
+        for(int i=1;i<last.length();i++){
+            if(initChar==last.charAt(i))
+                count++;
+            else
+                break;
         }
+        
+        
+        return count;
+        
     }
-    return count;
-}
-    
-    
     
     
 }
