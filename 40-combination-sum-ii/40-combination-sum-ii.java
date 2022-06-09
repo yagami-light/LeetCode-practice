6abe9034d12b67896d9a757d6e7edf8497355d3b
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-09 21:20:39 +0530

    Time: 6 ms (52.35%), Space: 42.8 MB (77.71%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,60 +1,67 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] cand, int target) {
-        
-         List<List<Integer>> res=new ArrayList();
-        Arrays.sort(cand);
-        
-        combSumUtil(cand, target,new ArrayList(),res,0);
-        return res;
-        
+    public List<List<Integer>> combinationSum2(int[] nums, int target) {
+    List<List<List<Integer>>> ans = new ArrayList<>();
+    Arrays.sort(nums);
+    if (nums[0] > target) {
+        return new ArrayList<List<Integer>>();
     }
-    
-    private void combSumUtil(int[] cand,int target,List<Integer> currList,List<List<Integer>> res,int index){
-        if(target==0){
-            res.add(new ArrayList(currList));
-        }else if(target<0)
-        return;
-        else{
-            
-            for(int i=index;i<cand.length;i++){
-                if(i>index && cand[i]==cand[i-1]) continue;
-                currList.add(cand[i]);
-                combSumUtil(cand,target-cand[i],currList,res,i+1);
-                currList.remove(currList.size()-1);
+    for (int i = 0; i <= target; i++) {
+        List<List<Integer>> ans_i = new ArrayList<List<Integer>>();
+        ans.add(i, ans_i);
+    }
+
+    for (int i = 0; i < nums.length; i++) {
+        /*******************修改的地方********************/
+        if (i > 0 && nums[i] == nums[i - 1]) {
+            continue;
+        }
+        /***********************************************/
+        for (int sum = nums[i]; sum <= target; sum++) {
+            List<List<Integer>> ans_sum = ans.get(sum);
+            List<List<Integer>> ans_sub = ans.get(sum - nums[i]);
+            if (sum == nums[i]) {
+                ArrayList<Integer> temp = new ArrayList<Integer>();
+                temp.add(nums[i]);
+                ans_sum.add(temp);
+
             }
-            
-            
+            if (ans_sub.size() > 0) {
+                for (int j = 0; j < ans_sub.size(); j++) {
+                    ArrayList<Integer> temp = new ArrayList<Integer>(ans_sub.get(j));
+                    temp.add(nums[i]);
+                    ans_sum.add(temp);
+                }
+            }
         }
     }
-    
-    private List<List<Integer>>  removeDuplicate(List<List<Integer>>  inputList){
-        List<List<Integer>> res=new ArrayList();
-        Map<String,String> map=new HashMap();
-        for(List list: inputList){
-            String str="";
-            Collections.sort(list);
-            for(int i=0;i<list.size()-1;i++){
-                str+=list.get(i)+",";
-            }
-            str+=list.get(list.size()-1);
-        
-        
-        map.put(str,"");
-        }
-        for(String str:map.keySet()){
-            List<Integer> list=new ArrayList();
-            String[] splitedStr=str.split(",");
-            for(String strr:splitedStr){
-                list.add(Integer.parseInt(strr));
-            }
-            res.add(list);
-        }
-        return res;
-        
-        
-        
+    return remove(ans.get(target), nums);
+
+}
+
+private List<List<Integer>> remove(List<List<Integer>> list, int[] nums) {
+    HashMap<Integer, Integer> nh = new HashMap<Integer, Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>(list);
+    for (int n : nums) {
+        int s = nh.getOrDefault(n, 0);
+        nh.put(n, s + 1);
     }
-    
+    for (int i = 0; i < list.size(); i++) {
+        List<Integer> l = list.get(i);
+        HashMap<Integer, Integer> temp = new HashMap<Integer, Integer>();
+        for (int n : l) {
+            int s = temp.getOrDefault(n, 0);
+            temp.put(n, s + 1);
+        }
+        for (int n : l) {
+            if (temp.get(n) > nh.get(n)) {
+                ans.remove(l);
+                break;
+            }
+        }
+    }
+    return ans;
+}
+
     
     
 }
