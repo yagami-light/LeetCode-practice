294a966e427f0548e3c59a3a80e2347bc942617a
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-09 21:02:46 +0530

    Time: 12 ms (16.80%), Space: 49.3 MB (8.57%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,37 +1,54 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] cand, int target) {
-        List<List<Integer>> res=new ArrayList();
+        List<List<List<Integer>>> res=new ArrayList();
         
-        combinationSum(cand, target, 0,new ArrayList(),res,0);
-        
-        
-        return res;
-        
-        
-    }
-    
-    private void combinationSum(int[] cand,int target,int currSum,List<Integer> currList,List<List<Integer>> res,int index){
-        if(target<0)
-            return;
-        
-        else if(target==0) res.add(new ArrayList(currList));
-            
-        else{
-            
-            for(int i=index;i<cand.length;i++){
-                
-                currList.add(cand[i]);
-                combinationSum(cand,target-cand[i],0,currList,res,i);
-                currList.remove(currList.size()-1);
-                
-            }
-            
-            
+        for(int i=0;i<=target;i++){
+            res.add(i,new ArrayList());
         }
         
+        for(int i=0;i<cand.length;i++){
+            
+            for(int sum=cand[i];sum<=target;sum++){
+                
+                List<List<Integer>> sum_list=res.get(sum);
+                List<List<Integer>> sub_list=res.get(sum-cand[i]);
+                
+                if(sum==cand[i]){
+                    List<Integer> templist=new ArrayList();
+                    templist.add(cand[i]);
+                    sum_list.add(templist);
+                }
+                
+                
+          /*      
+                if(sub_list.size()>0){
+                    
+                    for(int j=0;j<sub_list.size();j++){
+                        List<Integer> list=new ArrayList(sub_list.get(j));
+                        list.add(cand[j]);
+                        sum_list.add(list);
+                        
+                    }
+                    
+                }
+                
+            */
+                
+                if (sub_list.size() > 0) {
+                for (int j = 0; j < sub_list.size(); j++) {
+                    ArrayList<Integer> temp = new ArrayList<Integer>(sub_list.get(j));
+                    temp.add(cand[i]);
+                    sum_list.add(temp);
+                }
+            }
+            }
+        }
+        
+        return res.get(target);
+        
+        
+        
         
         
     }
-    
-    
 }
