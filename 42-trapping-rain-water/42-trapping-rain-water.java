8ca72406a8862acb36f0c3d6fe97c484dc79f4e0
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-10 18:41:31 +0530

    Time: 1 ms (96.82%), Space: 48.7 MB (19.53%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,28 +1,33 @@
 class Solution {
     public int trap(int[] height) {
         
+        int maxL=height[0];
+        int maxR=height[height.length-1];
+        int left=0;
+        int right=height.length-1;
         int ans=0;
-        int n=height.length;
         
-        int[] leftMax=new int[n];
-        int[] maxRight=new int[n];
-        
-        for(int i=1;i<height.length;i++){
-            leftMax[i]=Math.max(leftMax[i-1],height[i-1]);
+        while(left<right){
+            // System.out.println("left height "+height[left]+" right height "+height[right]+"  max Lefgt "+maxL+" max right "+maxR);
+            
+           if(maxL < maxR){
+               left++;
+               maxL=Math.max(maxL,height[left]);
+               ans+=maxL-height[left];
+               
+               
+               
+               
+           } else{
+               right--;
+               maxR=Math.max(maxR,height[right]);
+               ans+=maxR-height[right];
+               
+           }
+           
+            
         }
-        
-        for(int i=height.length-2;i>=0;i--){
-            maxRight[i]=Math.max(maxRight[i+1],height[i+1]);
-        }
-        
-        for(int i=0;i<height.length;i++){
-            if(Math.min(leftMax[i],maxRight[i])-height[i] <0) continue;
-            ans+=Math.min(leftMax[i],maxRight[i])-height[i];
-        }
-        
         return ans;
-        
-   
         
         
     }
