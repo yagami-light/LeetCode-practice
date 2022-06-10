fedc1808b3f1934c4b0009316fdc12b55d22a95c
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-10 13:30:40 +0530

    Time: 4 ms (63.32%), Space: 126.8 MB (60.27%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,15 +1,27 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Set<Integer> set=Arrays.stream(nums).boxed().collect(Collectors.toSet());
-        // System.out.println(set);
-        // Arrays.sort(nums);
-        for(int i=1;i<=nums.length+1;i++){
-            // System.out.println("i"+i);
-            // System.out.println(Arrays.binarySearch(nums,i));
-            if(!set.contains(i))
-                return i;
-            
+        
+        for(int i=0;i<nums.length;i++){
+            // System.out.println(Arrays.toString(nums));
+            int curr=nums[i];
+            while(curr-1>=0 && curr-1<nums.length && nums[curr-1]!=curr){
+                // System.out.println("element at "+i+" "+nums[i]);
+                int next = nums[curr-1];
+                nums[curr-1] = curr;
+                curr = next;
+                
+            }
         }
-        return -1;
+        
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=i+1)
+                return i+1;
+        }
+        
+        return nums.length+1;
+        
+        
+        
+        
     }
 }
