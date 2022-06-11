7a5b126dbec4f7f2b6a69c9de74ea25828e5c5a0
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-11 16:12:29 +0530

    Time: 2 ms (99.86%), Space: 42 MB (99.21%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -1,35 +1,33 @@
 class Solution {
-  public boolean isMatch(String s, String p) {
-    int i = 0, j = 0;
-    int m = s.length(), n = p.length();
-    int last_match = -1, startj = -1;
-    while (i < m) {
-
-      if (j < n && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')) {
-        i++;
-        j++;
-
-      } else if (j < n && p.charAt(j) == '*') {
-        startj = j;
-
-        j++;
-        last_match = i;
-
-      } else if (startj != -1) {
-        j = startj + 1;
-        last_match++;
-        i = last_match;
-
-      } else
-        return false;
-
+    public boolean isMatch(String s, String p) {
+        int i=0,j=0;
+        int m=s.length(),n=p.length();
+        int startj=-1,lastI=-1;
+        while(i<m){
+            
+            if(j<n && (s.charAt(i)==p.charAt(j) || p.charAt(j)=='?')){
+                i++;
+                j++;
+            }else if(j<n && p.charAt(j)=='*'){
+                startj=j;
+                j++;
+                lastI=i;
+            }else if(startj!=-1){
+                
+                j=startj+1;
+                lastI++;
+                i=lastI;
+                
+                
+                
+            }else return false;
+            
+            
+        }
+        
+        while(j<n && p.charAt(j)=='*') j++;
+        return j==n;
+        
+        
     }
-
-    while (j < n && p.charAt(j) == '*')
-      j++;
-
-    return j == n;
-
-  }
-
 }
