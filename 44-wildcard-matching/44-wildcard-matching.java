f4588334c79342b0d1f824358699864762ac8560
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-11 18:29:35 +0530

    Time: 865 ms (5.01%), Space: 468.2 MB (5.03%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -1,32 +1,55 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int m=s.length(),n=p.length();        
-        boolean[][] dp=new boolean[m+1][n+1];
-        dp[0][0]=true;
+                 Boolean[][] dp
+=new Boolean[s.length()+1][p.length()+1];
         
-        for(int i=0;i<n;i++) if(p.charAt(i)=='*') dp[0][i+1]=dp[0][i];
-        
-        
-        for(int i=0;i<m;i++){
-            /*
-            abcdf
-            ab*f
-            
-            */
-            
-            for(int j=0;j<n;j++){
-                
-            if(p.charAt(j)=='?' || p.charAt(j)==s.charAt(i)) dp[i+1][j+1]=dp[i][j];
-            
-            if(p.charAt(j)=='*') dp[i+1][j+1]=dp[i+1][j] || dp[i][j+1];
-                
-                
-            }
-            
-            
-        }
-        
-        return dp[m][n];
+        return isMatch(s,p,dp,0,0);
+
         
     }
+    
+    private boolean isMatch(String s,String p,Boolean[][] dp,int sStart,int pStart){
+        
+        
+           if(p.isEmpty())
+            return s.isEmpty();
+        
+        
+        if(s.isEmpty())
+            return p.isEmpty() | isStarPattern(p);
+        String key=s+"@"+p;
+        
+        if(dp[sStart][pStart]!=null)
+            return dp[sStart][pStart];
+        
+        boolean firstMatch=s.charAt(0)==p.charAt(0) || p.charAt(0)=='?';
+        if(p.charAt(0)=='*'){
+            boolean res=  isMatch(s,p.substring(1),dp,sStart,pStart+1) || isMatch(s.substring(1),p,dp,sStart+1,pStart);
+            dp[sStart][pStart]=res;
+            return res;
+        }else
+        { 
+            boolean res=firstMatch && isMatch(s.substring(1),p.substring(1),dp,sStart+1,pStart+1);
+            dp[sStart][pStart]=res;
+            return res;
+        
+         
+        }
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    private boolean isStarPattern(String p){
+        for(int i=0;i<p.length();i++){
+            if(p.charAt(i)!='*')
+                return false;
+        }
+        return true;
+    }
+    
 }
