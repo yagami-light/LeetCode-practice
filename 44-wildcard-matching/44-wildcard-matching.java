c20321315098d47ba4fe0086dafcb3d94f8f30bb
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-11 16:32:14 +0530

    Time: 55 ms (25.19%), Space: 48.3 MB (48.70%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -1,33 +1,32 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int i=0,j=0;
-        int m=s.length(),n=p.length();
-        int startj=-1,lastI=-1;
-        while(i<m){
+        int m=s.length(),n=p.length();        
+        boolean[][] dp=new boolean[m+1][n+1];
+        dp[0][0]=true;
+        
+        for(int i=0;i<n;i++) if(p.charAt(i)=='*') dp[0][i+1]=dp[0][i];
+        
+        
+        for(int i=0;i<m;i++){
+            /*
+            abcdf
+            ab*f
             
-            if(j<n && (s.charAt(i)==p.charAt(j) || p.charAt(j)=='?')){
-                i++;
-                j++;
-            }else if(j<n && p.charAt(j)=='*'){
-                startj=j;
-                j++;
-                lastI=i;
-            }else if(startj!=-1){
+            */
+            
+            for(int j=0;j<n;j++){
                 
-                j=startj+1;
-                lastI++;
-                i=lastI;
+            if(p.charAt(j)=='?' || p.charAt(j)==s.charAt(i)) dp[i+1][j+1]=dp[i][j];
+            
+            if(p.charAt(j)=='*') dp[i+1][j+1]=dp[i+1][j] || dp[i][j+1];
                 
                 
-                
-            }else return false;
+            }
             
             
         }
         
-        while(j<n && p.charAt(j)=='*') j++;
-        return j==n;
-        
+        return dp[m][n];
         
     }
 }
