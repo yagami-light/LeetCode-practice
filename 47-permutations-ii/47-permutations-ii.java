9d8c832bae9a28a3f7cd4ae432cf9d6990b51cca
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-13 11:54:48 +0530

    Time: 4 ms (68.82%), Space: 47.8 MB (65.83%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,43 +1,38 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-         List<List<Integer>>  res=new ArrayList();
-        permute(nums,0,res);
+        
+        List<List<Integer>> res=new ArrayList();
+        Arrays.sort(nums);
+        boolean[] found=new boolean[nums.length];
+        permute(nums,new ArrayList(),res,found);
+        
         return res;
         
-        
     }
     
-    private void permute(int[] nums,int begin, List<List<Integer>>  res){
-        if(begin==nums.length){
+    private void permute(int[] nums,List<Integer> currList,List<List<Integer>> res,boolean[] found){
         
-            List<Integer> tempList=new ArrayList();
-            
-            for(int i=0;i<nums.length;i++){
-                tempList.add(nums[i]);
-            }
-            
-            res.add(tempList);
+        if(nums.length==currList.size()){
+            res.add(new ArrayList(currList));
             return;
         }
-        Set<Integer> set=new HashSet();
         
-        for(int i=begin;i<nums.length;i++){
-            // if(nums[i]==nums[begin] && i!=begin) continue;
-            if(set.contains(nums[i])) continue;
-            set.add(nums[i]);
-            swap(nums,i,begin);
-            permute(nums,begin+1,res);
-            swap(nums,i,begin);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==-100) continue;
+            if(i>0 && nums[i]==nums[i-1] && nums[i-1]!=-100) continue;
+            // found[i]=true;
+            currList.add(nums[i]);
+            int temp=nums[i];
+            nums[i]=-100;
+            permute(nums,currList,res,found);
+            currList.remove(currList.size()-1);
+            found[i]=false;
+            nums[i]=temp;
+            
         }
-        
         
         
     }
     
-    private void swap(int[] nums,int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-    }
     
 }
