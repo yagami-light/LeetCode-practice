4d718ce00cdf646b89648dc303b7638bb6510073
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-13 15:58:08 +0530

    Time: 1 ms (78.47%), Space: 41.6 MB (79.57%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,18 +1,41 @@
 class Solution {
-    public double myPow(double x, int n) {
-        if(x==1) return 1;
-        if(x==-1) return n%2==0 ? 1: -1;
-        System.out.println(Integer.MIN_VALUE);
-        if(n==Integer.MIN_VALUE) return 0;
-                
-        int sign=n>=0 ? 1 : -1;
-        double res=1;
-        n=Math.abs(n);
-        for(int i=0;i<n;i++){
-            res*=x;
-        }
-        
-        return sign==1 ? res : 1/res;
-        
+   public double powRecursion(double x, int n) {
+    if (n == 0) {
+        return 1;
     }
+    //偶数的情况
+    if ((n & 1) == 0) { 
+        double temp = powRecursion(x, n / 2);
+        return temp * temp;
+    } else { //奇数的情况
+        double temp = powRecursion(x, n / 2);
+        return temp * temp * x;
+    }
+}
+
+public double myPow(double x, int n) {
+    if (x == -1) {
+        if ((n & 1) != 0) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+    if (x == 1.0f)
+        return 1;
+
+    if (n == -2147483648) {
+        return 0;
+    }
+    double mul = 1;
+    if (n > 0) {
+        mul = powRecursion(x, n);
+    } else {
+        n = -n;
+        mul = powRecursion(x, n);
+        mul = 1 / mul;
+    }
+    return mul;
+}
+
 }
