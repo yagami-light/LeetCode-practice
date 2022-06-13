453b1b51834d8c4e8a83500445e1ebfb112ae79e
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-13 11:17:50 +0530

    Time: 76 ms (6.02%), Space: 47.8 MB (6.03%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,27 +1,45 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         
-        List<List<Integer>> res=new ArrayList();
-        res.add(new ArrayList());
-        
-        for(int i=0;i<nums.length;i++){
-            
-            int currSize=res.size();
-            for(int j=0;j<currSize;j++){
-                
-                for(int k=0;k<=i;k++){
-                List<Integer> tempList=new ArrayList(res.get(j));
-                tempList.add(k,nums[i]);
-                
-                res.add(tempList);
-                }
-                
-            }
-            
-            for(int k=0;k<currSize;k++) res.remove(0);
-            
-            
-        }
+         List<List<Integer>> res=new ArrayList();
+        permute(nums,0,res);
         return res;
+        
     }
+    
+    private void permute(int[] nums,int begin, List<List<Integer>> res){
+        System.out.println("beging index "+begin);
+        if(begin==nums.length){
+            System.out.println("alex");
+            
+            List<Integer> temp=new ArrayList();
+            for(int i=0;i<nums.length;i++){
+                System.out.println("alaska");
+                temp.add(nums[i]);
+               
+            }
+             res.add(new ArrayList(temp));
+            System.out.println("res is"+temp);
+            return;
+        }
+        
+        for(int i=begin;i<nums.length;i++){
+            swap(nums,i,begin);
+            permute(nums,begin+1,res);
+            swap(nums,i,begin);
+        }
+        
+    
+        
+        
+    }
+    
+    
+private void swap(int[] nums, int i, int begin) {
+    int temp = nums[i];
+    nums[i] = nums[begin];
+    nums[begin] = temp;
+}
+    
+    
 }
