33af01aff137d42364bfb68c35ad7a13f754b255
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-14 16:54:24 +0530

    Time: 9 ms (18.61%), Space: 42.4 MB (9.82%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.java
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -1,59 +1,46 @@
 class Solution {
+     public final Set<Integer> setCol=new HashSet();
+        public final Set<Integer> setDiag1=new HashSet();
+        public final Set<Integer> setDiag2=new HashSet();
     public int totalNQueens(int n) {
-        List<List<String>> res=new ArrayList();
-        
-        totalNQueens(new ArrayList(),res,n);
-        
-        return res.size();
-        
+       
+      return totalNQueensUtil(0,0,n);
     }
     
-    private void totalNQueens(List<Integer> currentQueen,List<List<String>> res,int n){
-        if(currentQueen.size()==n){
-            List<String> temp=new ArrayList();
-            for(int i:currentQueen){
-                char[] charc=new char[n];
-                Arrays.fill(charc,'.');
-                charc[i]='Q';
-                temp.add(new String(charc));
-            }
-            
-            
-            res.add(temp);
-            
-        }
-        
+    
+    private int totalNQueensUtil(int row,int count,int n){
         for(int col=0;col<n;col++){
-            if(!currentQueen.contains(col) && !diagAttack(currentQueen,col)){
+            
+            if(setCol.contains(col)) continue;
+            
+            int diag1=col-row;
+            int diag2=col+row;
+            if(setDiag1.contains(diag1)) continue;
+            
+            if(setDiag2.contains(diag2)) continue;
+            if(row==n-1){
+                count++;
                 
-                currentQueen.add(col);
-                totalNQueens(currentQueen,res,n);
-                currentQueen.remove(currentQueen.size()-1);
+            }else{
                 
-                
+                setCol.add(col);
+                setDiag1.add(diag1);
+                setDiag2.add(diag2);
+                 count=totalNQueensUtil(row+1,count,n);
+                setCol.remove(col);
+                setDiag1.remove(diag1);
+                setDiag2.remove(diag2);
+
                 
             }
+
+            
+            
         }
+        return count;
         
         
         
     }
-    
-    
-    private boolean diagAttack(List<Integer> currentQueen,int col){
-        
-        int curerntRow=currentQueen.size();
-        int currentCol=col;
-        
-        for(int row=0;row<currentQueen.size();row++){
-            
-            if(Math.abs(curerntRow-row)==Math.abs(currentCol-currentQueen.get(row)))
-                return true;
-            
-        }
-        return false;
-        
-    }
-    
     
 }
