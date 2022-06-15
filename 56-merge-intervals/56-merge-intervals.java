e7e67306e5dbf84d0b44b49a7c021a6b107ca60e
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-15 16:30:56 +0530

    Time: 15 ms (36.52%), Space: 55.1 MB (57.87%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -4,20 +4,16 @@ class Solution {
             return intervals;
         
         Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-        List<int[]> res=new ArrayList();
+        LinkedList<int[]> res=new LinkedList();
         int[] newinterval=intervals[0];
-        res.add(newinterval);
+        // res.add(newinterval);
         for(int[] currInterval:intervals){
-            
-            if(newinterval[1]>=currInterval[0]){
-                
-                newinterval[1]=Math.max(newinterval[1],currInterval[1]);
-                // newinterval[1]=-111;
+           
+            if(!res.isEmpty() && res.getLast()[1] >= currInterval[0]){
+                res.getLast()[1]=Math.max(res.getLast()[1],currInterval[1]);
                 
             }else{
-                
                 res.add(currInterval);
-                newinterval=currInterval;
             }
             
             
