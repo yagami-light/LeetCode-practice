fc9731981550a39511241db432ab4d4f942803e5
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-17 09:24:09 +0530

    Time: 9 ms (5.21%), Space: 47.7 MB (43.80%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -2,29 +2,26 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> res=new ArrayList();
         
-        
         for(int[] interval: intervals){
-            
-            if(newInterval[1]<interval[0]){
+            if(newInterval[1] <  interval[0]){
                 res.add(newInterval);
                 newInterval=interval;
             }
-            else if(interval[1] < newInterval[0]){
+            else if(newInterval[0]>interval[1]){
                 res.add(interval);
             }else{
-                
-                newInterval[0]=Math.min(newInterval[0],interval[0]);
-                newInterval[1]=Math.max(newInterval[1],interval[1]);
-                
-                
+                newInterval[0]=Math.min(interval[0],newInterval[0]);
+                newInterval[1]=Math.max(interval[1],newInterval[1]);
+
             }
-            
-            
-            
         }
         
+        System.out.println("new interval "+Arrays.toString(newInterval));
+        
         res.add(newInterval);
+        
         return res.toArray(new int[0][]);
+        
         
     }
 }
