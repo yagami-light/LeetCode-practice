4f89a25461738f8e5e75cec87e86430f27970f04
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-17 09:37:51 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (22.51%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -1,7 +1,26 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] splittedString=s.split(" ");
-      
-            return splittedString[splittedString.length-1].length();
+ 
+        int inputLen=s.length();
+        boolean isStarted=false;
+        int count=0;
+        for(int i=inputLen-1;i>=0;i--){
+            
+            if(s.charAt(i)==' ' && isStarted==false)
+                continue;
+            if(s.charAt(i)==' ' && isStarted==true){
+                return count;
+            }
+            
+            if(Character.isAlphabetic(s.charAt(i))){
+                count++;
+                isStarted=true;
+            }
+            
+        }
+        return count;
+        
+        
+        
     }
 }
