7fe594d717e50c63faa6d883380503cae7e0add3
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-17 14:38:55 +0530

    Time: 11 ms (20.62%), Space: 42 MB (27.26%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.java
+++ 60-permutation-sequence/60-permutation-sequence.java
@@ -1,42 +1,44 @@
 class Solution {
     public String getPermutation(int n, int k) {
         List<Integer> factList=new ArrayList();
-        for(int i=1;i<=n;i++)
-            factList.add(getFact(i));
-                System.out.println("factg list man "+factList);
-
         List<Integer> digits=new ArrayList();
+    
         for(int i=1;i<=n;i++)
+        {
             digits.add(i);
-        StringBuilder result=new StringBuilder();
+        }        
+        for(int i=1;i<=9;i++){
+            factList.add(fact(i));
+        }
         
-        getPermutation( n, k,factList,digits,result);
-        return result.toString();
+        System.out.println("facrt List "+factList);
+        
+        StringBuilder res=new StringBuilder();
+        getPermutation( n, k,factList,digits,res);
+        return res.toString();
+        
+        
         
     }
     
-    private void getPermutation(int n,int k,List<Integer> fact,List<Integer> digits,StringBuilder result){
-        System.out.println("n is "+n);
-        System.out.println("digit si "+digits);
-        System.out.println("k is "+k);
+    private void getPermutation(int n,int k,List<Integer> factList,List<Integer> digits,StringBuilder str){
+        
         if(n==1){
-            result.append(digits.get(0));
+            str.append(digits.get(0));
             return;
         }
-        System.out.println("factg list"+fact);
-        int factNumber=fact.get(n-1-1);
-        System.out.println("factNumber is "+factNumber);
-        int index= k/factNumber;
-        // index=index-1;
-        if(k % factNumber==0){
-            index=index-1;
-        }
-        System.out.println("index is "+(index));
         
-        result.append(digits.get(index));
+        int factNumber=factList.get(n-1-1);
+        int index=k/factNumber;
+        if(k % factNumber == 0){
+            index--;
+        }
+        
+        str.append(digits.get(index));
         digits.remove(index);
         k-=factNumber*(index);
-        getPermutation(n-1,k,fact,digits,result);
+        getPermutation(n-1,k,factList,digits,str);
+        
         
         
         
@@ -45,8 +47,8 @@ class Solution {
     
     
     
-    int getFact(int i){
-        return i==1 ? 1 : i*getFact(i-1);
+    private int fact(int i){
+        return i==1 ? 1 : fact(i-1)*i;
     }
     
 }
