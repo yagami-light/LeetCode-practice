dddb002914650300b7dfea92af2f7fd0cb8a6e9c
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-20 09:16:11 +0530

    Time: 10 ms (5.88%), Space: 44.5 MB (99.58%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,35 +1,36 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-    int m=grid.length;
-    int n=grid[0].length;
-    int[][] dp=new int[m][n];
-    
-    for(int i=0;i<m;i++){
+        int m=grid.length;
+        int n=grid[0].length;
         
-        for(int j=0;j<n;j++){
-            if(i==0 && j==0 ){
-                dp[i][j]=grid[i][j];
-            }
-            else if(i==0){
-                dp[i][j]=grid[i][j]+dp[i][j-1];
-            }
-            else if(j==0){
-                dp[i][j]=grid[i][j]+dp[i-1][j];   
-            }
-            else{
-                          dp[i][j]=grid[i][j]+Math.min(( dp[i-1][j]),(dp[i][j-1]));
-  
-            }
+        int[] row=new int[n];
+        
+        for(int i=0;i<m;i++){
             
+            int[] newRow=new int[n];
+            for(int j=0;j<n;j++){
+                
+                if(j==0 && i==0){
+                    newRow[j]=grid[i][j];
+                }
+                else if(i==0)
+                    newRow[j]=grid[i][j]+newRow[j-1];
+                else if(j==0)
+                    newRow[j]=grid[i][j]+row[j];
+                else {
+                    // System.out.println("prev min "+newRow[j-1]+" and row is "+row[i-1]);
+                    newRow[j]=grid[i][j]+Math.min(newRow[j-1],row[j]);
+                }
+                
+                
+            }
+            System.out.println("newRow print "+Arrays.toString(newRow));
+            row=newRow;
             
         }
         
-    }
+        return row[n-1];
         
-    // System.out.println("printig dp array "+ Arrays.deepToString(dp));
-        
-        
-        return dp[m-1][n-1];
         
         
     }
