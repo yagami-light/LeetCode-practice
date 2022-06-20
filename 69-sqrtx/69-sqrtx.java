514c7deb9863c0afc88e87cb1334e9eb7a5edaac
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-20 16:15:02 +0530

    Time: 3 ms (32.35%), Space: 42.2 MB (6.58%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,10 +1,25 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0) return 0;
-        int i=1;
-        while(i <= x/i)
-            i++;
-        return i-1;
+        int ans=0;
+        long start=0,end=Integer.MAX_VALUE;
+        while(start<=end){
+            
+            long mid=(start+end)/2;
+            if(mid* mid <= x){
+                ans=(int)mid;
+                start=mid+1;
+            }else 
+            {
+                end=mid-1;
+            }
+            
+            
+            
+            
+        }
+        return ans;
+        
+        
         
     }
 }
