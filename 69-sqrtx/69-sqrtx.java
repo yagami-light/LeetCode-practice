152d90de1ec7a4c9c8ea56d3bed9e0d61ed4c691
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-20 15:42:33 +0530

    Time: 143 ms (5.05%), Space: 41.4 MB (51.40%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,12 +1,10 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==1 || x==0)
-            return x;
-       for(long i=1;i<=(x/2+1);i++)
-           if(i*i<=x && (i+1)*(i+1)>x ) 
-               return (int)i;
+        if(x==0) return 0;
+        int i=1;
+        while(i <= x/i)
+            i++;
+        return i-1;
         
-        
-        return -1;
     }
 }
