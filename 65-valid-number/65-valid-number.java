9d25376acb7fec62b9baa2aaa182f3246436eafa
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-20 10:18:20 +0530

    Time: 3 ms (70.90%), Space: 43.2 MB (53.42%) - LeetHub

--- 65-valid-number/65-valid-number.java
+++ 65-valid-number/65-valid-number.java
@@ -1,46 +1,38 @@
 class Solution {
     public boolean isNumber(String s) {
-        boolean digitSeen=false,eSeen=false,dotSeen=false;
+        boolean dotSeen=false,digitSeen=false,eSeen=false;
         int countPlusMinus=0;
+        int n=s.length();
         
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(Character.isDigit(c) || Character.toLowerCase(c)=='e' || c=='+' || c=='-' || c=='.'){
-                System.out.println("character is "+c);
-                if(Character.isDigit(c)){
-                    digitSeen=true;
-                }
-                else if(c=='+' || c=='-'){
-                    if(countPlusMinus==2)
-                        return false;
-                    if(i>0 && s.charAt(i-1)!='e' && s.charAt(i-1)!='E')
-                        return false;
-                    
-                    if(i==s.length()-1)
-                        return false;
-                    
-                    
-                    
-                    countPlusMinus++;
-                }
-                else if(c=='.'){
-                    if(eSeen || dotSeen)
-                        return false;
-                    
-                    if(i==s.length()-1 && digitSeen==false)
-                        return false;
-                    dotSeen=true;
-                }
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            
+            if(Character.isDigit(ch)){
                 
-                else if(c=='e' || c=='E'){
-                    if(eSeen || digitSeen==false || i==s.length()-1)
+                digitSeen=true;
+                
+            }
+            else if(Character.toLowerCase(ch)=='e'){
+                    if(eSeen || i==n-1 || !digitSeen)
                         return false;
-                    eSeen=true;
-                    
-                }
-                else
+                
+                eSeen=true;
+                
+            }
+            else if(ch=='.'){
+                if(eSeen|| dotSeen || (!digitSeen && i==n-1))
+                    return false;
+                dotSeen=true;
+                
+            }else if(ch=='+' || ch=='-'){   
+                if(countPlusMinus==2)
+                    return false;
+                if(i>0 && s.charAt(i-1)!='e' && s.charAt(i-1)!='E')
+                    return false;
+                if(i==n-1)
                     return false;
                 
+                countPlusMinus++;
                 
             }
             else
@@ -50,5 +42,8 @@ class Solution {
         }
         
         return true;
+        
+        
+        
     }
 }
