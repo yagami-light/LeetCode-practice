f212e3772f11277bb4da4f2c9470133407c62b6d
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-21 11:48:35 +0530

    Time: 8 ms (66.17%), Space: 45.5 MB (14.47%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,35 +1,43 @@
 class Solution {
-    Map<String,Integer> map=new HashMap();
     public int minDistance(String word1, String word2) {
-        
-        
-        return minDistanceUtil(word1,word2);
-        
-    }
-    
-    public int minDistanceUtil(String word1,String word2){
-    
         if(word1.length()==0 && word2.length()==0)
             return 0;
-        if(word1.length()==0 )
+        if(word1.length()==0)
             return word2.length();
         if(word2.length()==0)
             return word1.length();
-        String key=word1+"@"+word2;
-        if(map.containsKey(key))
-            return map.get(key);
-        int minOps= 1+Math.min(minDistanceUtil(word1.substring(1),word2.substring(1)),(Math.min(minDistanceUtil(word1.substring(1),word2) , minDistanceUtil(word1,word2.substring(1)))));
-        int noOps=Integer.MAX_VALUE;
-        if(word1.charAt(0)==word2.charAt(0))
-         noOps=minDistanceUtil(word1.substring(1),word2.substring(1));
+        int m=word1.length();
+        int n=word2.length();
+        int[][] dp=new int[m+1][n+1];
         
-        map.put(key, Math.min(minOps,noOps));
-        return map.get(key);
         
+        for(int i=0;i<=m;i++){
+            
+            
+            for(int j=0;j<=n;j++){
+                
+                if(i==0 && j==0)
+                    dp[i][j]=0;
+                else if(i==0)
+                    dp[i][j]=j;
+                else if(j==0)
+                    dp[i][j]=i;
+                else{
+                    
+                    dp[i][j]=1+Math.min(dp[i-1][j-1] ,Math.min(dp[i-1][j] , dp[i][j-1]));
+                    if(word1.charAt(i-1)==word2.charAt(j-1))
+                    dp[i][j]=Math.min(dp[i][j],dp[i-1][j-1]);
+                    
+                    
+                }
+                
+            }
+            
+        }
+        
+        return dp[m][n];
         
         
         
     }
-    
-    
 }
