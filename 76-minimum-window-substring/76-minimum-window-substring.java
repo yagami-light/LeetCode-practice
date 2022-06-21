89f158a8233f18f9486394642b49980ea586dc5f
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-21 19:08:25 +0530

    Time: 6 ms (87.46%), Space: 43.8 MB (73.34%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,44 +1,39 @@
 class Solution {
     public String minWindow(String s, String t) {
-        
-        int count=t.length();
         int[] map=new int[256];
+        int count=t.length();
         for(char c:t.toCharArray())
             map[c]++;
-        int end=0;
-        int start=0;
+        
+        int end=0,start=0;
         int n=s.length();
         int minLen=Integer.MAX_VALUE;
-        int startI=0;
+        int minStart=0;
         while(end<n){
             
-            int ch=s.charAt(end);
-            if(map[ch]>0)
-                count--;
-            map[ch]--;
+            char c1=s.charAt(end);
+            if(map[c1]>0) count--;
+            map[c1]--;
             end++;
             while(count==0){
                 
-                if(minLen> (end-start)){
-                    startI=start;
+                if(minLen>(end-start)){
+                    minStart=start;
                     minLen=end-start;
                 }
                 
-                int ch2=s.charAt(start);
-                map[ch2]++;
-                if(map[ch2]>0) count++;
+                char c2=s.charAt(start);
+                map[c2]++;
+                if(map[c2]>0) count++;
                 start++;
-                
                 
                 
             }
             
             
             
-            
-            
         }
-        return minLen==Integer.MAX_VALUE ? "" : s.substring(startI,startI+minLen);
+        return minLen==Integer.MAX_VALUE ? "" : s.substring(minStart,minStart+minLen);
         
     }
 }
