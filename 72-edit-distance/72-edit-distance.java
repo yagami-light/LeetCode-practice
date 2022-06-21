139f3bcc170ee428180b6fbd8134667dfd461488
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-21 12:03:14 +0530

    Time: 16 ms (6.72%), Space: 46.9 MB (5.00%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -10,32 +10,41 @@ class Solution {
         int n=word2.length();
         int[][] dp=new int[m+1][n+1];
         
+        int[] row=new int[n+1];
+        for(int i=0;i<row.length;i++)
+            row[i]=i;
         
-        for(int i=0;i<=m;i++){
+        System.out.println("printing array : "+Arrays.toString(row));
+        
+        
+        for(int i=1;i<=m;i++){
             
-            
+            int[] newRow=new int[n+1];
             for(int j=0;j<=n;j++){
                 
                 if(i==0 && j==0)
-                    dp[i][j]=0;
-                else if(i==0)
-                    dp[i][j]=j;
+                    newRow[j]=0;
+                
                 else if(j==0)
-                    dp[i][j]=i;
+                    newRow[j]=i;
                 else{
                     
-                    dp[i][j]=1+Math.min(dp[i-1][j-1] ,Math.min(dp[i-1][j] , dp[i][j-1]));
+                    newRow[j]= 1 + Math.min(row[j-1],Math.min(row[j],newRow[j-1]));
+                    
+                    // dp[i][j]=1+Math.min(dp[i-1][j-1] ,Math.min(dp[i-1][j] , dp[i][j-1]));
                     if(word1.charAt(i-1)==word2.charAt(j-1))
-                    dp[i][j]=Math.min(dp[i][j],dp[i-1][j-1]);
+                    newRow[j]= Math.min(newRow[j],row[j-1]);
+                    // dp[i][j]=Math.min(dp[i][j],dp[i-1][j-1]);
                     
                     
                 }
                 
             }
+            row=newRow;
             
         }
         
-        return dp[m][n];
+        return row[n];
         
         
         
