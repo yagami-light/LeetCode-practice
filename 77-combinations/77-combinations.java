0badf1cb3423c8efca56c950956323d2bb072953
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-21 19:35:48 +0530

    Time: 37 ms (42.65%), Space: 54.5 MB (68.04%) - LeetHub

---
+++ 77-combinations/77-combinations.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+       List<List<Integer>> res=new ArrayList();
+        combineUtil(n,k,new ArrayList(),res,1);
+        return res;
+    }
+    
+    private void combineUtil(int n,int k,List<Integer> currList,List<List<Integer>> res,int index){
+        if(currList.size()==k){
+            
+            res.add(new ArrayList(currList));
+            return;
+            
+        }
+        
+        for(int i=index;i<=n;i++){
+            
+            currList.add(i);
+            combineUtil(n,k,currList,res,i+1);
+            currList.remove(currList.size()-1);
+            
+            
+        }
+        
+        
+        
+        
+    }
+    
+    
+}
