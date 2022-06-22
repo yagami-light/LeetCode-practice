c74de822aa0fb0128575a7fc649ad7fb3f4cc30b
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-22 10:58:59 +0530

    Time: 1 ms (87.25%), Space: 42.7 MB (80.41%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,23 +1,27 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
+        
         List<List<Integer>> res=new ArrayList();
+        int bit_length=nums.length;
+        int ans_length=1 << bit_length;
         
-        res.add(new ArrayList());
-        
-        for(int i=0;i<nums.length;i++){
-             List<List<Integer>> tempList=new ArrayList();
+        for(int i=0;i<ans_length;i++){
             
-            for(List<Integer> list:res){
-                List<Integer> temp=new ArrayList(list);
-                temp.add(nums[i]);
-                tempList.add(temp);
+            List<Integer> temp=new ArrayList();
+            int count=0;
+            int i_copy=i;
+            while(i_copy!=0){
+                if((i_copy & 1) ==1){
+                    temp.add(nums[count]);
+                }
+                count++;
+                i_copy=i_copy >>1;
             }
-            res.addAll(tempList);
+            res.add(temp);
+            
         }
         
-        
         return res;
-        
         
     }
 }
