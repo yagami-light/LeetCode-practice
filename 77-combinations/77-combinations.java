805be04019cae46bc91fd9e9d894abed5ea8141d
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-22 09:17:52 +0530

    Time: 14 ms (84.02%), Space: 55.2 MB (30.37%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,31 +1,46 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-       List<List<Integer>> res=new ArrayList();
-        combineUtil(n,k,new ArrayList(),res,1);
+        if(n==0 || k>n || k==0) return  Collections.emptyList();
+        // List<Integer> currList=new ArrayList();
+        List<List<Integer>> res=new ArrayList();
+        for(int i=1;i<=n+1-k;i++)
+            res.add(Arrays.asList(i));
+        
+        
+        for(int i=2;i<=k;i++){
+             List<List<Integer>> temp=new ArrayList();
+            for(List<Integer> list: res){
+                
+                
+               for (int m = list.get(list.size() - 1) + 1; m <= n - (k - (i - 1)) + 1; m++){
+                    
+                    List<Integer> tempList=new ArrayList(list);
+                    tempList.add(m);
+                    
+                    temp.add(tempList);
+                    
+                    
+                }
+                
+            }
+            
+            
+            res=temp;
+            
+            
+        }
+        
+     
+        
+        
         return res;
-    }
-    
-    private void combineUtil(int n,int k,List<Integer> currList,List<List<Integer>> res,int index){
-        if(currList.size()==k){
-            
-            res.add(new ArrayList(currList));
-            return;
-            
-        }
+     
         
-        for(int i=index;i<=(n-(k-currList.size())+1);i++){
-            
-            currList.add(i);
-            combineUtil(n,k,currList,res,i+1);
-            currList.remove(currList.size()-1);
-            
-            
-        }
+        
+        
         
         
         
         
     }
-    
-    
 }
