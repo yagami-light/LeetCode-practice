83840e7548fd9372137bd0cbe0518c14ac95253c
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-22 09:30:10 +0530

    Time: 14 ms (84.02%), Space: 56.6 MB (20.27%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,44 +1,18 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        if(n==0 || k>n || k==0) return  Collections.emptyList();
-        // List<Integer> currList=new ArrayList();
-        List<List<Integer>> res=new ArrayList();
-        for(int i=1;i<=n+1-k;i++)
-            res.add(Arrays.asList(i));
-        
-        
-        for(int i=2;i<=k;i++){
-             List<List<Integer>> temp=new ArrayList();
-            for(List<Integer> list: res){
-                
-                
-               for (int m = list.get(list.size() - 1) + 1; m <= n - (k - (i - 1)) + 1; m++){
-                    
-                    List<Integer> tempList=new ArrayList(list);
-                    tempList.add(m);
-                    
-                    temp.add(tempList);
-                    
-                    
-                }
-                
-            }
+        if(k==0 || n==k){
+            List<Integer> list=new LinkedList();
+            for(int i=1;i<=k;i++)
+                list.add(i);
             
             
-            res=temp;
-            
-            
+            return new LinkedList(Arrays.asList(list));
         }
         
-     
-        
-        
+        List<List<Integer>> res=combine(n-1,k-1);
+        res.forEach(i->i.add(n));
+        res.addAll(combine(n-1,k));
         return res;
-     
-        
-        
-        
-        
         
         
         
