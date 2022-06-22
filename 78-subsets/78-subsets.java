c3ea4d2865890edc6a5c50961ef1535b0c5eaeab
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-22 10:47:17 +0530

    Time: 2 ms (34.74%), Space: 43.7 MB (17.52%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,35 +1,23 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        
-        
         List<List<Integer>> res=new ArrayList();
         
-        subSetsUtil(nums,new ArrayList(),0,res);
+        res.add(new ArrayList());
+        
+        for(int i=0;i<nums.length;i++){
+             List<List<Integer>> tempList=new ArrayList();
+            
+            for(List<Integer> list:res){
+                List<Integer> temp=new ArrayList(list);
+                temp.add(nums[i]);
+                tempList.add(temp);
+            }
+            res.addAll(tempList);
+        }
+        
         
         return res;
         
         
     }
-    
-    private void subSetsUtil(int[] nums,List<Integer> currList,int index,List<List<Integer>> res){
-        
-        res.add(new ArrayList(currList));
-        
-        for(int i=index;i<nums.length;i++){
-            if(currList.contains(nums[i])) continue;
-            currList.add(nums[i]);
-            subSetsUtil(nums,currList,i+1,res);
-                              
-            currList.remove(currList.size()-1);
-            
-            
-        }
-        
-        
-        
-        
-        
-    }
-    
-    
 }
