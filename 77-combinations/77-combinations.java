13954a9b155ba57df1ffd409818e9e1bd691d031
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-22 10:25:30 +0530

    Time: 161 ms (5.06%), Space: 215.7 MB (5.05%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,18 +1,41 @@
 class Solution {
+    
+    // combine(n,k)=combine(n-1,k-1) + combine(n-1,k)
+    
+    
     public List<List<Integer>> combine(int n, int k) {
-        if(k==0 || n==k){
-            List<Integer> list=new LinkedList();
-            for(int i=1;i<=k;i++)
-                list.add(i);
-            
-            
-            return new LinkedList(Arrays.asList(list));
+        List<List<Integer>>[][] dp=new List[n+1][k+1];
+        
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0]=new ArrayList();
+            dp[i][0].add(new ArrayList());
         }
         
-        List<List<Integer>> res=combine(n-1,k-1);
-        res.forEach(i->i.add(n));
-        res.addAll(combine(n-1,k));
-        return res;
+        for(int i=1;i<=n;i++){
+            
+            
+            for(int j=1;j<=i && j<=k;j++){
+                
+                
+                dp[i][j]=new ArrayList();
+                if(i>j)
+                dp[i][j].addAll(dp[i-1][j]);
+                
+                for(List<Integer> list:dp[i-1][j-1]){
+                    
+                    List<Integer> temp=new ArrayList(list);
+                    temp.add(i);
+                    dp[i][j].add(temp);
+                    
+                }
+                
+            }
+            
+        }
+        
+        
+        return dp[n][k];
         
         
         
