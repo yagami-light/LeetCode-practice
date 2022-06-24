a49db5cb2e18ae7ca66bf68ce207e6531b33e79e
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-06-24 09:34:09 +0530

    Time: 89 ms (73.23%), Space: 75.8 MB (79.58%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,9 +1,7 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
+    public int largestRectangleArea(int[] nums) {
         
-        
-        return largestRectangleAreaUtil(heights,0,heights.length-1);
-        
+        return largestRectangleAreaUtil(nums,0,nums.length-1);
     }
     
     private int largestRectangleAreaUtil(int[] nums,int start,int end){
@@ -13,46 +11,44 @@ class Solution {
         int mid=(start+end)/2;
         
         
-        int left=largestRectangleAreaUtil(nums,start,mid);
+        int area1=largestRectangleAreaUtil(nums,start,mid);
+        int area2=largestRectangleAreaUtil(nums,mid+1,end);
         
+        int area3=calculateMidArea(nums,start,end,mid);
         
-        int right=largestRectangleAreaUtil(nums,mid+1,end);
-        
-        int middle=calculateMiddle(nums,start,end,mid);
-        
-        
-        return Math.max(middle,Math.max(left,right));
+        return Math.max(area3,Math.max(area1,area2));
         
     }
     
-    
-    
-    private int calculateMiddle(int[] nums,int start,int end,int mid){
+    private int calculateMidArea(int[] nums,int start,int end,int mid){
         int i=mid;
         int j=mid+1;
-        
-        
-        int minH=Math.min(nums[i],nums[j]);
-        int area=minH * 2;
-        
+        int minHeight=Math.min(nums[i],nums[j]);
+        int width=2* minHeight;
+        int area=0;
         while(i>=start && j<=end){
-            minH=Math.min(minH,Math.min(nums[i],nums[j]));
-            area=Math.max(area,minH * (j-i+1));
-            if(i==start){
+            
+            minHeight=Math.min(minHeight,Math.min(nums[i],nums[j]));
+            area=Math.max(area,minHeight * (j-i+1));
+            if(i==start)
                 j++;
-            }else if(j==end){
+            else if(j==end)
                 i--;
-            }
-            else if(nums[i-1]>=nums[j+1]){
-                i--;
-                
-            }else
+            else if(nums[i-1] <= nums[j+1])
                 j++;
+            else
+                i--;
+            
+            
         }
         
         
         return area;
+        
+        
     }
+    
+    
     
     
 }
