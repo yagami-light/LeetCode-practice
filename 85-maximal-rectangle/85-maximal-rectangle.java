0ea3857c68687981ec1772994fad1e829bf3a328
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-07-03 18:08:30 +0530

    Time: 45 ms (44.87%), Space: 55 MB (32.06%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.java
+++ 85-maximal-rectangle/85-maximal-rectangle.java
@@ -1,47 +1,41 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        int row=matrix.length;
-        int col=matrix[0].length;
+        int numRow=matrix.length;
+        int numCol=matrix[0].length;
         
-        int[][] width=new int[row][col];
-        int maxArea=0;
-        
-        for(int i=0;i<row;i++){
+        int[][] width=new int[numRow][numCol];
+        int area=0;
+        for(int row=0;row<numRow;row++){
             
-            
-            for(int j=0;j<col;j++){
+            for(int col=0;col<numCol;col++){
                 
-                if(matrix[i][j]=='0') width[i][j]=0;
+                if(matrix[row][col]=='0') width[row][col]=0;
                 else{
                     
-                    if(j==0) width[i][j]=1;
-                    else
-                        width[i][j]=1+width[i][j-1];
-                    
-                                     
-                 // System.out.print(width[i][j]+" ");   
-                int minWidth=width[i][j];
-                    
-                    
-                for(int up_row=i;up_row>=0;up_row--){
-                    
-                    int height=i-up_row+1;
-                    minWidth=Math.min(minWidth,width[up_row][j]);
-                    // System.out.println(" height is :"+height+" and minWidth is :"+minWidth);
-                    maxArea=Math.max(maxArea,height*minWidth);
-                    
-                }
-                    
+                    if(col==0) width[row][col]=1;
+                    else{
+                        
+                        width[row][col]=1+width[row][col-1];
+                        
+                    }
                 }
                 
+                int minWidth=width[row][col];
+                
+                for(int up_row=row;up_row>=0;up_row--){
+                    
+                    int height=row-up_row+1;
+                    minWidth=Math.min(minWidth,width[up_row][col]);
+                    area=Math.max(area,height*minWidth);
+                    
+                }
                 
                 
             }
-            // System.out.println();
             
         }
-        System.out.println(Arrays.deepToString(width));
-        return maxArea;
+        
+        return area;
         
         
     }
