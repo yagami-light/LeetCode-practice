e30409468bd327398af8894e8f7354314f25d087
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-07-05 12:39:02 +0530

    Time: 9 ms (65.84%), Space: 56 MB (32.72%) - LeetHub

--- 89-gray-code/89-gray-code.java
+++ 89-gray-code/89-gray-code.java
@@ -1,19 +1,13 @@
 class Solution {
     public List<Integer> grayCode(int n) {
-        List<Integer> res=new ArrayList();
-        res.add(0);
-        
-        for(int i=0;i<n;i++){
-            int size=res.size();
-            for(int k=size-1;k>=0;k--){
-                
-                res.add(res.get(k) | 1<<i);
-                
-                
-                
-            }
+         List<Integer> list=new ArrayList();
+        for(int i=0;i< 1<<n;i++){
+            
+            list.add(i ^ i>>1);
+            
+            
         }
-        return res;
+        return list;
         
     }
 }
