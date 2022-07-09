881b22a3d96eb08d84d759e8bf3d43964f563f24
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-07-09 12:07:25 +0530

    Time: 114 ms (5.45%), Space: 60.1 MB (5.01%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.java
+++ 93-restore-ip-addresses/93-restore-ip-addresses.java
@@ -1,60 +1,32 @@
 class Solution {
     public List<String> restoreIpAddresses(String s) {
-        
-        Set<String> res=new HashSet();
-        StringBuilder str=new StringBuilder(s);
-        restoreIpAddressesBackTrack(s,res,str,0,3);
-        // System.out.println(isVallidIpAddress("1.01.02.3"));
-        // System.out.println("parsing : "+Integer.parseInt("01"));
-        return new ArrayList(res);
-    }
-    
-    private void restoreIpAddressesBackTrack(String s,Set<String> res,StringBuilder str,int index,int dotCount){
-        // System.out.println("dot Count :"+dotCount+" index "+index +" stringBuilder :"+str);
-        if(dotCount<0 || index>=s.length()) return;
-        
-        if(dotCount==0 && isVallidIpAddress(str.toString())){
-            res.add(str.toString());
-            return;
-        }
-        
-        for(int i=index;i<=s.length();i++){
-            
-            str.insert(i,".");
-            restoreIpAddressesBackTrack(s,res,str,index+1,dotCount-1);
-            str.deleteCharAt(i);
-            
-            
-        }
-        
-        
-        
-        
-        
-    }
+        List<String> res=new ArrayList();
+        for(int i=0;i<=255;i++){
+            if(s.startsWith(i+"")){
+                // System.out.println(" i is :"+i);
+            for(int j=0;j<=255;j++){
+                if(s.startsWith(""+i+j)){
+                                    // System.out.println(" i+j is :"+i+j);
 
+                for(int k=0;k<=255;k++){
+                    if(s.startsWith(""+i+j+k)){
+                    // System.out.println(" i+j+k is :"+i+j+k);
+
+                    for(int l=0;l<=255;l++){
+                        // System.out.println(" i+j+k+l is :"+i+j+k+l);
     
-    
-    
-    private boolean isVallidIpAddress(String s){
-        String[] splittedIp=s.split("\\.");
-        // System.out.println("length is "+splittedIp.length);
-        if(splittedIp.length !=4)
-            return false;
-        for(String str: splittedIp){
-            // System.out.println(" str is :"+str);
-            if(str.length()==0) return false;
-            if(!str.equals("0") && str.startsWith("0")) return false;
-            long num=Long.parseLong(str);
-            if(num>=0 && num<=255) continue;
-            else
-                return false;
-            
-            
+                        if(s.equals(""+i+j+k+l))
+                            res.add(i+"."+j+"."+k+"."+l);
+                    }
+                    }
+                }
+            }
+            }
+            } 
         }
-        return true;
+        
+        return res;
         
         
     }
-    
 }
