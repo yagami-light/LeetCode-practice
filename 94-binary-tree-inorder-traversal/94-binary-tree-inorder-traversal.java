f15b635c8a4eae1b9f28178e1a9210ea519d7cca
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-07-09 18:06:11 +0530

    Time: 1 ms (48.73%), Space: 42.2 MB (57.99%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -15,25 +15,25 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
+        
         List<Integer> res=new ArrayList();
         Stack<TreeNode> stack=new Stack();
-        
         TreeNode curr=root;
         while(curr!=null || !stack.isEmpty()){
             
             while(curr!=null){
                 stack.add(curr);
                 curr=curr.left;
-                
             }
-            
             curr=stack.pop();
             res.add(curr.val);
             curr=curr.right;
             
-            
         }
-        return res;
+        
+        
+        
+        return res;    
         
     }
 }
