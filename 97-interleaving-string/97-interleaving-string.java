ab523255993114dfd73e00977ee8538779d47f2e
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-07-11 11:52:23 +0530

    Time: 3 ms (71.16%), Space: 40.4 MB (96.01%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -3,7 +3,7 @@ class Solution {
         if(s1.length() + s2.length() != s3.length())
         return false;
         
-        return isInterleaveUtil(s1,s2,s3);
+        return isInterleaveUtil2(s1,s2,s3);
     }
     
     private boolean isInterleaveUtil(String s1,String s2,String s3){
@@ -35,5 +35,38 @@ class Solution {
         return dp[s1.length()][s2.length()];
         
     }
+    
+     private boolean isInterleaveUtil2(String s1,String s2,String s3){
+        boolean[] dp=new boolean[s2.length()+1];
+        for(int i=0;i<=s1.length();i++){
+            
+            for(int j=0;j<=s2.length();j++){
+              
+                
+                if(i==0 && j==0) dp[j]=true;
+                else if(i==0){
+                    dp[j]=dp[j-1] && s2.charAt(j-1) == s3.charAt(i+j-1);
+                }
+                else if(j==0){
+                    dp[j]=dp[j] && s1.charAt(i-1) == s3.charAt(i+j-1);
+
+                }
+                else{
+                    
+                    dp[j]=(dp[j-1] && s2.charAt(j-1) == s3.charAt(i+j-1))  || (dp[j] && s1.charAt(i-1) == s3.charAt(i+j-1));
+
+                    
+                }
+                
+              
+            }
+            
+        }
+        return dp[s2.length()];
+        
+    }
+    
+    
+    
     
 }
