d6a66078ecc12d073a6ca10165601fd1629fadd5
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-07-12 08:07:53 +0530

    Time: 1 ms (62.83%), Space: 40.1 MB (98.41%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -15,40 +15,28 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        Queue<TreeNode> stack1=new LinkedList();
-        LinkedList<TreeNode> stack2=new LinkedList();
-        
         if(root==null) return true;
-        if(root!=null && root.left==null && root.right==null) return true;
-        TreeNode leftNode=root.left;
-        TreeNode rightNode=root.right;
-        stack1.add(leftNode);
-        stack2.add(rightNode);
-        while(!stack1.isEmpty() && !stack2.isEmpty()){
+        
+        Queue<TreeNode> queue=new LinkedList();
+        queue.add(root.left);
+        queue.add(root.right);
+        
+        while(!queue.isEmpty()){
             
-            int size1=stack1.size();
-            int size2=stack2.size();
-            if(size1!=size2) return false;
-            TreeNode node1=stack1.remove();
-            TreeNode node2=stack2.remove();
-            // System.out.println("value left node :"+node1.val+" value right node :"+node2.val);
-            if(node1==null && node2==null) return true;
-            if(node1==null || node2==null) return false;
-            if(node1.val!=node2.val) return false;
-            if(node1.left!=null)
-                stack1.add(node1.left);
-            if(node2.right!=null)
-                stack2.add(node2.right);
+            TreeNode node1=queue.remove();
+            TreeNode node2=queue.remove();
+            if(node1==null && node2==null) continue;
+            if(node1==null || node2==null || node1.val!=node2.val) return false;
             
-            if(stack1.size()!=stack2.size()) return false;
-            if(node1.right!=null)
-                stack1.add(node1.right);
-            if(node2.left!=null)
-                stack2.add(node2.left);
+            queue.add(node1.left);
+            queue.add(node2.right);
+            
+            queue.add(node1.right);
+            queue.add(node2.left);
+            
             
         }
-        
-        return stack1.size()==stack2.size();
+        return true;
         
     }
 }
