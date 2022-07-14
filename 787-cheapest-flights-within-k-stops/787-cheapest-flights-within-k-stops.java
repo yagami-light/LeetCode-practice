802de29275ad05656eaa9194d768f286710fab59
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-07-14 13:20:42 +0530

    Time: 9 ms (53.06%), Space: 46.4 MB (78.14%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -1,29 +1,26 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         int[][] dp=new int[k+2][n];
+        for(int[] array:dp)
+            Arrays.fill(array,Integer.MAX_VALUE);
         for(int i=0;i<dp.length;i++)
-        Arrays.fill(dp[i],Integer.MAX_VALUE);
+            dp[i][src]=0;
         
-        for(int i=0;i<=k+1;i++){
-         dp[i][src]=0;
-        }
         
-        for(int i=1;i<=k+1;i++){
+        for(int i=1;i<dp.length;i++){
             
             
             for(int[] flight:flights){
-                
                 int u=flight[0];
                 int v=flight[1];
                 int w=flight[2];
                 
-                if(dp[i-1][u] != Integer.MAX_VALUE){
-                    dp[i][v]=Math.min(dp[i][v],dp[i-1][u]+w);
-                }
+                if(dp[i-1][u]!=Integer.MAX_VALUE)
+                dp[i][v]=Math.min(dp[i][v],dp[i-1][u]+w);
+                
             }
             
         }
-        
         
         return dp[k+1][dst]==Integer.MAX_VALUE ? -1 : dp[k+1][dst];
         
