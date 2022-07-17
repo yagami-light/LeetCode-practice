1f6a94cd603b3d55c137341d7e61dd7a46a311e6
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-07-17 11:17:30 +0530

    Time: 1 ms (60.82%), Space: 43.8 MB (35.37%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -16,45 +16,18 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         
-        Deque<TreeNode> stack=new LinkedList();
+        return hasPathSumUtil(root,targetSum,0);
         
-        int currSum=0;
-        TreeNode currNode=root;
-        TreeNode prev=null;
-        while(!stack.isEmpty() || currNode!=null){
-            
-            while(currNode!=null){
-                
-                stack.push(currNode);
-                currSum+=currNode.val;
-
-                currNode=currNode.left;
-            }
-            
-            currNode=stack.peek();
-            if(currNode.left==null && currNode.right==null && targetSum==currSum)
-                return true;
-            
-            
-            if(currNode.right!=null && prev!=currNode.right)
-                currNode=currNode.right;
-            else{
-                
-                prev=currNode;
-                currSum-=currNode.val;
-                stack.pop();
-                currNode=null;
-                
-                
-                
-            }
-            
-            
-            
-            
-        }
+    }
+    
+    private boolean hasPathSumUtil(TreeNode root,int targetSum,int currSum){
+        if(root==null) return false;
+        if(root.left==null && root.right==null && currSum+root.val ==targetSum) return true;
+        if(hasPathSumUtil(root.left,targetSum,currSum+root.val)) return true;
+        if(hasPathSumUtil(root.right,targetSum,currSum+root.val)) return true;
         return false;
         
         
     }
+    
 }
