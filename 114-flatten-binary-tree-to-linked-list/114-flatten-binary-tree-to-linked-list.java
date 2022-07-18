18af951e343790a38800731b8fe314b552961775
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-07-18 12:41:49 +0530

    Time: 2 ms (16.23%), Space: 43.3 MB (12.21%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -14,16 +14,36 @@
  * }
  */
 class Solution {
-  private TreeNode pre = null;
-
-public void flatten(TreeNode root) {
-    if (root == null)
-        return;
-    flatten(root.right);
-    flatten(root.left);
-    root.right = pre;
-    root.left = null;
-    pre = root;
-}
-
+    public void flatten(TreeNode root) {
+        Deque<TreeNode> stack=new LinkedList();
+        TreeNode prev=null;
+        TreeNode curr=root;
+        
+        while(curr!=null || !stack.isEmpty()){
+            while(curr!=null){    
+            stack.push(curr);
+            curr=curr.right;
+                
+            }
+            
+             curr=stack.peek();
+            if(curr.left!=null && curr.left!=prev){
+               
+                 curr=curr.left;
+            }else{
+               
+                
+                
+                 stack.pop();
+                curr.left=null;
+                curr.right=prev;
+                prev=curr;
+                curr=null;
+                
+            }
+            
+            
+        }
+        
+    }
 }
