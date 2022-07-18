d2a7d79d2a0d145b749701fd3b034d64ef365dc7
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-07-18 12:14:06 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (8.27%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -14,36 +14,16 @@
  * }
  */
 class Solution {
-    public void flatten(TreeNode root) {
-        
-        
-         preorderUtil(root);
-        
-    }
-    
-    private TreeNode preorderUtil(TreeNode root){
-        System.out.println("alex");
-        if(root==null) return null;
-        
-        TreeNode preleft=preorderUtil(root.left);
-        TreeNode preright=preorderUtil(root.right);
-        
-        root.left=null;
-        root.right=preleft;
-        TreeNode curr=root;
-        TreeNode prev=null;
-        while(curr!=null){
-            // System.out.println("preleft is :"+curr.val);
-            prev=curr;
-            curr=curr.right;
-        
-        }
-        prev.right=preright;
-        
-        return root;
-        
-        
-    }
-    
-    
+  private TreeNode pre = null;
+
+public void flatten(TreeNode root) {
+    if (root == null)
+        return;
+    flatten(root.right);
+    flatten(root.left);
+    root.right = pre;
+    root.left = null;
+    pre = root;
+}
+
 }
