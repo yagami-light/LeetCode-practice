c391e50987355739b5adec40d7dd6d9d605a4109
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-07-18 17:59:34 +0530

    Time: 2 ms (49.37%), Space: 45.1 MB (35.01%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -23,55 +23,35 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
+        if(root==null) return root;
         
-        return connectUtil(root);
-        
-    }
-    
-    private Node connectUtil(Node root){
-        if(root==null)
-            return null;
-        
-        
-        if(root.left!=null){
-            if(root.right!=null)
-            root.left.next=root.right;
-            else
-                root.left.next=findNext(root);
-        }
-        
-      
-        
-         if(root.right!=null){
-            root.right.next=findNext(root);
-        }
-        
-        connectUtil(root.right);
-                connectUtil(root.left);
+        Queue<Node> queue=new LinkedList();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            Node prev=null;
+            
+            for(int i=0;i<size;i++){
 
-        
+            Node node=queue.remove();
+                                        // System.out.println("alex node value :"+node.val);
+
+            if(node.left!=null)
+                queue.add(node.left);
+            
+            if(node.right!=null)
+                queue.add(node.right);
+            
+            if(prev!=null){
+                prev.next=node;
+            }
+            prev=node;
+            
+            }
+            
+        }
         
         return root;
         
     }
-    
-    
-    private  Node findNext(Node root){
-        if(root==null)
-            return null;
-        
-        while(root.next!=null){
-            root=root.next;
-            if(root.left!=null)
-                return root.left;
-            if(root.right!=null)
-                return root.right;
-            
-        }
-        return null;
-    }
-    
-    
-    
-    
 }
