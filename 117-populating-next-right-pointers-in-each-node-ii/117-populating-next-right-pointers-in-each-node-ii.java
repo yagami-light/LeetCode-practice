7ffbfcc22e356bfd387e52c27e379eca7be4951e
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-07-19 09:46:10 +0530

    Time: 0 ms (100.00%), Space: 45.5 MB (13.23%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -24,76 +24,42 @@ class Node {
 class Solution {
     public Node connect(Node root) {
         
-       return helper(root);
+        return connectUtil(root);
+        
     }
     
-    private Node helper(Node root){
-        
-        if(root==null)
-         return root;
-        
-        // System.out.println(:root value )
+    private Node connectUtil(Node root){
+        if(root==null) return root;
         if(root.left!=null){
-
-            if(root.right!=null)
+            if(root.right!=null){
                 root.left.next=root.right;
-            else
-                root.left.next=getNextValidNode(root);
-            
+                
+            }else{
+                root.left.next=getNext(root);
+            }
         }
-            
-           
-            
-        if(root.right!=null)
-            root.right.next=getNextValidNode(root);
-            
-
-        helper(root.right);
-                    helper(root.left);
-
+        
+        if(root.right!=null){
+            root.right.next=getNext(root);
+        }
+        
+        connectUtil(root.right);
+        connectUtil(root.left);
+        
         return root;
         
-        
     }
     
-    
-    private Node getNextValidNode2(Node root){
-        System.out.println("alex");
-        
-        while(root!=null){
-                        root=root.next;
-
-            // System.out.println("root value "+root.val);
-                        if(root.left!=null) return root.left;
-            
-            if(root.right!=null) return root.right;
-
-        }
-        System.out.println("alex 2");
-        
-        return null;
-        
-    }
-    
-     private Node getNextValidNode(Node root){
-        if(root==null)
-            return null;
+    private Node getNext(Node root){
+        if(root==null) return root;
         
         while(root.next!=null){
             root=root.next;
-            if(root.left!=null)
-                return root.left;
-            if(root.right!=null)
-                return root.right;
+            if(root.left!=null) return root.left;
+            if(root.right!=null) return root.right;
         }
-        
-        
         return null;
-        
-        
     }
-    
-    
     
     
 }
