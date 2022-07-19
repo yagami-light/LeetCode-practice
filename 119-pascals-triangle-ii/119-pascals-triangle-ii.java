04b8fca3f2b7c6e4f2c9c3b659b4e680b3615711
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-07-19 12:51:14 +0530

    Time: 2 ms (57.62%), Space: 42.1 MB (26.74%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -4,22 +4,19 @@ class Solution {
         int prev=1;
         res.add(1);
         
-        
         for(int i=1;i<=rowIndex;i++){
+            
             for(int j=1;j<i;j++){
-                
                 int temp=res.get(j);
                 res.set(j,prev+res.get(j));
                 prev=temp;
-                
-                
-                
             }
-            res.add(1);
             
+            res.add(1);
         }
         
         return res;
+        
         
     }
 }
