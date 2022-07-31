6087b981e334930cd4ce6dc761c1f643c05b94c0
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-07-31 09:13:21 +0530

    Time: 1555 ms (25.35%), Space: 55.1 MB (26.17%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
@@ -1,29 +1,43 @@
 class Solution {
     public int minCut(String s) {
         int n=s.length();
-        int[] cut=new int[n+1];
-        
-        for(int i=0;i<=n;i++){
-            cut[i]=i-1;
+        int[][] dp=new int[n][n];
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                dp[i][j]=-1;
+            }
         }
         
-        System.out.println("print cut :"+Arrays.toString(cut));
         
-        for(int i=0;i<n;i++){
-            
-            for(int j=0;i-j>=0 && i+j<n && s.charAt(i-j)==s.charAt(i+j);j++)
-                cut[i+j+1]=Math.min(cut[i+j+1],1+cut[i-j]);
-            
-             for(int j=1;(i-j+1)>=0 && i+j<n && s.charAt(i-j+1)==s.charAt(i+j);j++)
-                cut[i+j+1]=Math.min(cut[i+j+1],1+cut[i-j+1]);
-            
-            
-            
+        return helper(s,0,s.length()-1,dp);
+    }
+    
+    private int helper(String str,int start,int end,int[][] dp){
+        if(start>=end) return 0;
+        if(isPalindrome(str,start,end)) {dp[start][end]=0;return dp[start][end];}
+        if(dp[start][end]!=-1) return dp[start][end];
+        int minCut=Integer.MAX_VALUE;
+        for(int i=start;i<end;i++){
+            int res=Integer.MAX_VALUE;
+            if(isPalindrome(str,start,i)){
+                res=1+helper(str,i+1,end,dp);
+            }
+            minCut=Math.min(minCut,res);
         }
         
-        return cut[n];
-        
-        
+        dp[start][end]=minCut;
+        return dp[start][end];
         
     }
+    
+    
+    private boolean isPalindrome(String str,int start,int end){
+        while(start<end){
+            if(str.charAt(start)!=str.charAt(end)) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+    
 }
