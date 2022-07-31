2959ed30a9cd39183125684e1d52f48645d298e2
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-07-31 18:54:53 +0530

    Time: 4 ms (71.88%), Space: 42 MB (91.32%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.java
+++ 137-single-number-ii/137-single-number-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         Map<Integer,Integer> map=new HashMap();
         for(int i:nums)
-            map.put(i,map.getOrDefault(i,0)+1);
+            map.merge(i,1,Integer::sum);
         
         for(int i:map.keySet()){
             if(map.get(i)==1) return i;
