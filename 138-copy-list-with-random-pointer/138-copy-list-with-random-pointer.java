350cf0bb2aed11a4acfefbbe8b241b6fffb815ef
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-01 08:38:58 +0530

    Time: 23 ms (5.14%), Space: 46.3 MB (7.94%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -15,45 +15,39 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        Node curr=head;
-        Node cloneCurr=new Node(-1);
-        Node cloneHead=cloneCurr;
         Map<Node,Node> map=new HashMap();
+        
+        Node curr=head;
+        Node dummy=new Node(-1);
+        
         while(curr!=null){
             
-            Node newNode=new Node(curr.val);
-            map.put(curr,newNode);
-
+            Node copyNode=new Node(curr.val);
+            System.out.println("currNode val:"+curr.val+" copy Node val:"+copyNode.val);
+            dummy.next=copyNode;
+            map.put(curr,copyNode);
             curr=curr.next;
-            cloneCurr.next=newNode;
-            cloneCurr=cloneCurr.next;
+            dummy=dummy.next;
             
         }
         
-          curr=head;
-         cloneCurr=cloneHead.next;
-       while(curr!=null){
+        
+        Node newHead=map.get(head);
+        Node copyCurr=newHead;
+         curr=head;
+        while(curr!=null){
             
-            // Node newNode=new Node(curr.val);
-           // System.out.println("curr value : "+curr.val+" cloneCurr  value :"+cloneCurr.val);
-            Node targetNode=curr.random;
-           if(targetNode==null)
-           {
-               cloneCurr.random=null;
-           }else{
-            Node clonedOfTargeNode=map.get(targetNode);
-            // System.out.println("target value : "+targetNode +" target cloneCurr  value :"+clonedOfTargeNode);
-
-            cloneCurr.random=clonedOfTargeNode;
-           }
+            Node nextRandom=curr.random;
+            copyCurr.random=map.get(nextRandom);
+            
             curr=curr.next;
-            
-            cloneCurr=cloneCurr.next;
-            // map.put(curr,newNode);
+            copyCurr=copyCurr.next;
             
         }
         
-        return cloneHead.next;
+        return newHead;
+        
+        
         
     }
 }
