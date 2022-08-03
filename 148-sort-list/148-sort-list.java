463eee9bb7f5a175b5100e833908fa18f70b3265
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-03 11:56:17 +0530

    Time: 86 ms (5.39%), Space: 131.9 MB (5.10%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -10,24 +10,57 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        List<Integer> list=new ArrayList();
-        ListNode curr=head;
-        while(curr!=null){
-            list.add(curr.val);
-            curr=curr.next;
+        if(head==null || head.next==null) return head;
+        ListNode fast=head;
+        ListNode slow=head;
+        ListNode prev=null;
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            fast=fast.next.next;
+            slow=slow.next;
         }
-        ListNode dummy=new ListNode();
-        ListNode newHead=dummy;
-        Collections.sort(list);
-        // System.out.println("liset :"+list);
-        for(int i=0;i<list.size();i++){
-            System.out.println("list :"+list.get(i));
-            dummy.next=new ListNode(list.get(i));
-            dummy=dummy.next;
-        }
+        prev.next=null;
+        ListNode list1=head;
+        ListNode list2=slow;
+        // System.out.println("slow value :"+slow.val);
         
+        // printList(list1);
+        // printList(list2);
+        return merge(sortList(list1),sortList(list2));
         
-        return newHead.next;
         
     }
+    
+    private void printList(ListNode head){
+        System.out.println("printing...");
+        while(head!=null){
+            System.out.println("print : "+head.val);
+            head=head.next;
+        }
+    }
+    
+    
+    private ListNode merge(ListNode list1,ListNode list2){
+        // if(list1==null && list2==null) return list1;
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+        ListNode dummy=new ListNode();
+        if(list1.val<=list2.val){
+            ListNode res=new ListNode(list1.val);
+            res.next=merge(list1.next,list2);
+            dummy.next=res;
+            // return res;
+        }else{
+            
+           ListNode res=new ListNode(list2.val);
+            res.next=merge(list1,list2.next);
+             dummy.next=res;
+            // return res;
+            
+            
+        }
+        
+        return dummy.next;
+    }
+    
 }
