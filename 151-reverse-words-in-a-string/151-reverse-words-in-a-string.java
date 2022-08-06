a065dbf9546fd6441a9436aa69a8054581339472
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-06 11:43:54 +0530

    Time: 7 ms (78.88%), Space: 43.3 MB (69.31%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,57 +1,65 @@
 class Solution {
     public String reverseWords(String s) {
-        char[] input=s.trim().toCharArray();
-        // int i=0,j=input.length-1;
+        char[] input=s.toCharArray();
         reverse(input,0,input.length-1);
+        reverseWord(input,0,input.length-1);
+        System.out.println("reversed Word :"+new String(input));
+        return removeSpaces(input,input.length-1);
         
+        // return s;
         
-        // System.out.println("input now :"+new String(input));
-        // int i=0;
-        int start=0;
-        for(int i=0;i<input.length;i++){
+    }
+    
+    private String removeSpaces(char[] input,int n){
+        int i=0,j=0;
+        while(j<=n){
             
-            start=i;
-            while(i<input.length && input[i]!=' '){
-                i++;
-            }
-            // System.out.println("start is : "+start+" i "+i);
-            reverse(input,start,i-1);
-            // i++;
-            
+            while(j<=n && input[j]==' ') j++;
+            while(j<=n && i<=n && input[j]!=' ')
+                input[i++]=input[j++];
+            while(j<=n && input[j]==' ') j++;
+            if(j<=n)
+            input[i++]=' ';
+                
             
             
         }
+        return new String(input).substring(0,i);
         
-        return cleanSpaces(input,input.length);
         
-        
-        // return new String(input);
         
     }
-    String cleanSpaces(char[] a, int n) {
-    int i = 0, j = 0;
-      
-    while (j < n) {
-      while (j < n && a[j] == ' ') j++;             // skip spaces
-      while (j < n && a[j] != ' ') a[i++] = a[j++]; // keep non spaces
-      while (j < n && a[j] == ' ') j++;             // skip spaces
-      if (j < n) a[i++] = ' ';                      // keep only one space
-    }
-  
-    return new String(a).substring(0, i);
-  }
     
     
     private void reverse(char[] input,int start,int end){
-        int i=start,j=end;
+        int i=start;
+        int j=end;
         while(i<j){
             char temp=input[i];
             input[i]=input[j];
             input[j]=temp;
             i++;
             j--;
+            
+            
+        }
+        
+        
+    }
+    
+    private void reverseWord(char[] input,int start,int end){
+        
+        for(int i=start;i<=end;i++){
+            
+            start=i;
+            while(i<=end && input[i]!=' ')
+                i++;
+            reverse(input,start,i-1);
+                
+            
         }
         
     }
+    
     
 }
