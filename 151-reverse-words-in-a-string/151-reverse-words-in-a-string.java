0abd5d2a9cef2497f98cfd97defbb11481bd77a6
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-06 11:45:03 +0530

    Time: 13 ms (49.82%), Space: 43.6 MB (62.76%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,65 +1,8 @@
 class Solution {
     public String reverseWords(String s) {
-        char[] input=s.toCharArray();
-        reverse(input,0,input.length-1);
-        reverseWord(input,0,input.length-1);
-        System.out.println("reversed Word :"+new String(input));
-        return removeSpaces(input,input.length-1);
-        
-        // return s;
+        String[] inputArray=s.trim().split(" +");
+        Collections.reverse(Arrays.asList(inputArray));
+        return String.join(" ",inputArray);
         
     }
-    
-    private String removeSpaces(char[] input,int n){
-        int i=0,j=0;
-        while(j<=n){
-            
-            while(j<=n && input[j]==' ') j++;
-            while(j<=n && i<=n && input[j]!=' ')
-                input[i++]=input[j++];
-            while(j<=n && input[j]==' ') j++;
-            if(j<=n)
-            input[i++]=' ';
-                
-            
-            
-        }
-        return new String(input).substring(0,i);
-        
-        
-        
-    }
-    
-    
-    private void reverse(char[] input,int start,int end){
-        int i=start;
-        int j=end;
-        while(i<j){
-            char temp=input[i];
-            input[i]=input[j];
-            input[j]=temp;
-            i++;
-            j--;
-            
-            
-        }
-        
-        
-    }
-    
-    private void reverseWord(char[] input,int start,int end){
-        
-        for(int i=start;i<=end;i++){
-            
-            start=i;
-            while(i<=end && input[i]!=' ')
-                i++;
-            reverse(input,start,i-1);
-                
-            
-        }
-        
-    }
-    
-    
 }
