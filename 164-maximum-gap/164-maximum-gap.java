382e1923dffc237c5863b4504ed1509d68b4a330
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-07 20:50:47 +0530

    Time: 230 ms (7.64%), Space: 110.4 MB (7.71%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.java
+++ 164-maximum-gap/164-maximum-gap.java
@@ -1,59 +1,42 @@
 class Solution {
     public int maximumGap(int[] nums) {
-        if(nums==null || nums.length < 2) return 0;
+        if(nums==null || nums.length<2) return 0;
         
         int max=nums[0];
-        
-        for(int i: nums){
+        for(int i:nums)
             max=Math.max(max,i);
-        }
         
-        int[] aux=new int[nums.length];
         int exp=1;
-        int R=10;
-        
+        int DIGIT=10;
+        int[] aux=new int[nums.length];
         while(max/exp>0){
             
-            int[] count=new int[R];
-            /*
-            for(int i=0;i<count.length;i++){
-                System.out.println("print index :"+(nums[i]/exp) % 10);
+            int[] count=new int[DIGIT];
+            for(int i=0;i<nums.length;i++){
+                
                 count[(nums[i]/exp) % 10]++;
+                
             }
             
-            */
-            
-             for (int i = 0; i < nums.length; i++) {
-            count[(nums[i] / exp) % 10]++;
-        }
-            
-            System.out.println("print count :"+Arrays.toString(count));
-            
-            for(int i=1;i<count.length;i++)
+            for(int i=1;i<count.length;i++){
                 count[i]+=count[i-1];
-            System.out.println(" again print count:"+Arrays.toString(count));
-            
+            }
             for(int i=nums.length-1;i>=0;i--){
+                
                 aux[--count[(nums[i]/exp)%10]]=nums[i];
             }
             
-                        System.out.println("thried print count :"+Arrays.toString(count));
-
-            
-            for(int i=0;i<aux.length;i++)
-                nums[i]=aux[i];
-            
+            for(int i=0;i<nums.length;i++)
+                    nums[i]=aux[i];
+        
             exp*=10;
             
         }
         
-        System.out.println("print Array :"+Arrays.toString(nums));
+        System.out.println("nums :"+Arrays.toString(nums));
         int maxDiff=0;
-        for(int i=1;i<nums.length;i++){
-            System.out.println("alex");
-            maxDiff=Math.max(maxDiff,nums[i]-nums[i-1]);
-            
-        }
+       for(int i=1;i<nums.length;i++)
+           maxDiff=Math.max(maxDiff,nums[i]-nums[i-1]);
         
         return maxDiff;
         
