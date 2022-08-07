9aa97531b41f3d93f812a936b5eeae389a8b0f02
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-07 19:19:26 +0530

    Time: 175 ms (10.11%), Space: 56 MB (89.18%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.java
+++ 164-maximum-gap/164-maximum-gap.java
@@ -1,14 +1,43 @@
 class Solution {
     public int maximumGap(int[] nums) {
-        if(nums.length==1) return 0;
+        
+        int min=Integer.MAX_VALUE;
         int max=Integer.MIN_VALUE;
-        Arrays.sort(nums);
-        for(int i=1;i<nums.length;i++){
-            max=Math.max(max,nums[i]-nums[i-1]);
+        
+        for(int i:nums){
+            min=Math.min(i,min);
+            max=Math.max(i,max);
         }
         
+        int interval=(int)Math.ceil((max-min+0.0)/(nums.length-1));
         
-        return max;
+        int[] minGroup=new int[nums.length-1];
+        int[] maxGroup=new int[nums.length-1];
+        
+        Arrays.fill(minGroup,Integer.MAX_VALUE);
+        Arrays.fill(maxGroup,Integer.MIN_VALUE);
+        
+        for(int i: nums){
+            if(i==min || i==max) continue;
+            int index= (i-min)/interval;
+            minGroup[index]=Math.min(minGroup[index],i);
+            maxGroup[index]=Math.max(maxGroup[index],i);
+
+        }
+        int prev=min;
+        int maxDiff=0;
+        for(int i=0;i<minGroup.length;i++){
+            if(maxGroup[i]==Integer.MIN_VALUE) continue;
+            maxDiff=Math.max(maxDiff,minGroup[i]-prev);
+            prev=maxGroup[i];
+            
+        }
+        
+        System.out.println("minGroup :"+Arrays.toString(minGroup));
+        System.out.println("maxGroup :"+Arrays.toString(maxGroup));
+
+         maxDiff=Math.max(maxDiff,max-prev);
+        return maxDiff;
         
     }
 }
