6ab079d7102e86020298733c69ec9e893cd6a718
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-10 15:20:11 +0530

    Time: 529 ms (39.67%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
 
-select email as Email from Person group by email having count(*)>1
+select distinct p1.email as Email from Person p1,Person p2 where p1.email=p2.email AND p1.id!=p2.id
