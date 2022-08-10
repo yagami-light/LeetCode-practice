42bda43305c22de035ad6c16ec7a20505995b5c0
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-10 18:21:43 +0530

    Time: 919 ms (61.84%), Space: 0B (100.00%) - LeetHub

--- 185-department-top-three-salaries/185-department-top-three-salaries.sql
+++ 185-department-top-three-salaries/185-department-top-three-salaries.sql
@@ -2,5 +2,4 @@
 
 
 
-select Department,name as Employee,Salary from (select dpt.name as Department,emp.name,emp.salary,DENSE_RANK() OVER(PARTITION BY dpt.id ORDER BY emp.salary desc) as rankvalue  from Employee emp, Department dpt  where emp.departmentId=dpt.id ) as res where res.rankvalue <=3
-#group by dpt.id having rank <4
+select Department,Employee, Salary from (select dpt.name as Department,emp.name as Employee,emp.Salary,DENSE_RANK() OVER(PARTITION BY dpt.id ORDER BY emp.salary DESC) as "rankValue" from Employee emp,Department dpt where emp.departmentId=dpt.id) res where res.rankValue <4  
