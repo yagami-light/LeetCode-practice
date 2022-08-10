4cca56490456cff740a54534d4a2ca24e63db888
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-10 15:28:17 +0530

    Time: 633 ms (27.49%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
 
-select distinct p1.email as Email from Person p1,Person p2 where p1.email=p2.email AND p1.id!=p2.id
+select distinct p1.email as Email from Person p1 where  exists  (select 1 from Person p2 where p1.id!=p2.id AND p1.email=p2.email)
