288a61400f0ad56b9b78a29a8e78b8c29bbb1b86
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-11 12:02:57 +0530

    Time: 33 ms (70.52%), Space: 50.9 MB (90.96%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.java
@@ -1,20 +1,35 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
         int K=10;
-        Set<String> set=new HashSet();
-        Map<String,Integer> map=new HashMap();
+        Set<String> wordSet=new HashSet();
+        Set<Integer> numSet=new HashSet();
+        List<String> res=new LinkedList();
+        char[] map=new char[26];
+        map['A'-'A']=0;
+        map['C'-'A']=1;
+        map['G'-'A']=2;
+        map['T'-'A']=3;
+        // int v=0;
         for(int i=0;i<s.length()-K+1;i++){
-        String substr=s.substring(i,i+K);
-        // System.out.println("substr :"+substr);
-        map.put(substr,map.getOrDefault(substr,0)+1);
-        if(map.get(substr)>1){
-            set.add(substr);
-        }
+            int v=0;
+            for(int j=i;j<i+10;j++){
+                v=v<<2;
+                v |= map[s.charAt(j)-'A'];
+                
+                
+            }
+            if(!numSet.add(v) ){
+                wordSet.add(s.substring(i,i+10));
+            }
+            
             
             
         }
         
         
-        return new ArrayList(set);
+        return new ArrayList(wordSet);
+        
+        
+        
     }
 }
