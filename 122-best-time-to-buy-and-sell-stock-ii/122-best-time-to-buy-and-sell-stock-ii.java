27b75f49590188af598d2a6bcf46dc401481d37b
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-11 17:10:45 +0530

    Time: 9 ms (5.09%), Space: 49.4 MB (5.07%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,15 +1,31 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int total=0;
-        int size=prices.length;
-        for(int i=1;i<size;i++){
+        int n=prices.length;
+        
+        int[][] state=new int[n][2];
+        for(int[] array:state){
+            Arrays.fill(array,-1);
+        }
+        int index=0,k=1;
+        boolean isBuy=true;
+        return find(prices,index,k,isBuy,state);
+    }
+    
+    private int find(int[] prices,int index,int k,boolean isBuy,int[][] state){
+        if(index>=prices.length) return 0;
+        if(state[index][isBuy ? 1 : 0] != -1) return state[index][isBuy ? 1 : 0];
+        
+        if(isBuy){
             
-            if(prices[i]-prices[i-1]>0){
-                total+=prices[i]-prices[i-1];
-            }
+            return state[index][isBuy ? 1 : 0]=Math.max(find(prices,index+1,k,isBuy,state),find(prices,index+1,k,!isBuy,state)-prices[index]);
+            
+        }else{
+            
+            return state[index][isBuy? 1: 0 ]=Math.max(find(prices,index+1,k,isBuy,state),find(prices,index+1,k,!isBuy,state)+prices[index]);
             
         }
-        return total;
+        
         
     }
+    
 }
