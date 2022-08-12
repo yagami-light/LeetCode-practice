05ba3f0cd2e5a8f195d71b7d5a35d0c708892934
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-12 16:57:21 +0530

    Time: 13 ms (15.06%), Space: 56.5 MB (74.86%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,54 +1,55 @@
 class Solution {
+    public static final int[][] DIRECTIONS=new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
     public int numIslands(char[][] grid) {
-        
         int m=grid.length;
         int n=grid[0].length;
         int count=0;
-        Set<String> set=new HashSet();
         for(int i=0;i<m;i++){
             
             for(int j=0;j<n;j++){
                 
                 if(grid[i][j]=='1'){
-                    dfs(grid,i,j,set);
                     count++;
+                       Queue<int[]> queue=new LinkedList();
+                        grid[i][j]='0';
+                        queue.add(new int[]{i,j});
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            for(int k=0;k<size;k++){
+                
+                int[] node=queue.remove();
+                // System.out.println("print node :" +Arrays.toString(node));
+                
+               for(int[] dir:DIRECTIONS){
+                   
+                   int ni=node[0]+dir[0];
+                   int nj=node[1]+dir[1];
+                   // System.out.println(" node i and j :"+ni+" "+nj);
+                   
+                   if(ni<0 || nj<0 || ni>=m || nj >=n || grid[ni][nj]=='0') {}
+                   else{
+                grid[ni][nj]='0';
+                   queue.add(new int[]{ni,nj});
+                   }
+                   
+               }
+                    
+                
+            }
+        }
                 }
                 
             }
-            
         }
+        
         return count;
-        
-        
     }
     
-    private void dfs(char[][] grid,int i,int j,Set<String> set){
-        int m=grid.length;
-        int n=grid[0].length;
+    void bfs(char[][] grid,int i,int j){
         
-        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]=='0') return;
-        
-        // set.add(i+"@"+j);
-        grid[i][j]='0';
-        
-        dfs(grid,i+1,j,set);
-        dfs(grid,i,j+1,set);
-        dfs(grid,i-1,j,set);
-        dfs(grid,i,j-1,set);
+        // if(i<0 || j<0 || i>=m || j >=n || grid[i][j]=='0') return;
+    
         
     }
-    
     
 }
-
-/*
-[
-["1","1","0","0","0"],
-["1","1","0","0","0"],
-["0","0","1","0","0"],
-["0","0","0","1","1"]]
-
-
-
-
-*/
