2863bf1f4ea6e69ae302b9d3c344f1930d5004f6
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-18 23:45:57 +0530

    Time: 0 ms (100.00%), Space: 50.1 MB (38.74%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
@@ -16,14 +16,42 @@
 class Solution {
     public int countNodes(TreeNode root) {
         if(root==null) return 0;
+        // int left=height(root,true);
+        // int right=height(root,false);
+        int left=getHeightLeft(root);
+        int right=getHeightRight(root);
+        if(left==right) return (2<<left)-1;
+        else
+            return countNodes(root.left)+countNodes(root.right)+1;
         
-        return helper(root);
         
     }
+      public int getHeightLeft(TreeNode root){
+       if(root.left==null) return 0;
+    else return 1+getHeightLeft(root.left);
+    }
     
-    private int helper(TreeNode root){
+    
+    public int getHeightRight1(TreeNode root){
+        int count=0;
+        while(root.right!=null){
+            count++;
+            root = root.right;
+        }
+        return count;
+    }
+    public int getHeightRight(TreeNode root){
+    if(root.right==null) return 0;
+        else
+            return 1+getHeightRight(root.right);
+    }
+    
+    private int height(TreeNode root,boolean isLeft){
+        
         if(root==null) return 0;
-        return 1+helper(root.left)+helper(root.right);
+        else
+            return 1+(isLeft ? height(root.left,isLeft) : height(root.right,isLeft));
+        
     }
     
 }
