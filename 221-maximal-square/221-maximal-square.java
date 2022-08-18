a6b982169ff62d56cefb3f54f42aed532652203f
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-18 19:43:24 +0530

    Time: 2467 ms (5.01%), Space: 57.2 MB (77.11%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -1,6 +1,5 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        
         int m=matrix.length;
         int n=matrix[0].length;
         int max_ans=0;
@@ -8,42 +7,45 @@ class Solution {
         for(int i=0;i<m;i++){
             
             for(int j=0;j<n;j++){
-                
                 if(matrix[i][j]=='1'){
-                    int curr=1;
-                    boolean flag=true;
-                    while(i+curr<m && j+curr<n && flag){
+                boolean flag=true;
+                int curr=1;
+                
+                while(i+curr<m && j+curr<n && flag){
+                    
+                    for(int k=i;k<=i+curr;k++){
                         
-                        for(int k=i;k<=i+curr;k++){
-                            if(matrix[k][j+curr]=='0'){
-                                flag=false;
-                                break;
-                            }
-                        }
-                        if(!flag) break;
-                        for(int k=j;k<=j+curr;k++){
-                            if(matrix[i+curr][k]=='0'){
-                                flag=false;
-                                break;
-                            }
-                            
+                        if(matrix[k][j+curr]=='0'){
+                            flag=false;
+                            break;
                         }
                         
-                        
-                        if(!flag) break;
-                        curr++;
                     }
+                    if(!flag) break;
+                    for(int k=j;k<=j+curr;k++){
+                        if(matrix[i+curr][k]=='0'){
+                            flag=false;
+                            break;
+                        }
+                        
+                    }
+                    if(!flag) break;
+                
                     
-                    max_ans=Math.max(max_ans,curr);
                     
-                    
+                   curr++; 
                 }
+                
+                max_ans=Math.max(max_ans,curr);
+                
+                
+                
+                
             }
             
         }
+        }
         
         return max_ans*max_ans;
-        
-        
     }
 }
