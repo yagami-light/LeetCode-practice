c6be1c13903ebc9e36d2f95bffd44629625586c0
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-19 17:43:26 +0530

    Time: 1 ms (33.04%), Space: 41.7 MB (58.29%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -1,67 +1,32 @@
 class MyStack {
 
-    Queue<Integer> queue1;
-    Queue<Integer> queue2;
+    Queue<Integer> queue;
     
     public MyStack() {
-        queue1=new LinkedList();
-        queue2=new LinkedList();
+        queue=new LinkedList();
+        
     }
     
     public void push(int x) {
-        queue1.add(x);
-        // System.out.println("push queue1 :"+queue1);
+        queue.add(x);
+        int size=queue.size();
+        for(int i=0;i<size-1;i++){
+            queue.add(queue.remove());
+        }
         
     }
     
     public int pop() {
-        int res=-1;
-        while(!queue1.isEmpty()){
-            int no=queue1.remove();
-            if(!queue1.isEmpty())
-            queue2.add(no);
-            else
-                res=no;
-            
-        }
-        
-      
-        Queue<Integer> temp=queue1;
-        queue1=queue2;
-        queue2=temp;
-     
-        return res;
-        
-        
-        
-        
+        return queue.remove();
     }
     
     public int top() {
-        int res=-1;
-        while(!queue1.isEmpty()){
-            int no=queue1.remove();
-            // System.out.println("no is :"+no);
-            queue2.add(no);
-            if(queue1.isEmpty()){
-                res=no;
-            }
-        }
-      
-           // System.out.println("res is top :"+res);
-       
-        Queue<Integer> temp=queue1;
-        queue1=queue2;
-        queue2=temp;
-              // System.out.println("queue1 :"+queue1);
-
-        return res;
+        return queue.peek();
         
     }
     
     public boolean empty() {
-        return queue1.isEmpty() && queue2.isEmpty();
-        
+        return queue.isEmpty();
     }
 }
 
