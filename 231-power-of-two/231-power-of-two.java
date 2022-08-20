cd2a1ad09adb27358989b09793f9880ec199a46d
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-20 22:51:08 +0530

    Time: 2 ms (72.24%), Space: 41 MB (73.40%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-     
-        return n>0 && (n & (n-1)) == 0;
+        if(n==0) return false;
+        if(n==1 || n==2) return true;
+        if(n%2 !=0) return false;
+        return isPowerOfTwo(n/2);
         
     }
 }
