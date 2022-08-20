fd9a8491d236a3c12938ba23f05d0b969ef41a7d
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-20 21:26:45 +0530

    Time: 2 ms (93.39%), Space: 49.2 MB (82.17%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,12 +1,10 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n=nums.length;
-        int count1=0;
-        int curr1=0;
-        int curr2=0;
-        int count2=0;
         List<Integer> res=new ArrayList();
+        int count1=0,count2=0,curr1=0,curr2=0;
         for(int i:nums){
+            
             if(curr1==i){
                 count1++;
             }
@@ -20,39 +18,32 @@ class Solution {
             else if(count2==0){
                 count2=1;
                 curr2=i;
-            }else{
+            }
+            else{
                 count1--;
                 count2--;
+                
             }
             
             
         }
-        System.out.println("curr1 : "+curr1+" curr2 :"+curr2);
-        // return Arrays.asList(new Integer[]{curr1,curr2});
         
         count1=0;
         count2=0;
-        
         for(int i:nums){
-            if(curr1==i){
+            if(i==curr1)
                 count1++;
-            }
-            else if(curr2==i){
+            else if(i==curr2)
                 count2++;
-            }
-            
+        }
+        if(count1>n/3){
+            res.add(curr1);
+        }
+        if(count2>n/3){
+            res.add(curr2);
         }
         
-        if(count1>n/3)
-            res.add(curr1);
-        if(count2>n/3)
-            res.add(curr2);
-        
         return res;
-        
-        
-        
-        
         
     }
 }
