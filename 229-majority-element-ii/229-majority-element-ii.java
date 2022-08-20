83ba13d9b0457361828f15fc0076e91c69f44a2e
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-20 21:22:48 +0530

    Time: 14 ms (25.97%), Space: 50 MB (39.27%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,17 +1,58 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer,Integer> map=new HashMap();
-        int minCount=nums.length/3;
-        Set<Integer> res=new HashSet();
+        int n=nums.length;
+        int count1=0;
+        int curr1=0;
+        int curr2=0;
+        int count2=0;
+        List<Integer> res=new ArrayList();
         for(int i:nums){
+            if(curr1==i){
+                count1++;
+            }
+            else if(curr2==i){
+                count2++;
+            }
+            else if(count1==0){
+                count1=1;
+                curr1=i;
+            }
+            else if(count2==0){
+                count2=1;
+                curr2=i;
+            }else{
+                count1--;
+                count2--;
+            }
             
-            map.put(i,map.getOrDefault(i,0)+1);
-            if(map.get(i)>minCount)
-                res.add(i);
+            
+        }
+        System.out.println("curr1 : "+curr1+" curr2 :"+curr2);
+        // return Arrays.asList(new Integer[]{curr1,curr2});
+        
+        count1=0;
+        count2=0;
+        
+        for(int i:nums){
+            if(curr1==i){
+                count1++;
+            }
+            else if(curr2==i){
+                count2++;
+            }
             
         }
         
-        return new ArrayList(res);
+        if(count1>n/3)
+            res.add(curr1);
+        if(count2>n/3)
+            res.add(curr2);
+        
+        return res;
+        
+        
+        
+        
         
     }
 }
