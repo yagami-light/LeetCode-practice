a4fea7ba703848428b176ec19fba891cc9c052a2
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-21 16:36:20 +0530

    Time: 25 ms (6.07%), Space: 57.4 MB (77.31%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,40 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        
+        int mul=1;
         int n=nums.length;
         int[] prefixMul=new int[n];
-        int[] res=new int[n];
-        int mul=1;
         for(int i=0;i<n;i++){
             prefixMul[i]=mul;
             mul *= nums[i];
         }
-        // System.out.println("print array : "+Arrays.toString(prefixMul));
-         mul=1;
+        
+        System.out.println("prefix mul : "+Arrays.toString(prefixMul));
+        
+        mul=1;
         for(int i=n-1;i>=0;i--){
-            prefixMul[i]= prefixMul[i]*mul;
-             mul=mul*nums[i];
+            prefixMul[i]=prefixMul[i]*mul;
+            mul *= nums[i];
             
         }
-        // nums[0] *=mul;
+        
         return prefixMul;
-        
-        
     }
 }
-
-/*
-
-[1,2,3,4]
-
-[1,1,2,6]
-
-[a1,a2,a3,a4]
-
-[1*a2*a3*a4, a1*a3*a4, a1*a2*a4, a1*a2*a3]
-
-
-[1 , a1, a1*a2,a1*a2*a3]
-
-
-
-*/
