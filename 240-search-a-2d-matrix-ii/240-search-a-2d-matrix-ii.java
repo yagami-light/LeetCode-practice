ea918f82dc6e9b3071e76b384a90bd95922a2897
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-21 19:42:13 +0530

    Time: 13 ms (36.71%), Space: 53.6 MB (84.18%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -4,31 +4,18 @@ class Solution {
         int m=matrix.length;
         int n=matrix[0].length;
         int i=0,j=n-1;
-        while(i<m && j < n){
-            if(j<0 || i ==m) return false;
+        while(i<m && j >=0){
             if(matrix[i][j]==target) return true;
-            
-            while(j>=0 && i <m && matrix[i][j] > target){
-                // // System.out.println("matrix i j "+matrix[i][j]);
+            else if(matrix[i][j]  > target){
                 j--;
             }
-                        if(j<0 || i ==m) return false;
-
-            // System.out.println("out while : "+matrix[i][j]);
-              while(j>=0 && i <m && matrix[i][j] < target){
-                // System.out.println("alex matrix i j "+matrix[i][j]);
+            else if(matrix[i][j] <target){
                 i++;
             }
-                        if(j<0 || i ==m) return false;
-
-            // System.out.println("alex");
+            
             
         }
         
-        
         return false;
-        
-        
-        
     }
 }
