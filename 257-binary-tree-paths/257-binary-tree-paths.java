b069ce7364869868f49798de15437630dcb653b4
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-23 18:20:50 +0530

    Time: 17 ms (14.63%), Space: 43.9 MB (9.24%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.java
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -14,23 +14,67 @@
  * }
  */
 class Solution {
+    
     public List<String> binaryTreePaths(TreeNode root) {
-        
         List<String> res=new ArrayList();
-        if(root==null) return res;
-        if(root.left==null && root.right==null){
-            res.add(Integer.toString(root.val));
-            return res;   
-        }
         
-        for(String str:binaryTreePaths(root.left)){
-            res.add(Integer.toString(root.val)+"->"+str);
-        }
-         for(String str:binaryTreePaths(root.right)){
-            res.add(Integer.toString(root.val)+"->"+str);
+        Queue<TreeNode> queueNode=new LinkedList();
+        Queue<String> queueStr=new LinkedList();
+        queueNode.add(root);
+        queueStr.add("");
+        
+        while(!queueNode.isEmpty()){
+            
+            TreeNode currNode=queueNode.remove();
+            String currStr=queueStr.remove();
+            
+            if(currNode.left==null && currNode.right==null){
+                res.add(currStr+currNode.val);
+            }
+            if(currNode.left!=null){
+                queueNode.add(currNode.left);
+                queueStr.add(currStr+currNode.val+"->");
+            }
+             if(currNode.right!=null){
+                queueNode.add(currNode.right);
+                queueStr.add(currStr+currNode.val+"->");
+            }
+            
+            
+            
         }
         
         return res;
         
+        
     }
+    
+    
+    
+   /* 
+    public List<String> binaryTreePaths(TreeNode root) {
+    List<String> list=new ArrayList<String>();
+    Queue<TreeNode> qNode=new LinkedList<TreeNode>();
+    Queue<String> qStr=new LinkedList<String>();
+    
+    if (root==null) return list;
+    qNode.add(root);
+    qStr.add("");
+    while(!qNode.isEmpty()) {
+        TreeNode curNode=qNode.remove();
+        String curStr=qStr.remove();
+        
+        if (curNode.left==null && curNode.right==null) list.add(curStr+curNode.val);
+        if (curNode.left!=null) {
+            qNode.add(curNode.left);
+            qStr.add(curStr+curNode.val+"->");
+        }
+        if (curNode.right!=null) {
+            qNode.add(curNode.right);
+            qStr.add(curStr+curNode.val+"->");
+        }
+    }
+    return list;
+}
+  */  
 }
