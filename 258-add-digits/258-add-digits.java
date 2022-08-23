0b8134b8c225fa347f71af2f0ff958c12f9ffb26
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-23 22:00:22 +0530

    Time: 2 ms (72.85%), Space: 41.9 MB (14.35%) - LeetHub

--- 258-add-digits/258-add-digits.java
+++ 258-add-digits/258-add-digits.java
@@ -1,29 +1,7 @@
 class Solution {
     public int addDigits(int num) {
-        int res=num;
-        while(!(res<10)){
-            res=helper(res);
-        }
         
-        return res;
-    }
-    
-    
-    
-    private int  helper(int nums){
-        System.out.println("nums : "+nums);
-        int res=0;
-        while(nums!=0){
-            res += nums%10;
-            int no=nums/10;
-            // System.out.println("no is :"+no);
-            // res+=no;
-            nums=no;
-            
-        }
+        return (num-1)%9+1;
         
-    return res;    
     }
-    
-    
 }
