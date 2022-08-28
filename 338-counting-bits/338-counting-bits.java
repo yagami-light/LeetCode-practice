66a9959908eeb3030d1e373a1cf77b7233d30af0
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-28 17:44:14 +0530

    Time: 2 ms (87.63%), Space: 48.7 MB (13.85%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] countBits(int n) {
         int[] res=new int[n+1];
         for(int i=1;i<=n;i++){
-            res[i]=Integer.bitCount(i);
+            res[i]=res[i/2]+ (i & 1);
         }
         return res;
     }
