22559a73ef1b4fa004ee12226099060da9f02fe6
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-08-30 20:56:21 +0530

    Time: 2 ms (96.39%), Space: 43 MB (69.68%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -1,39 +1,35 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        
-        Deque<Integer> stack=new LinkedList();
-        stack.push(-1);
-        
-        int maxLength=Integer.MIN_VALUE;
+        int open=0,close=0;
+        int max=0;
         for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                open++;
+            }else
+                close++;
             
-            char c=s.charAt(i);
-            if(c=='('){
-                stack.push(i);
+            if(close==open){
+                max=Math.max(max,open+close);
+            }else if(open<close){
+                open=0;close=0;
             }
-            else{
-                stack.pop();
-                if(stack.isEmpty()){
-                    stack.push(i);
-                    
-                }else{
-                    
-                    int len=i-stack.peek();
-                    maxLength=Math.max(len,maxLength);
-                }
-                
-                
+        }
+         open=0;close=0;
+        
+        for(int i=s.length()-1;i>=0;i--){
+               if(s.charAt(i)=='('){
+                open++;
+            }else
+                close++;
+            
+            if(close==open){
+                max=Math.max(max,open+close);
+            }else if(close<open){
+                open=0;close=0;
             }
-            
-            
-            
-            
         }
         
-        
-        return maxLength==Integer.MIN_VALUE ? 0 : maxLength;
-        
-        
+        return max;
         
         
     }
