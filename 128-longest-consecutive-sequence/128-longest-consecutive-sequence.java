f3b4a0654f5151160c19750243bb6b8fb47d21a2
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-09-02 11:43:38 +0530

    Time: 529 ms (27.10%), Space: 151 MB (28.07%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,61 +1,24 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        Map<Integer,List<Integer>> map=new HashMap();
-        for(int val:nums)
-            map.put(val,new ArrayList());
-        
-        for(int val:nums){
-            if(map.containsKey(val-1)){
-                map.get(val-1).add(val);
-                map.get(val).add(val-1);
-            }
-            }
-        
-        for(int val:nums){
-            if(map.containsKey(val+1)){
-                map.get(val+1).add(val);
-                map.get(val).add(val+1);
-            }
-        }
-        
-        Queue<Integer> queue=new LinkedList();
-        Set<Integer> set=new HashSet();
-        int maxLength=0;
-        
-        for(int i: map.keySet()){
-            if(!set.contains(i)){
-            set.add(i);
-            queue.add(i);
-                   int length=1;
-            while(!queue.isEmpty()){
-             
-                int size=queue.size();
-                // for(int j=0;j<size;j++){
-                    int num=queue.remove();
-                    for(int val:map.get(num)){
-                        if(!set.contains(val)){
-                            set.add(val);
-                        queue.add(val);
-                        length++;
-                        
-                        }
-                        
-                    // }
-                    
-                }
-                maxLength=Math.max(maxLength,length);
-                
+        Set<Integer> set=new HashSet(Arrays.stream(nums).boxed().toList());
+        // System.out.println("print set :"+set);
+        int count=0;
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            
+            int num=nums[i];
+            if(!set.contains(num-1)){
+               while(set.contains(num++)){
+                   count++;
+               }
                 
             }
-            
-            
+            max=Math.max(max,count);
+            count=0;
             
         }
-        }
         
-        
-        return maxLength;
-        
+        return max;
         
     }
 }
