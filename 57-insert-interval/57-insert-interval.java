5e1a0b263c5ebc5a0328e6d615385502cfba299a
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-09-02 19:53:51 +0530

    Time: 2 ms (82.78%), Space: 47.8 MB (39.07%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -3,17 +3,16 @@ class Solution {
         List<int[]> res=new ArrayList();
         for(int[] interval:intervals){
             
-            if(newInterval[1] <  interval[0]){
+            if(newInterval[1] < interval[0]){
                 res.add(newInterval);
                 newInterval=interval;
-                
-            }
-            else if(newInterval[0] > interval[1]){
+            }else if(interval[1] < newInterval[0]){
                 res.add(interval);
-            }
-            else{
+            }else{
+                
                 newInterval[0]=Math.min(newInterval[0],interval[0]);
                 newInterval[1]=Math.max(newInterval[1],interval[1]);
+                
             }
             
         }
