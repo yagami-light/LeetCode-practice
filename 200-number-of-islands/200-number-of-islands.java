e2b05dcf965791818f93dc461cce0f8af71d77aa
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-09-02 11:15:25 +0530

    Time: 15 ms (9.91%), Space: 56.7 MB (74.25%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,6 +1,9 @@
 class Solution {
-    public static final int[][] DIRECTIONS=new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+    
+    public static final int[][] DIR=new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+    
     public int numIslands(char[][] grid) {
+        
         int m=grid.length;
         int n=grid[0].length;
         int count=0;
@@ -9,47 +12,36 @@ class Solution {
             for(int j=0;j<n;j++){
                 
                 if(grid[i][j]=='1'){
-                    count++;
-                       Queue<int[]> queue=new LinkedList();
-                        grid[i][j]='0';
-                        queue.add(new int[]{i,j});
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            for(int k=0;k<size;k++){
-                
-                int[] node=queue.remove();
-                // System.out.println("print node :" +Arrays.toString(node));
-                
-               for(int[] dir:DIRECTIONS){
-                   
-                   int ni=node[0]+dir[0];
-                   int nj=node[1]+dir[1];
-                   // System.out.println(" node i and j :"+ni+" "+nj);
-                   
-                   if(ni<0 || nj<0 || ni>=m || nj >=n || grid[ni][nj]=='0') {}
-                   else{
-                grid[ni][nj]='0';
-                   queue.add(new int[]{ni,nj});
-                   }
-                   
-               }
                     
-                
-            }
-        }
+                    count++;
+                    Queue<int[]> queue=new LinkedList();
+                    queue.add(new int[]{i,j});
+                    
+                    while(!queue.isEmpty()){
+                        
+                        int[] node=queue.remove();
+                        grid[node[0]][node[1]]='0';
+                        
+                        for(int[] dir:DIR){
+                            
+                            int newX=node[0]+dir[0];
+                            int newY=node[1]+dir[1];
+                            if(newX<0 || newY <0 || newX >=m || newY >=n)  continue;
+                            if(grid[newX][newY]=='1'){
+                                grid[newX][newY]='0';
+                            queue.add(new int[]{newX,newY});
+                            }
+                        }
+                        
+                    }
+                    
                 }
                 
             }
+            
+            
         }
-        
         return count;
-    }
-    
-    void bfs(char[][] grid,int i,int j){
-        
-        // if(i<0 || j<0 || i>=m || j >=n || grid[i][j]=='0') return;
-    
         
     }
-    
 }
