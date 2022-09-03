d64aa6b6661e6ef1e275414228ebcf335f52bbf3
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-09-03 19:10:44 +0530

    Time: 194 ms (42.63%), Space: 42.6 MB (27.19%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,58 +1,49 @@
 class Solution {
-    
-    
+    private static int[][] DIR=new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
     public boolean exist(char[][] board, String word) {
         int m=board.length;
         int n=board[0].length;
+        char[] input=word.toCharArray();
+        
+        // int m=board.length;
+        // int n=board[0].length;
         
         for(int i=0;i<m;i++){
             
             for(int j=0;j<n;j++){
                 
-                if(existUtil(board,word,i,j,0))
+                if(exist(board,input,i,j,0)){
                     return true;
+                }
                 
                 
             }
+            
         }
         
         return false;
         
     }
     
-    private boolean existUtil(char[][] board,String word,int i,int j,int index){
-        if(i<0 || j<0 | i>=board.length || j>= board[0].length)
-            return false;
-        if(board[i][j]==word.charAt(index) && index==word.length()-1)
-            return true;
+    private boolean exist(char[][] board,char[] input,int i,int j,int index){
+        int m=board.length;
+        int n=board[0].length;
+         if(index>=input.length) return true;
+        if( i<0 || j<0 || i>=m || j>=n || board[i][j]=='#') return false;
+       
+        char ch=board[i][j];
+        // System.out.println("i :"+i+" j :"+j+" index :"+index);
+        board[i][j]='#';
         
-        else{
+        for(int[] dir:DIR){
             
-            if(board[i][j]==word.charAt(index)){
-                
-                char ch=board[i][j];
-                board[i][j]='.';
-                
-                boolean flag= existUtil(board,word,i+1,j,index+1) ||
-                    existUtil(board,word,i,j+1,index+1) ||
-                    existUtil(board,word,i-1,j,index+1) ||
-                    existUtil(board,word,i,j-1,index+1);
-                board[i][j]=ch;
-                return flag;
-            }
-            
-            return false;
-            
-            
+            if(ch==input[index] && exist(board,input,i+dir[0],j+dir[1],index+1))
+                return true;
             
         }
         
-        
-        
-        
+        board[i][j]=ch;
+        return false;
         
     }
-    
-    
-    
 }
