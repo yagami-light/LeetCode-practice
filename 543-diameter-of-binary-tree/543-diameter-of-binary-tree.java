ead5e91ad00db02c68a7f2d8e277b38451c73877
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-09-05 10:20:00 +0530

    Time: 6 ms (17.76%), Space: 44.6 MB (6.54%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -15,23 +15,34 @@
  */
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        if(root==null)
-            return 0;
-        
-        int[] max=new int[1];
-         helper(root,max);
-        return max[0];
-        
+        Deque<TreeNode> stack=new LinkedList();
+        stack.push(root);
+        Map<TreeNode,Integer> map=new HashMap();
+        int max=0;
+        while(!stack.isEmpty()){
+            
+            TreeNode node=stack.peek();
+            if(node.left!=null && !map.containsKey(node.left)){
+                stack.push(node.left);
+            }
+            else if(node.right!=null && !map.containsKey(node.right)){
+                stack.push(node.right);
+            }
+
+            else{
+                
+                node=stack.pop();
+                int left=map.getOrDefault(node.left,0);
+                int right=map.getOrDefault(node.right,0);
+                map.put(node,Math.max(left,right)+1);
+                max=Math.max(max,left+right);
+                
+                
+            }
+            
+            
+        }
+        return max;
         
     }
-    
-    private int helper(TreeNode root,int[] max){
-        if(root==null) return 0;
-        
-        int left=helper(root.left,max);
-        int right=helper(root.right,max);
-        max[0]=Math.max(max[0],Math.max(left+right,left+right));
-        return Math.max(left,right)+1;
-    }
-    
 }
