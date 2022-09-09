fee41d89f315439cba8bdcefa8e6f320cce02d27
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-09-09 18:21:08 +0530

    Time: 22 ms (10.13%), Space: 44 MB (85.30%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,43 +1,49 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        //always n1 short
-        
-        int n1=nums1.length;
-        int n2=nums2.length;
-        
-        
-        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
-        
-        int lo=0,hi=n1;
-        while(lo<=hi){
+    public double findMedianSortedArrays(int[] a, int[] b) {
+              int n1=a.length;
+        int n2=b.length;
             
-            int mid1=(lo+hi)/2;
-            int mid2=(n1+n2)/2-mid1;
+        if(n1>n2) return findMedianSortedArrays(b,a);
+        return helper(a,b,0,a.length);
+    }
+    
+      private static double helper(int[] a,int[] b,int start,int end){
+      
+        if(start>end) return -1;
+        int n1=a.length;
+        int n2=b.length;
+        int mid1=(start+end)/2;
+        int mid2=(n1+n2)/2-mid1;
+        int L1=mid1 == 0 ? Integer.MIN_VALUE : a[mid1-1];
+        int L2= mid2 == 0 ? Integer.MIN_VALUE : b[mid2-1];
+        int R1=(mid1==n1) ? Integer.MAX_VALUE : a[mid1];
+        int R2=(mid2==n2) ? Integer.MAX_VALUE : b[mid2];
+       System.out.println(" L1 : "+L1+" L2 :"+L2+" R1 :"+R1+" R2 :"+R2);
+       
+        if(  L1>R2 ){
+            return helper(a,b,start,mid1-1);
+        }else if(L2 > R1){
+            return helper(a,b,mid1+1,end);
+        }else{
+             {
+         if((n1+n2) %2!=0){
+             return Math.min(R1,R2);
+         }else
+             return ((double)Math.min(R1,R2) +Math.max(L1,L2))/2;
+        }
             
-            
-            int L1=mid1==0 ? Integer.MIN_VALUE : nums1[mid1-1];
-            int L2=mid2==0 ? Integer.MIN_VALUE : nums2[mid2-1];
-            int R1=mid1==n1 ? Integer.MAX_VALUE : nums1[mid1];
-            int R2=mid2==n2 ?Integer.MAX_VALUE : nums2[mid2];
-            
-            if(L1>R2){
-                hi=mid1-1;
-            }
-            else if (L2>R1){
-                lo=mid1+1;
-            }
-            else{
-                // return (Math.max(L1,L2) + Math.min(R1, R2)) / 2;
-                return (n1+n2)%2 !=0 ? Math.min(R1,R2) : (Math.max(L1,L2) + Math.min(R1,R2))/2.;
-            }
-            
-            
-            
+            ///
             
         }
         
-        return -1;
+        /*   
+        
+       3  
+2 3        5 7 8
+        
+        */
         
         
     }
+    
 }
