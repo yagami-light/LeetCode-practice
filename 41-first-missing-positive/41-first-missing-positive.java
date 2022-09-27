ca7ae67cef6aa3a1af1e6846d7cd1cf22855c308
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-09-27 19:06:32 +0530

    Time: 2 ms (89.05%), Space: 57.5 MB (72.94%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -2,35 +2,33 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         
         for(int i=0;i<nums.length;i++){
-            
-            if(nums[i]<0) nums[i]=0;
-            
+            if(nums[i]<0){
+                nums[i]=0;
+            }
         }
-        
-        // [1,2,0]
         
         for(int i=0;i<nums.length;i++){
             
             int index=Math.abs(nums[i]);
-            System.out.println("index :"+index);
-            if(index-1>=0 &&index-1 < nums.length){
+            if(index-1>=0 && index-1<nums.length){
                 
                 if(nums[index-1]==0){
                     nums[index-1]=-(nums.length+1);
-                }else
-                
-            nums[index-1]=-Math.abs(nums[index-1]);
+                }else{
+                    nums[index-1]=-Math.abs(nums[index-1]);
+                }
                 
             }
             
             
         }
-        System.out.println("print array :"+Arrays.toString(nums));
         for(int i=1;i<=nums.length;i++){
             if(nums[i-1]>=0) return i;
         }
         
         return nums.length+1;
+        
+        
         
     }
 }
