1008c84e13291f4b5fac6d712cca39bd5ebc537e
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-09-28 09:39:47 +0530

    Time: 11 ms (33.45%), Space: 45.5 MB (48.56%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,15 +1,16 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        
-        List<List<String>> res=new ArrayList();
+        List<List<String>>  res=new ArrayList();
         
         helper(new ArrayList(),n,res);
+        
         return res;
         
+        
     }
+    
     private void helper(List<Integer> currentQueen,int n,List<List<String>> res){
         if(currentQueen.size()==n){
-            
             List<String> tempList=new ArrayList();
             for(int i:currentQueen){
                 char[] chars=new char[n];
@@ -17,44 +18,40 @@ class Solution {
                 chars[i]='Q';
                 tempList.add(new String(chars));
                 
-                
             }
             res.add(tempList);
-            
             
         }
         
         for(int col=0;col<n;col++){
+            
             if(!currentQueen.contains(col) && !digAttack(currentQueen,col)){
+                
                 currentQueen.add(col);
                 helper(currentQueen,n,res);
                 currentQueen.remove(currentQueen.size()-1);
+                
+                
             }
             
             
         }
-        
-        
         
         
     }
     
     private boolean digAttack(List<Integer> currentQueen,int col){
-        int currentCol=col;
+        
         int currentRow=currentQueen.size();
         
         for(int row=0;row<currentQueen.size();row++){
             
-            if(Math.abs(row-currentRow)==Math.abs(col-currentQueen.get(row)))
-                return true;
+            if(Math.abs(row-currentRow)==Math.abs(col-currentQueen.get(row))) return true;
             
         }
         return false;
         
         
-        
     }
-    
-    
     
 }
