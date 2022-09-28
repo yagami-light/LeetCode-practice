dd224204e161c5ec233c930a317215f288965c37
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-09-28 19:49:37 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (94.87%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -2,26 +2,28 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         
         List<List<Integer>> res=new ArrayList();
-        int bit_length=nums.length;
-        int ans_length=1 << bit_length;
-        
-        for(int i=0;i<ans_length;i++){
-            
-            List<Integer> temp=new ArrayList();
-            int count=0;
-            int i_copy=i;
-            while(i_copy!=0){
-                if((i_copy & 1) ==1){
-                    temp.add(nums[count]);
-                }
-                count++;
-                i_copy=i_copy >>1;
-            }
-            res.add(temp);
-            
-        }
-        
+        int index=0;
+        helper(nums,new ArrayList(),res,index);
         return res;
         
+        
     }
+    
+    private void helper(int[] nums,List<Integer> currList,List<List<Integer>> res,int index){
+        
+        res.add(new ArrayList(currList));
+        if(index>=nums.length) return;
+        
+        for(int i=index;i<nums.length;i++){
+        
+        currList.add(nums[i]);
+        helper(nums,currList,res,i+1);
+        currList.remove(currList.size()-1);
+        }
+        
+        
+        
+    }
+    
+    
 }
