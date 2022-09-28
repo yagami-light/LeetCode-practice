293a8eec286f9e6e2b0ae093c27223098887b3ed
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-09-28 09:30:00 +0530

    Time: 10 ms (39.04%), Space: 45.2 MB (61.99%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,38 +1,39 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
+        
         List<List<String>> res=new ArrayList();
-        solveNQueens(new ArrayList(),res,n);
+        
+        helper(new ArrayList(),n,res);
         return res;
+        
     }
-    
-    private void solveNQueens(List<Integer> currentQueen,List<List<String>> res,int n){
+    private void helper(List<Integer> currentQueen,int n,List<List<String>> res){
         if(currentQueen.size()==n){
-            List<String> temp=new ArrayList();
             
+            List<String> tempList=new ArrayList();
             for(int i:currentQueen){
                 char[] chars=new char[n];
                 Arrays.fill(chars,'.');
                 chars[i]='Q';
-                temp.add(new String(chars));
+                tempList.add(new String(chars));
+                
+                
             }
+            res.add(tempList);
             
-            res.add(temp);
+            
         }
         
         for(int col=0;col<n;col++){
-            
             if(!currentQueen.contains(col) && !digAttack(currentQueen,col)){
-                
                 currentQueen.add(col);
-                solveNQueens(currentQueen,res,n);
+                helper(currentQueen,n,res);
                 currentQueen.remove(currentQueen.size()-1);
-                
-                
             }
             
             
-            
         }
+        
         
         
         
@@ -44,15 +45,16 @@ class Solution {
         
         for(int row=0;row<currentQueen.size();row++){
             
-            if((Math.abs(row-currentRow)) == Math.abs(currentCol-currentQueen.get(row)))
+            if(Math.abs(row-currentRow)==Math.abs(col-currentQueen.get(row)))
                 return true;
             
         }
-        
         return false;
         
         
+        
     }
+    
     
     
 }
