ed63225f76cfe2f19489cbf4147485b473781a97
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2022-09-28 15:54:01 +0530

    Time: 55 ms (5.37%), Space: 111.4 MB (12.21%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -10,53 +10,27 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head==null || head.next==null) return true;
-        
-       ListNode fast=head;
-       ListNode slow=head;
-    ListNode prev=null;
-       while(fast!=null && fast.next!=null){
-           
-           fast=fast.next.next;
-             prev=slow;
-           slow=slow.next;
-         
-       }
-        prev.next=null;
-        
-       ListNode temp1=head;
-    ListNode    temp2=reverse(slow);
-        while(temp1!=null){
-            if(temp1==null  && temp2==null) return true;
-            if(temp1==null || temp2==null) {
-                System.out.println(" failing ...");
-                // System.out.println(" temp 1 :"+temp1 == null ? "null":temp1.val);
-                // System.out.println(" temp 2 :"+temp2 == null ? "null":temp2.val);
-
-                return false;
-                
-            }
-            // System.out.println("temp1 val :"+temp1.val + " temp2 val : "+temp2.val);
-            if(temp1.val ==temp2.val){
-                temp1=temp1.next;
-                temp2=temp2.next;
-            }else{
-                return false;
-            }
+        Stack<ListNode> stack=new Stack();
+        ListNode curr=head;
+        while(curr!=null){
+            
+            stack.push(curr);
+            curr=curr.next;
+            
+            
+            
         }
         
-        return true;
+        curr=head;
+        while(curr!=null){
+            if(curr.val!=stack.peek().val) return false;
+            stack.pop();
+            curr=curr.next;
+            
+            
+        }
+        return stack.isEmpty();
+        
         
     }
-    
-    private ListNode reverse(ListNode head){
-        if(head==null || head.next==null) return head;
-        
-        ListNode secondReverse=reverse(head.next);
-        head.next.next=head;
-        head.next=null;
-        return secondReverse;
-        
-    }
-    
 }
