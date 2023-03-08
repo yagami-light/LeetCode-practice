0aafd09a3ca75d0efe1329030f5318cdda0b352d
KEVAL VAGHELA <kevalvaghela1993@gmail.com>
2023-03-08 21:44:35 +0530

    Time: 91 ms (5.62%), Space: 76.2 MB (8.70%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,27 +1,43 @@
 class Solution {
     public int maxProfit(int[] prices) {
         
-        int n=prices.length;
-        int[][] state=new int[n][2];
+        int size=prices.length;
+        int transactionSize=1;
+        boolean isBuy=true;
+        int[][] state=new int[size][2];
         for(int[] arr:state){
             Arrays.fill(arr,-1);
         }
-        boolean isBuy=true;
         int index=0;
-        int k=1;
-        return helper(prices,index,state,isBuy,k);
         
+        return helper(prices,index,isBuy,state,transactionSize);
         
     }
     
-    int helper(int[] prices,int index,int[][] state,boolean isBuy,int k){
-        if(k==0 || index >=prices.length) return 0;
-        if(state[index][isBuy?0:1]!=-1) return state[index][isBuy?0:1];
+    int helper(int[] prices,int index,boolean isBuy,int[][] state,int transactionSize){
+        if(transactionSize==0 || index>=prices.length)
+            return 0;
+        
+        if(state[index][isBuy?0:1]!=-1)
+            return state[index][isBuy?0:1];
+        
         if(isBuy){
-            return state[index][isBuy?0:1]=Math.max(helper(prices,index+1,state,isBuy,k),-prices[index]+helper(prices,index+1,state,!isBuy,k));
+            
+            return state[index][isBuy?0:1]=Math.max(
+                helper(prices,index+1,isBuy,state,transactionSize)
+                ,-prices[index]+helper(prices,index+1,!isBuy,state,transactionSize)
+            
+            );
         }else{
-            return state[index][isBuy?0:1]=Math.max(helper(prices,index+1,state,isBuy,k),prices[index]+helper(prices,index+1,state,!isBuy,k-1));
+            
+            return state[index][isBuy?0:1]=Math.max(
+                helper(prices,index+1,isBuy,state,transactionSize)
+                ,prices[index]+helper(prices,index+1,!isBuy,state,transactionSize-1)
+            );
+                                            
         }
+        
+        
         
     }
     
